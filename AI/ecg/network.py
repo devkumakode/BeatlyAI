2e5d154ca27ba275bf8713448d0a86f2226ba835
devganesh <devganesh@velozityglobal.com>
2024-07-03 17:46:42 +0000

    Optimize ecg/network for better inference time (lines 0-17)

--- AI/ecg/network.py
+++ AI/ecg/network.py
@@ -1,4 +1,17 @@
-from keras import backend as K
+    from keras.layers import Activation
+    layer = BatchNormalization()(layer)
+    layer = Activation(params["conv_activation"])(layer)
 
-def _bn_relu(layer, dropout=0, **params):
-    from keras.layers import BatchNormalization
+    if dropout > 0:
+        from keras.layers import Dropout
+        layer = Dropout(params["conv_dropout"])(layer)
+
+    return layer
+
+def add_conv_weight(
+        layer,
+        filter_length,
+        num_filters,
+        subsample_length=1,
+        **params):
+    from keras.layers import Conv1D 
