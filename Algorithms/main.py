dfba192b4e9fe1e8b9dc5ac54836eefe71aea103
mjragul <mjragul@velozityglobal.com>
2024-07-03 14:33:27 +0000

    Implement active learning strategy in main (lines 0-1)

--- Algorithms/main.py
+++ Algorithms/main.py
@@ -1,12 +1,1 @@
-WINDOWS_SIZE = 220
-DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-qtdb_pkl = 'resources/qtdb_pkl/'
-save_path = 'resources/ckpt'
-
-PATH_TO_TEST_MODEL = 'resources/z-score-data/ckpt' + '/epoch_99.ckpt'
-
-if __name__ == '__main__':
-    print(DEVICE)
-    if DEVICE == 'cuda':
-        print(torch.cuda.get_device_name(0))
