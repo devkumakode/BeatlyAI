49986fbd099dcfeea3bb8fef32d324811ee37179
mjragul <mjragul@velozityglobal.com>
2024-07-04 12:00:12 +0000

    Refactor modeling/model for better code organization (lines 0-2)

--- Algorithms/modeling/model.py
+++ Algorithms/modeling/model.py
@@ -1,15 +1,2 @@
-					num_layers=num_layers,
-					batch_first=True,
-					bidirectional=True
-			),
-		)
-		self.classifier = torch.nn.Sequential(
-			torch.nn.Linear(2*hidden_size, 2*hidden_size),
-			torch.nn.ReLU(inplace=True),
-			torch.nn.Dropout(),
 
-			torch.nn.Linear(2 * hidden_size, 2 * hidden_size),
-			torch.nn.ReLU(inplace=True),
-			torch.nn.Dropout(),
-		)
-		self.output = torch.nn.Linear(2*hidden_size, out_size)
+	def forward(self, x):
