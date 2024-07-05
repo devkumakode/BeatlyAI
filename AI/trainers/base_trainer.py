0d2c172367d9f31f72888e9b6b59f2833353a5ee
devganesh <devganesh@velozityglobal.com>
2024-07-05 15:18:27 +0000

    Add support for mixed precision training in trainers/base_trainer (lines 0-2)

--- AI/trainers/base_trainer.py
+++ AI/trainers/base_trainer.py
@@ -1,15 +1,2 @@
-import os
-import os.path as osp
-from datetime import datetime
-
-import numpy as np
-import torch
-from torch import nn, optim
-from torch.utils.tensorboard import SummaryWriter
-from tqdm import tqdm
-
-from utils.network_utils import load_checkpoint, save_checkpoint
-
-
-class BaseTrainer:
-    def __init__(self, config):
+        self.config = config
+        self.exp_name = self.config.get("exp_name", None)
