9447f779884c551325d081cec6a1f1a267252d16
aman <aman@velozityglobal.com>
2024-07-06 14:34:09 +0000

    Add support for multiclass classification in engine/test (lines 0-20)

--- Algorithms/engine/test.py
+++ Algorithms/engine/test.py
@@ -1,8 +1,20 @@
-# 	num_classes = params["num_classes"]
+# 		output = model(steps)
 #
-# 	confusion = np.zeros((num_classes, num_classes))
-# 	for iteration, (steps, targets, _) in enumerate(tqdm(val_loader)):
-# 		if params["use_cuda"]:
-# 			steps = steps.cuda()
-# 			targets = targets.cuda()
+# 		rows = targets.cpu().numpy()
+# 		cols = output.max(1)[1].cpu().numpy()
 #
+# 		confusion[rows, cols] += 1
+#
+# 		loss += criterion(output, targets)
+#
+# 	loss = loss / iteration
+# 	acc = np.trace(confusion) / np.sum(confusion)
+#
+# 	# Plot confusion matrix in visdom
+# 	logger.heatmap(confusion, win='4', opts=dict(
+# 		title="Confusion_Matrix_epoch_{}".format(epoch),
+# 		columnnames=["A","B","C","D","E"],
+# 		rownames=["A","B","C","D","E"])
+# 	)
+#
+# 	return loss, acc
