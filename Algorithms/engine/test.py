1e0832625406b3c57e803cc3be75cecde879321e
mjragul <mjragul@velozityglobal.com>
2024-07-06 14:38:06 +0000

    Add type hints to engine/test (lines 0-18)

--- Algorithms/engine/test.py
+++ Algorithms/engine/test.py
@@ -1,20 +1,18 @@
-# 		output = model(steps)
-#
-# 		rows = targets.cpu().numpy()
-# 		cols = output.max(1)[1].cpu().numpy()
-#
-# 		confusion[rows, cols] += 1
-#
-# 		loss += criterion(output, targets)
-#
-# 	loss = loss / iteration
-# 	acc = np.trace(confusion) / np.sum(confusion)
-#
-# 	# Plot confusion matrix in visdom
-# 	logger.heatmap(confusion, win='4', opts=dict(
-# 		title="Confusion_Matrix_epoch_{}".format(epoch),
-# 		columnnames=["A","B","C","D","E"],
-# 		rownames=["A","B","C","D","E"])
-# 	)
-#
-# 	return loss, acc
+
+
+def test(net, test_loader, device, batch_size, plot_ecg=False, plot_ecg_windows_size=None):
+	with torch.no_grad():
+		ecgs_list = []
+		labels_list = []
+		predicted_list = []
+		right = 0.0
+		total = 0.0
+		net.eval()
+		for ix, sample in enumerate(test_loader):
+			ecgs = sample['ecg']
+			labels = sample['label']
+
+			ecgs = ecgs.to(device)
+			labels = labels.to(device)
+
+			if ecgs.shape[0] < batch_size:
