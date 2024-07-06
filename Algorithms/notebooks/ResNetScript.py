98724accec34b93e2e5f499d4624bb4c2bd1055c
devakumar <devakumar@velozityglobal.com>
2024-07-06 16:15:51 +0000

    Add support for sparse matrices in notebooks/ResNetScript (lines 0-11)

--- Algorithms/notebooks/ResNetScript.py
+++ Algorithms/notebooks/ResNetScript.py
@@ -1,10 +1,11 @@
-# a: https://discuss.pytorch.org/t/what-is-the-difference-between-torch-nn-and-torch-nn-functional/33597/2
 
-# Get cpu, gpu or mps device for training 
-device = (
-    "cuda"
-    if torch.cuda.is_available()
-    else "mps"
-    if torch.backends.mps.is_available()
-    else "cpu"
-)
+
+class ECGDataSet(Dataset):
+    
+    def __init__(self, split='train'):
+
+        self.split = split
+
+        # data loading
+        current_directory = os.getcwd()
+        self.parent_directory = os.path.dirname(current_directory)
