6f561b168814fa732ee9cc062f853173b7c5c461
devakumar <devakumar@velozityglobal.com>
2024-07-06 16:55:22 +0000

    Refactor modeling/model for better code organization (lines 0-5)

--- Algorithms/modeling/model.py
+++ Algorithms/modeling/model.py
@@ -1,5 +1,5 @@
-		super().__init__()
-		self.batch_size = batch_size
-		self.features = nn.Sequential(
-			nn.Conv1d(in_channels=input_size, out_channels=1, kernel_size=kernel_size, padding=0),
-			nn.ReLU(),
+			# nn.MaxPool1d(3, stride=1),
+			nn.Dropout()
+		)
+		# Classify output, fully connected layers
+		self.memory = nn.Sequential(
