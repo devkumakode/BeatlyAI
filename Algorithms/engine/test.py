1e8355406b43a2b7561c0c9f5675273536c1cc93
mjragul <mjragul@velozityglobal.com>
2024-07-06 17:44:14 +0000

    Implement grid search for hyperparameter tuning in engine/test (lines 0-8)

--- Algorithms/engine/test.py
+++ Algorithms/engine/test.py
@@ -1,20 +1,8 @@
-import pickle
-
-import numpy as np
-import torch
-from sklearn.metrics import classification_report
-from sklearn.metrics._plot import confusion_matrix
-
-import config
-from util.utils import plot_learning_curve
-from util.utils import plot_confusion_matrix
-from visualization import visualise_ecg
-
-
-# def eval(val_loader, model, criterion, params, epoch, logger):
-# 	"""Main method to evaluate model."""
-# 	model.eval()
-# 	print("================================")
-# 	print("Evaluating...")
+# 	num_classes = params["num_classes"]
 #
-# 	loss = 0
+# 	confusion = np.zeros((num_classes, num_classes))
+# 	for iteration, (steps, targets, _) in enumerate(tqdm(val_loader)):
+# 		if params["use_cuda"]:
+# 			steps = steps.cuda()
+# 			targets = targets.cuda()
+#
