d4ef5d68875b5710d9d6567d9958e9cd33f65e01
aman <aman@velozityglobal.com>
2024-07-06 11:16:55 +0000

    Optimize notebooks/ResNetScript for better performance (lines 0-10)

--- Algorithms/notebooks/ResNetScript.py
+++ Algorithms/notebooks/ResNetScript.py
@@ -1,16 +1,10 @@
-import torch
-from torch import nn
-from torch.utils.data import DataLoader, Dataset # wraps an iterable around the dataset
-from torchvision import datasets    # stores the samples and their corresponding labels
-from torchvision.transforms import transforms  # transformations we can perform on our dataset
-from torchvision.transforms import ToTensor
-import pandas as pd
-import numpy as np
-import os
-import wandb
-import matplotlib.pyplot as plt
+# a: https://discuss.pytorch.org/t/what-is-the-difference-between-torch-nn-and-torch-nn-functional/33597/2
 
-
-import torch.optim as optim
-import torch.nn.functional as F
-# q: what is the difference between torch.nn.functional and torch.nn
+# Get cpu, gpu or mps device for training 
+device = (
+    "cuda"
+    if torch.cuda.is_available()
+    else "mps"
+    if torch.backends.mps.is_available()
+    else "cpu"
+)
