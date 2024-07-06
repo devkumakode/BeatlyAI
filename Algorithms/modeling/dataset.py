1716eaf0910d901bebca042911273b46f226c28b
devakumar <devakumar@velozityglobal.com>
2024-07-06 14:00:02 +0000

    Update dependencies for modeling/dataset (lines 0-8)

--- Algorithms/modeling/dataset.py
+++ Algorithms/modeling/dataset.py
@@ -1,14 +1,8 @@
+		label = torch.from_numpy(self.y[idx]).float()
 
-	def __init__(self, data_path, transform=None):
-		self.transform = transform
-		with open(data_path, 'rb') as f:
-			self.x, self.y = pickle.load(f)
+		sample = {
+			'ecg': ecg,
+			'label': label
+		}
 
-	def __len__ (self):
-		return len(self.x)
-
-	def __getitem__ (self, idx):
-		if torch.is_tensor(idx):
-			idx = idx.tolist()
-
-		ecg = torch.from_numpy(self.x[idx]).float()
+		if self.transform:
