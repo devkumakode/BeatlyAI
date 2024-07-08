b0a3f63d397dc087d14a8d9972061a774ef63e7c
naveenkurra <naveenkurra@velozityglobal.com>
2024-07-08 11:49:03 +0000

    Update dependencies for train (lines 0-3)

--- AI/train.py
+++ AI/train.py
@@ -1,6 +1,3 @@
-import torch
-from binarynet import ConvLayer_bin, FCLayer_bin
-import warnings
-warnings.filterwarnings("ignore")
 
-TOPK = (1,5)
+def accuracy(output, target, correct_sum, topk=(1,)):
+    """Compute the accuracy over the k top predictions for the specified values of k."""
