0b3cd06acc87709f18ae52636b21bc592c927531
aman <aman@velozityglobal.com>
2024-07-08 10:56:04 +0000

    Add feature scaling in modeling/model (lines 0-3)

--- Algorithms/modeling/model.py
+++ Algorithms/modeling/model.py
@@ -1,19 +1,3 @@
-			torch.nn.LSTM(
-					input_size=input_size,
-					hidden_size=hidden_size,
-					num_layers=1,
-					batch_first=True,
-					bidirectional=True
-			)
-		)
-		self.deconv = nn.ConvTranspose1d(in_channels=2*hidden_size, out_channels=hidden_size, kernel_size=kernel_size, padding=0)
-		self.classifier = torch.nn.Sequential(
-			torch.nn.Linear(hidden_size, 2 * hidden_size),
-			torch.nn.ReLU(inplace=True),
-			torch.nn.Dropout(),
-		)
-		self.output = torch.nn.Linear(2 * hidden_size, out_size)
-
-	def forward(self, x):
+		:return:
 		"""
-		:param x: shape(batch, seq_len, input_size) -->  (batch_size, features/input_channels, timesteps/signal_length)
+		batch, seq_len, nums_fea = x.size()
