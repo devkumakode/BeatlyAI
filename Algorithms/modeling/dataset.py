ffba4998bc7d3ed881cc7d170ec7dbed95eba2b8
aman <aman@velozityglobal.com>
2024-07-09 09:56:25 +0000

    Implement reinforcement learning algorithm in modeling/dataset (lines 0-14)

--- Algorithms/modeling/dataset.py
+++ Algorithms/modeling/dataset.py
@@ -1,5 +1,14 @@
-			sample['ecg'] = self.transform(sample['ecg'])
+class PyTorchMinMaxScalerVectorized(object):
 
-		return sample
+	def __init__(self, fitted_min_max_scaler: MinMaxScaler):
+		self.fitted_min_max_scaler: MinMaxScaler = fitted_min_max_scaler
+
+	def __call__(self, tensor):
+		return torch.tensor(self.fitted_min_max_scaler.transform(tensor.numpy()))
 
 
+def fit_min_max_scaler (path, type='stad'):
+	with open(path, 'rb') as f:
+		x, y = pickle.load(f)
+
+		reshaped_x = x.reshape((x.shape[0] * x.shape[1], x.shape[2]))
