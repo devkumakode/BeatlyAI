36871880c504254329dd9ebd67273fe8b466ef6c
maheshb <maheshb@velozityglobal.com>
2024-07-09 15:09:31 +0000

    Implement early stopping in trainers/base_trainer (lines 0-14)

--- AI/trainers/base_trainer.py
+++ AI/trainers/base_trainer.py
@@ -1,2 +1,14 @@
-        self.config = config
-        self.exp_name = self.config.get("exp_name", None)
+        if self.exp_name is None:
+            self.exp_name = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+
+        self.log_dir = osp.join(self.config["exp_dir"], self.exp_name, "logs")
+        self.pth_dir = osp.join(self.config["exp_dir"], self.exp_name, "checkpoints")
+        os.makedirs(self.log_dir, exist_ok=True)
+        os.makedirs(self.pth_dir, exist_ok=True)
+
+        self.writer = SummaryWriter(log_dir=self.log_dir)
+
+        self.model = self._init_net()
+        self.optimizer = self._init_optimizer()
+        self.criterion = nn.CrossEntropyLoss().to(self.config["device"])
+
