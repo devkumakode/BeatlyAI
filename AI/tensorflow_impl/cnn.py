1a64f71a6c6070c41bbe941dcbd4b0e2d0d51433
devganesh <devganesh@velozityglobal.com>
2024-07-10 14:09:30 +0000

    Add logging to tensorflow_impl/cnn (lines 0-8)

--- AI/tensorflow_impl/cnn.py
+++ AI/tensorflow_impl/cnn.py
@@ -1,9 +1,8 @@
-import time
-import argparse
 
-import tensorflow as tf
-import numpy as np
 
-from utils import (shuffle_tensors, next_minibatch, get_labels,
-                   get_datasets, TensorBoardHandler, ModelSaver,
-                   check_processed_dir_existance)
+par = argparse.ArgumentParser(description="ECG Convolutional " +
+                                           "Neural Network implementation")
+
+par.add_argument("-lr", dest="learning_rate",
+                 type=float, default=0.001,
+                 help="Learning rate used by the model")
