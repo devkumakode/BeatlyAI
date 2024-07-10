24200586c1b634c0d89c841440eaa6d7984f067f
devganesh <devganesh@velozityglobal.com>
2024-07-10 17:35:19 +0000

    Add support for sparse matrices in modeling/model (lines 0-11)

--- Algorithms/modeling/model.py
+++ Algorithms/modeling/model.py
@@ -1,3 +1,11 @@
-		x = x.reshape((batch, 1, seq_len))
 
-		batch, nums_fea, seq_len = x.size()
+		features = self.features(x)
+		features_res = features.reshape((batch, 398, 1))
+
+		memory, _ = self.memory(features_res)
+		deconv = self.deconv(memory.reshape((batch, 64, 398)))
+		deconv = deconv.reshape((batch, seq_len, 32))
+		output = self.classifier(deconv)
+		out = self.output(output.view(batch * seq_len, -1))
+
+		return out
