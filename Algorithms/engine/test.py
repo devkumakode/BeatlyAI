f7d9e0757a4a9a1e95cba9ab10f5b8523693860e
devakumar <devakumar@velozityglobal.com>
2024-07-10 12:49:59 +0000

    Implement reinforcement learning algorithm in engine/test (lines 0-5)

--- Algorithms/engine/test.py
+++ Algorithms/engine/test.py
@@ -1,18 +1,5 @@
+				batch_size = ecgs.shape[0]
 
+			output = net(ecgs)
+			output = output.to(device)
 
-def test(net, test_loader, device, batch_size, plot_ecg=False, plot_ecg_windows_size=None):
-	with torch.no_grad():
-		ecgs_list = []
-		labels_list = []
-		predicted_list = []
-		right = 0.0
-		total = 0.0
-		net.eval()
-		for ix, sample in enumerate(test_loader):
-			ecgs = sample['ecg']
-			labels = sample['label']
-
-			ecgs = ecgs.to(device)
-			labels = labels.to(device)
-
-			if ecgs.shape[0] < batch_size:
