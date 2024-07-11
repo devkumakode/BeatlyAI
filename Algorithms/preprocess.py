4fbade03abdd8d11182b7bd22f43b760f3c85e4c
aman <aman@velozityglobal.com>
2024-07-11 15:28:31 +0000

    Add support for imbalanced datasets in preprocess (lines 0-3)

--- Algorithms/preprocess.py
+++ Algorithms/preprocess.py
@@ -1,15 +1,3 @@
-
-		return data
-
-
-def split_ecg_lead_by_window (ecg, window=200):
-	tmp = [x for x in np.array_split(np.array(ecg), int(len(ecg) / window)) if x.size > 0]
-	res = np.zeros((len(tmp), window, 1))
-
-	try:
-		for ix in range(len(tmp)):
-			if len(tmp[ix]) // window:
-				tmp[ix] = tmp[ix][:window]
-
-				BaselineWanderRemoval.fix_baseline_wander(tmp[ix], sr=127)
-
+			res[ix] = np.array(tmp[ix]).reshape((window, 1))
+	except ValueError as e:
+		print(e)
