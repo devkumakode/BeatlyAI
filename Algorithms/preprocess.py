f216dd4896dc85e9fa6e130a031117d001d818b3
devakumar <devakumar@velozityglobal.com>
2024-07-11 15:27:42 +0000

    Add support for multi-label classification in preprocess (lines 0-15)

--- Algorithms/preprocess.py
+++ Algorithms/preprocess.py
@@ -1,14 +1,15 @@
-SAVE_TO_FILE = False
+
+		return data
 
 
-def extract_name (text, start_marker, end_marker):
-	start = text.index(start_marker) + len(start_marker)
-	end = text.index(end_marker, start)
+def split_ecg_lead_by_window (ecg, window=200):
+	tmp = [x for x in np.array_split(np.array(ecg), int(len(ecg) / window)) if x.size > 0]
+	res = np.zeros((len(tmp), window, 1))
 
-	return text[start:end]
+	try:
+		for ix in range(len(tmp)):
+			if len(tmp[ix]) // window:
+				tmp[ix] = tmp[ix][:window]
 
+				BaselineWanderRemoval.fix_baseline_wander(tmp[ix], sr=127)
 
-def load_json (filename):
-	with open(filename) as f:
-		data = json.load(f)
-		f.close()
