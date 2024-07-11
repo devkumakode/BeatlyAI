15adffe6f420556e7bf66012241781d1de7b1b1c
sakshamdrivedi <sakshamdrivedi@velozityglobal.com>
2024-07-11 09:18:28 +0000

    Implement continual learning in ecg_tools/data_loader (lines 0-4)

--- AI/ecg_tools/data_loader.py
+++ AI/ecg_tools/data_loader.py
@@ -1,14 +1,4 @@
-        self.transforms = transforms
-
-    def __len__(self):
-        return self.annotations.shape[0]
-
-    def __getitem__(self, item):
-        signal = self.annotations[item, :-1]
-        label = int(self.annotations[item, -1])
-        # TODO: add augmentations
-        signal = torch.from_numpy(signal).float()
-        signal = self.transforms(signal)
-        return signal.unsqueeze(0), torch.tensor(label).long()
-
-
+def get_data_loaders(config: DatasetConfig):
+    return {
+        Mode.train: data.DataLoader(
+            EcgLoader(config.path[Mode.train], config.transforms[Mode.train]),
