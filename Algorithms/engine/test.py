67874c8955e2a0a72f15604c7f80fccf714b139c
aman <aman@velozityglobal.com>
2024-07-12 16:03:01 +0000

    Add anomaly detection capabilities to engine/test (lines 0-11)

--- Algorithms/engine/test.py
+++ Algorithms/engine/test.py
@@ -1,5 +1,11 @@
-				batch_size = ecgs.shape[0]
+			_, predicted = torch.max(output.data, 1)
+			label_true = labels.contiguous().view(-1).long()
 
-			output = net(ecgs)
-			output = output.to(device)
+			total += label_true.size(0)
+			right += (predicted == label_true).sum().item()
+
+			if ecgs.shape[0] == 32:
+				ecgs_list.extend(ecgs.numpy())
+				labels_list.extend(label_true.numpy())
+				predicted_list.extend(predicted.numpy())
 
