fa0f37791447a1e9981dfb6283aca4352726e397
aman <aman@velozityglobal.com>
2024-07-13 16:25:34 +0000

    Improve error handling in engine/train (lines 0-19)

--- Algorithms/engine/train.py
+++ Algorithms/engine/train.py
@@ -1,4 +1,19 @@
-    cmap = ['k', 'r', 'g', 'b']
-    start = end = 0
-    for i in range(len(y)-1):
-        if y[i] != y[i+1]:
+            end = i
+            plt.plot(np.arange(start, end+1), x[start:end+1], cmap[int(y[i])])
+            start = i+1
+    plt.show()
+
+
+def train(net, train_loader, val_loader, epochs, criterion, optimizer, device, batch_size):
+    loss_values_final = []
+    accuracy_final = []
+
+    for step in range(epochs):
+        running_loss = 0.0
+        net.train()
+        net.to(device)
+
+        loss_values = []
+        accuracy = []
+
+        for i, samples_batch in enumerate(train_loader):
