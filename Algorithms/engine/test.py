bd288b107f8dc44e46423807a9e1db60b68788b7
premkumar <premkumar@velozityglobal.com>
2024-07-13 10:45:03 +0000

    Add support for imbalanced datasets in engine/test (lines 0-5)

--- Algorithms/engine/test.py
+++ Algorithms/engine/test.py
@@ -1,11 +1,5 @@
-			_, predicted = torch.max(output.data, 1)
-			label_true = labels.contiguous().view(-1).long()
-
-			total += label_true.size(0)
-			right += (predicted == label_true).sum().item()
-
-			if ecgs.shape[0] == 32:
-				ecgs_list.extend(ecgs.numpy())
-				labels_list.extend(label_true.numpy())
-				predicted_list.extend(predicted.numpy())
-
+			if plot_ecg:
+				visualise_ecg(
+					ecg=ecgs.numpy(),
+					labels=labels.numpy(),
+					pred_vec=predicted.numpy(),
