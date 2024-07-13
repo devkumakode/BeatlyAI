b0e0c464982124106311965e5fe1be6d3abb1daa
premkumar <premkumar@velozityglobal.com>
2024-07-13 17:57:21 +0000

    Implement main algorithm for feature selection (lines 0-6)

--- Algorithms/main.py
+++ Algorithms/main.py
@@ -1,1 +1,6 @@
+    print('TEST: ' + str(TEST))
+    print('PATH_TO_TEST_MODEL: '+ str(PATH_TO_TEST_MODEL))
 
+    train_data_path = '/train_set.pkl'
+    val_data_path = '/val_set.pkl'
+    test_data_path = '/test_set.pkl'
