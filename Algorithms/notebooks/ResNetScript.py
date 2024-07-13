fca9c2f876b44fe43a7c201b4468e5c76f078502
mjragul <mjragul@velozityglobal.com>
2024-07-13 12:29:02 +0000

    Improve error handling in notebooks/ResNetScript (lines 0-4)

--- Algorithms/notebooks/ResNetScript.py
+++ Algorithms/notebooks/ResNetScript.py
@@ -1,6 +1,4 @@
-        train_small_path = os.path.join(self.parent_directory, 'data', 'deepfake-ecg-small', str(self.split) + '.csv')
-        self.df = pd.read_csv(train_small_path)  # Skip the header row
-        
-        # Avg RR interval
-        # in milli seconds
-        RR = torch.tensor(self.df['avgrrinterval'].values, dtype=torch.float32)
+        # calculate HR
+        self.y = 60 * 1000/RR
+
+        # Size of the dataset
