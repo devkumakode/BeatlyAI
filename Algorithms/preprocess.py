102bdd93ac8e31b88bc2b7ae71673552b86c079c
aman <aman@velozityglobal.com>
2024-07-13 09:25:00 +0000

    Implement online learning for preprocess (lines 0-12)

--- Algorithms/preprocess.py
+++ Algorithms/preprocess.py
@@ -1,3 +1,12 @@
-			res[ix] = np.array(tmp[ix]).reshape((window, 1))
-	except ValueError as e:
-		print(e)
+
+	return res
+
+
+def split_ecg_label_by_window (ecg, window=200):
+	tmp = [x for x in np.array_split(np.array(ecg), int(len(ecg) / window)) if x.size > 0]
+	res = np.zeros((len(tmp), window,))
+
+	for ix in range(len(tmp)):
+		if len(tmp[ix]) // window:
+			tmp[ix] = tmp[ix][:window]
+		res[ix] = np.array(tmp[ix]).reshape((window,))
