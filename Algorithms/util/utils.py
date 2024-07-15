9fa3333a15fda328477e4ce716354a339face91a
devakumar <devakumar@velozityglobal.com>
2024-07-15 12:53:08 +0000

    Add type hints to util/utils (lines 0-19)

--- Algorithms/util/utils.py
+++ Algorithms/util/utils.py
@@ -1,6 +1,19 @@
-		val=.05,
-		test=.2
-):
-	assert len(X) == len(y), 'Length of X and y must be the same'
-	n = len(X)
-	n_test = int(n * test)
+	n_val = int(n * val)
+	n_train = n - (n_test + n_val)
+
+	idx = list(range(n))
+	random.shuffle(idx)
+
+	train_idx = idx[:n_train]
+	val_idx = idx[n_train:(n_train + n_val)]
+	test_idx = idx[(n_train + n_val):]
+
+	train_set = np.array([X[ix] for ix in train_idx]), np.array([y[ix] for ix in train_idx])
+	val_set = np.array([X[ix] for ix in val_idx]), np.array([y[ix] for ix in val_idx])
+	test_set = np.array([X[ix] for ix in test_idx]), np.array([y[ix] for ix in test_idx])
+
+	return train_set, val_set, test_set
+
+
+def restore_net(ckpt):
+	with open(ckpt, 'rb') as f:
