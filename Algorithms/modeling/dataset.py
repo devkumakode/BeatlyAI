a2a10be2774076d8d68e7c7feae187680f8a2cf5
mjragul <mjragul@velozityglobal.com>
2024-07-15 13:06:17 +0000

    Implement dimension reduction technique in modeling/dataset (lines 0-5)

--- Algorithms/modeling/dataset.py
+++ Algorithms/modeling/dataset.py
@@ -1,14 +1,5 @@
-class PyTorchMinMaxScalerVectorized(object):
 
-	def __init__(self, fitted_min_max_scaler: MinMaxScaler):
-		self.fitted_min_max_scaler: MinMaxScaler = fitted_min_max_scaler
-
-	def __call__(self, tensor):
-		return torch.tensor(self.fitted_min_max_scaler.transform(tensor.numpy()))
-
-
-def fit_min_max_scaler (path, type='stad'):
-	with open(path, 'rb') as f:
-		x, y = pickle.load(f)
-
-		reshaped_x = x.reshape((x.shape[0] * x.shape[1], x.shape[2]))
+		if type == 'stad':
+			scaler = StandardScaler()
+			scaler.fit(reshaped_x)
+		elif type == 'norm':
