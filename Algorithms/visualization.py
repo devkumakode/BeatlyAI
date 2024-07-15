318b1c473298fb8786c8389eb7672b7021e6bc1b
mjragul <mjragul@velozityglobal.com>
2024-07-15 14:37:19 +0000

    Optimize visualization for faster convergence (lines 0-18)

--- Algorithms/visualization.py
+++ Algorithms/visualization.py
@@ -1,20 +1,18 @@
-"""
-# ECG wave segmentation
+and `fs` which is the sampling frequency of the signal.
 
-The goal is to segment different types of waves from the ECG signal. There are 4 types of waves: P-wave,
-QRS-complex, T-wave, extrasystole (parts of the signal are not part of any wave). You should focus on detecting
-boundaries where each wave starts and ends. You are provided with an annotated dataset of ECG signals
-from 3 databases (cardiplus, incartdb, mitdb) in the data directory. There is also split.csv file containing
-the paths to all the files. Each file contains one ECG signal,
-also called a lead, in a JSON format. The structure of the JSON format is as follows:
-```
-{'data': {
-        '<lead_name>': {
-            'ecg': [[]],
-            'label': [[]],
-            'fs': int} },
- 'legend': {0: 'none', 1: "p_wave", 2: "qrs", 3: "t_wave", 4: "extrasystole"}
-```
-The format is designed for multiple leads and multiple signals per lead, however, in this case, each file contains
-exactly one lead with one signal. For each lead there is `ecg` with the ecg signal, `labels` with list
-of integers [0-4] of the same length as the ecg signal (category of each point of the signal),
+
+You are free to use the language of your choice or any other tool you would like. You are provided with two
+Python functions for loading and visualization of ECG signal and the corresponding wave annotation.
+
+You are not expected to provide the perfect solution. What counts the most is your approach and design choices.
+Working pipeline with a poor performance is completely sufficient, in fact, you can choose to only describe
+your approach. You should think of model/algorithm suitable for this task, any pre/post-processing, evaluation
+metric. You should spend no more than 2 days on this task. For a better understanding of this task check out our blog
+post: https://medical.powerful.digital/deep-learning-for-ecg-interpretation-b3ce1d094f5e
+"""
+
+
+import os
+import json
+import numpy as np
+import pandas as pd
