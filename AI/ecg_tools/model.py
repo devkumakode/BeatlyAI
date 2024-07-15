9f122eabdd37e3db2031eb97cdeb8fe90a79c56a
naveenkurra <naveenkurra@velozityglobal.com>
2024-07-15 15:43:15 +0000

    Implement transfer learning for ecg_tools/model (lines 0-20)

--- AI/ecg_tools/model.py
+++ AI/ecg_tools/model.py
@@ -1,12 +1,20 @@
-            nn.GELU(),
-            nn.Linear(input_channels * expansion, input_channels)
-        ])
 
 
-class ResidualAdd(torch.nn.Module):
-    def __init__(self, block):
+class MultiHeadAttention(torch.nn.Module):
+    def __init__(self, embed_size, num_heads, attention_store=None):
         super().__init__()
-        self.block = block
+        self.queries_projection = nn.Linear(embed_size, embed_size)
+        self.values_projection = nn.Linear(embed_size, embed_size)
+        self.keys_projection = nn.Linear(embed_size, embed_size)
+        self.final_projection = nn.Linear(embed_size, embed_size)
+        self.embed_size = embed_size
+        self.num_heads = num_heads
 
     def forward(self, x):
-        return x + self.block(x)
+        assert len(x.shape) == 3
+        keys = self.keys_projection(x)
+        values = self.values_projection(x)
+        queries = self.queries_projection(x)
+        keys = einops.rearrange(keys, "b n (h e) -> b n h e", h=self.num_heads)
+        queries = einops.rearrange(queries, "b n (h e) -> b n h e", h=self.num_heads)
+        values = einops.rearrange(values, "b n (h e) -> b n h e", h=self.num_heads)
