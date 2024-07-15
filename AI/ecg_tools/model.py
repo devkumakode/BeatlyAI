b95d510573c93087f756f2a9dd81e05e74dcd639
sakshamdrivedi <sakshamdrivedi@velozityglobal.com>
2024-07-15 11:55:33 +0000

    Add support for model compression in ecg_tools/model (lines 0-19)

--- AI/ecg_tools/model.py
+++ AI/ecg_tools/model.py
@@ -1,20 +1,19 @@
+        energy_term = torch.einsum("bqhe, bkhe -> bqhk", queries, keys)
+        divider = sqrt(self.embed_size)
+        mh_out = torch.softmax(energy_term, -1)
+        out = torch.einsum('bihv, bvhd -> bihd ', mh_out / divider, values)
+        out = einops.rearrange(out, "b n h e -> b n (h e)")
+        return self.final_projection(out)
 
 
-class MultiHeadAttention(torch.nn.Module):
-    def __init__(self, embed_size, num_heads, attention_store=None):
-        super().__init__()
-        self.queries_projection = nn.Linear(embed_size, embed_size)
-        self.values_projection = nn.Linear(embed_size, embed_size)
-        self.keys_projection = nn.Linear(embed_size, embed_size)
-        self.final_projection = nn.Linear(embed_size, embed_size)
-        self.embed_size = embed_size
-        self.num_heads = num_heads
-
-    def forward(self, x):
-        assert len(x.shape) == 3
-        keys = self.keys_projection(x)
-        values = self.values_projection(x)
-        queries = self.queries_projection(x)
-        keys = einops.rearrange(keys, "b n (h e) -> b n h e", h=self.num_heads)
-        queries = einops.rearrange(queries, "b n (h e) -> b n h e", h=self.num_heads)
-        values = einops.rearrange(values, "b n (h e) -> b n h e", h=self.num_heads)
+class TransformerEncoderLayer(torch.nn.Sequential):
+    def __init__(self, embed_size=768, expansion=4, num_heads=8, dropout=0.1):
+        super(TransformerEncoderLayer, self).__init__(
+            *[
+                ResidualAdd(nn.Sequential(*[
+                    nn.LayerNorm(embed_size),
+                    MultiHeadAttention(embed_size, num_heads),
+                    nn.Dropout(dropout)
+                ])),
+                ResidualAdd(nn.Sequential(*[
+                    nn.LayerNorm(embed_size),
