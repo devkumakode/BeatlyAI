112df0460caa48ab64b1d2480243dca0f51a13f1
devganesh <devganesh@velozityglobal.com>
2024-07-15 13:30:53 +0000

    Refactor preprocess for better code organization (lines 0-3)

--- Algorithms/preprocess.py
+++ Algorithms/preprocess.py
@@ -1,12 +1,3 @@
 
 	return res
 
-
-def split_ecg_label_by_window (ecg, window=200):
-	tmp = [x for x in np.array_split(np.array(ecg), int(len(ecg) / window)) if x.size > 0]
-	res = np.zeros((len(tmp), window,))
-
-	for ix in range(len(tmp)):
-		if len(tmp[ix]) // window:
-			tmp[ix] = tmp[ix][:window]
-		res[ix] = np.array(tmp[ix]).reshape((window,))
