0c6c4941a99d6909989462f138131e43b8e483c2
premkumar <premkumar@velozityglobal.com>
2024-07-16 12:08:53 +0000

    Implement engine/test algorithm for feature selection (lines 0-19)

--- Algorithms/engine/test.py
+++ Algorithms/engine/test.py
@@ -1,9 +1,19 @@
-					plot_window=plot_ecg_windows_size,
-					max_plots=3
-				)
+def eval (ecgs, y_true, y_pred, labels, target_names, plot_acc=False, plot_loss=False, plot_conf_matrix=False, plot_ecg=False, plot_ecg_windows_size=None):
 
-		print("{} ACC: {:.4f}".format('testing', right / total))
+	if plot_acc:
+		with open(config.RESOURCES_DIR + '/loss.pkl', 'rb') as f:
+			loss = pickle.load(f)
+			plot_learning_curve(loss, xlabel='Episode', ylabel='Loss')
 
-		return ecgs, labels_list, predicted_list
+	if plot_loss:
+		with open(config.RESOURCES_DIR + '/accuracy.pkl', 'rb') as f:
+			acc = pickle.load(f)
+			plot_learning_curve(acc, xlabel='Episode', ylabel='Accuracy')
 
-
+	# if plot_ecg:
+	# 	visualise_ecg(
+	# 		ecg=ecgs.numpy(),
+	# 		labels=y_true.numpy(),
+	# 		pred_vec=y_pred.numpy(),
+	# 		plot_window=plot_ecg_windows_size,
+	# 		max_plots=3
