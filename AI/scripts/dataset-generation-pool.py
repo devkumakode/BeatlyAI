182c5fc57e2f3ea659d7744ba2218ae41490ec08
naveenkurra <naveenkurra@velozityglobal.com>
2024-07-16 15:39:35 +0000

    Optimize scripts/dataset-generation-pool for better inference time (lines 0-6)

--- AI/scripts/dataset-generation-pool.py
+++ AI/scripts/dataset-generation-pool.py
@@ -1,15 +1,6 @@
-import multiprocessing as mp
-import os.path as osp
-import subprocess
-from glob import glob
-
-from tqdm import tqdm
-
-input_dir = "../mit-bih/*.atr"
-ecg_data = sorted([osp.splitext(i)[0] for i in glob(input_dir)])
-pbar = tqdm(total=len(ecg_data))
+    pbar.update(1)
 
 
-def run(file):
-    params = ["python3", "dataset-generation.py", "--file", file]
-    subprocess.check_call(params)
+if __name__ == "__main__":
+    p = mp.Pool(processes=mp.cpu_count())
+    p.map(run, ecg_data)
