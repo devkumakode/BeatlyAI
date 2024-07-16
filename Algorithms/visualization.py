25d2c8fbd4a562a3c5f1d82012142645685dfeeb
devganesh <devganesh@velozityglobal.com>
2024-07-16 09:53:07 +0000

    Implement incremental learning in visualization (lines 0-17)

--- Algorithms/visualization.py
+++ Algorithms/visualization.py
@@ -1,18 +1,17 @@
-and `fs` which is the sampling frequency of the signal.
+import matplotlib.pyplot as plt
+from matplotlib.ticker import FixedLocator, FixedFormatter
+from typing import Dict
 
 
-You are free to use the language of your choice or any other tool you would like. You are provided with two
-Python functions for loading and visualization of ECG signal and the corresponding wave annotation.
+def load_single_ecg(path: str) -> Dict[str, dict]:
+    """annot format:
+    {'data': {
+        '<lead_name>': {
+            'ecg': [[]],
+            'label': [[]],
+            'fs': int} },
+     'legend': {0: 'none', 1: "p_wave", 2: "qrs", 3: "t_wave", 4: "extrasystole"}"""
+    with open(path) as f:
+        annot = json.load(f)
+    return annot
 
-You are not expected to provide the perfect solution. What counts the most is your approach and design choices.
-Working pipeline with a poor performance is completely sufficient, in fact, you can choose to only describe
-your approach. You should think of model/algorithm suitable for this task, any pre/post-processing, evaluation
-metric. You should spend no more than 2 days on this task. For a better understanding of this task check out our blog
-post: https://medical.powerful.digital/deep-learning-for-ecg-interpretation-b3ce1d094f5e
-"""
-
-
-import os
-import json
-import numpy as np
-import pandas as pd
