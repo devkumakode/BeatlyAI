89c107091818d10158d64db0711006b925e720f3
devakumar <devakumar@velozityglobal.com>
2024-07-16 12:32:39 +0000

    Optimize visualization for better performance (lines 0-16)

--- Algorithms/visualization.py
+++ Algorithms/visualization.py
@@ -1,17 +1,16 @@
-import matplotlib.pyplot as plt
-from matplotlib.ticker import FixedLocator, FixedFormatter
-from typing import Dict
 
+def visualise_ecg(ecg: np.ndarray,
+                  labels: np.ndarray,
+                  pred_vec: np.ndarray = None,
+                  start_offset: int = 0,
+                  end_offest: int = None,
+                  plot_window: int = 300,
+                  max_plots: int = 2) -> None:
+    """
 
-def load_single_ecg(path: str) -> Dict[str, dict]:
-    """annot format:
-    {'data': {
-        '<lead_name>': {
-            'ecg': [[]],
-            'label': [[]],
-            'fs': int} },
-     'legend': {0: 'none', 1: "p_wave", 2: "qrs", 3: "t_wave", 4: "extrasystole"}"""
-    with open(path) as f:
-        annot = json.load(f)
-    return annot
-
+    :param ecg: ecg signal
+    :param labels: array of the same length as ecg, containing labels [0, 1, 2, 3, 4]
+    :param pred_vec: same sa labels, but it is expected to be the model predictions
+    :param start_offset: plot from given time-step (array index)
+    :param end_offest: plot until given time-step (array index)
+    :param plot_window: width of the plot window (array index units)
