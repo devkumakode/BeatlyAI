febc5c74304f84536f424e64ae12fd3396733d32
premkumar <premkumar@velozityglobal.com>
2024-07-16 12:01:52 +0000

    Add feature scaling in engine/train (lines 0-9)

--- Algorithms/engine/train.py
+++ Algorithms/engine/train.py
@@ -1,19 +1,9 @@
-            end = i
-            plt.plot(np.arange(start, end+1), x[start:end+1], cmap[int(y[i])])
-            start = i+1
-    plt.show()
 
+            total = 0.0
+            correct = 0.0
 
-def train(net, train_loader, val_loader, epochs, criterion, optimizer, device, batch_size):
-    loss_values_final = []
-    accuracy_final = []
+            ecgs = samples_batch['ecg']
+            labels = samples_batch['label']
+            target = labels.contiguous().view(-1).long()
 
-    for step in range(epochs):
-        running_loss = 0.0
-        net.train()
-        net.to(device)
-
-        loss_values = []
-        accuracy = []
-
-        for i, samples_batch in enumerate(train_loader):
+            ecgs = ecgs.to(device)
