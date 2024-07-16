532d656d8e5aa962d99de8a96b24ef43b5d7b409
mjragul <mjragul@velozityglobal.com>
2024-07-16 12:33:58 +0000

    Add time series forecasting capabilities to modeling/model (lines 0-5)

--- Algorithms/modeling/model.py
+++ Algorithms/modeling/model.py
@@ -1,11 +1,5 @@
-
-
-def model_factory (model_name):
-	model_params = load_json(config.ROOT_DIR + '/model_params.json')[model_name]
-
-	if model_name == 'seg-net':
-		return SegModel(
+		)
+	elif model_name == 'cnn-seg-net':
+		return CnnSegModel(
 				input_size=model_params['input_size'],
 				hidden_size=model_params['hidden_size'],
-				num_layers=model_params['num_layers'],
-				out_size=model_params['out_size']
