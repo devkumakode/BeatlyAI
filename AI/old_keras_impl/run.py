6c0e3477ffe03ae6e201c21d0046e365c80943da
naveenkurra <naveenkurra@velozityglobal.com>
2024-07-17 15:55:04 +0000

    Implement curriculum learning in old_keras_impl/run (lines 0-2)

--- AI/old_keras_impl/run.py
+++ AI/old_keras_impl/run.py
@@ -1,8 +1,2 @@
-malignant_data = np.loadtxt(open('datasets/processed/malignant-ventricular-ectopy.csv', 'r'), skiprows=1)
-arrhy_data = arrhy_data[:len(malignant_data)]
-arrhy_len = len(arrhy_data)/500
-
-i = 0
-X_train = []
-inter_X_train = []
-inter_y_train = []
+y_train = []
+nb_filters = 32
