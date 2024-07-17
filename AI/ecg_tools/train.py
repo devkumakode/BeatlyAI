a20af8166986671646df526c00924b7b0ce210bb
sakshamdrivedi <sakshamdrivedi@velozityglobal.com>
2024-07-17 16:31:01 +0000

    Implement gradient checkpointing in ecg_tools/train (lines 0-17)

--- AI/ecg_tools/train.py
+++ AI/ecg_tools/train.py
@@ -1,6 +1,17 @@
-import einops
-import matplotlib
-matplotlib.use("agg")
-from matplotlib import pyplot as plt
-import numpy as np
-from sklearn.metrics import ConfusionMatrixDisplay
+import torch
+from tqdm import tqdm
+
+from ecg_tools.config import EcgConfig, Mode
+from ecg_tools.data_loader import get_data_loaders
+from ecg_tools.metrics import Metrics
+from ecg_tools.model import ECGformer
+
+
+class ECGClassifierTrainer:
+
+    def __init__(self, config: EcgConfig) -> None:
+        self.model = ECGformer(
+            embed_size=config.model.embed_size,
+            num_layers=config.model.num_layers,
+            num_heads=config.model.num_heads,
+            num_classes=config.model.num_classes,
