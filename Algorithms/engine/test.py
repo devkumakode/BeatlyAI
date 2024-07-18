268d7d697677029b4cf15e836ec1f85335e517b2
premkumar <premkumar@velozityglobal.com>
2024-07-18 14:47:48 +0000

    Implement boosting algorithm in engine/test (lines 0-12)

--- Algorithms/engine/test.py
+++ Algorithms/engine/test.py
@@ -1,3 +1,12 @@
-	# 	)
+		conf_mat = confusion_matrix.confusion_matrix(y_true=y_true, y_pred=y_pred, labels=labels)
 
-	if plot_conf_matrix:
+		plot_confusion_matrix(
+			confusion_matrix=conf_mat,
+			target_names=target_names,
+			title='Confusion matrix',
+			normalize=True
+		)
+
+	report = classification_report(y_true=y_true, y_pred=y_pred, labels=labels, target_names=target_names)
+
+	print('Report: ' + str(report))
