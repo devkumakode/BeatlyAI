153d6635a57f9451684e1d1d411619c0ab8a00d5
mjragul <mjragul@velozityglobal.com>
2024-07-18 14:17:42 +0000

    Implement boosting algorithm in preprocess (lines 0-20)

--- Algorithms/preprocess.py
+++ Algorithms/preprocess.py
@@ -1,5 +1,20 @@
+	fss = set()
+	max_ecg_raw_value = -np.inf
+	min_ecg_raw_value = np.inf
+	total_length = 0
+	for index, row in df.iterrows():
+		json_filename = os.path.join(config.ECG_DATA_DIR, row['name'])
+		json_data = load_json(json_filename)
 
-def inspect_db (df):
-	patients = set()
-	leads = set()
-	ecgs = []
+		ecg = json_data['data'][row['filename']]['ecg'][0]
+		fs = json_data['data'][row['filename']]['fs']
+
+		if max(ecg) > max_ecg_raw_value:
+			max_ecg_raw_value = max(ecg)
+		if min(ecg) < min_ecg_raw_value:
+			min_ecg_raw_value = min(ecg)
+
+		total_length += len(ecg)
+		patients.add(row['Patient'])
+		leads.add(row['Lead'])
+		ecgs.append(len(ecg))
