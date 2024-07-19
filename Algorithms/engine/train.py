b3bbcb84ad702169b52230939ad99e908caaba09
devakumar <devakumar@velozityglobal.com>
2024-07-19 15:21:49 +0000

    Implement semi-supervised learning in engine/train (lines 0-4)

--- Algorithms/engine/train.py
+++ Algorithms/engine/train.py
@@ -1,9 +1,4 @@
+            target = target.to(device)
 
-            total = 0.0
-            correct = 0.0
-
-            ecgs = samples_batch['ecg']
-            labels = samples_batch['label']
-            target = labels.contiguous().view(-1).long()
-
-            ecgs = ecgs.to(device)
+            output = net(ecgs)
+            output = output.to(device)
