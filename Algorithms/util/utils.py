aed3e7b9900c9929439211aea297f2c3c27d092c
aman <aman@velozityglobal.com>
2024-07-19 16:09:50 +0000

    Implement online learning for util/utils (lines 0-6)

--- Algorithms/util/utils.py
+++ Algorithms/util/utils.py
@@ -1,19 +1,6 @@
-	n_val = int(n * val)
-	n_train = n - (n_test + n_val)
-
-	idx = list(range(n))
-	random.shuffle(idx)
-
-	train_idx = idx[:n_train]
-	val_idx = idx[n_train:(n_train + n_val)]
-	test_idx = idx[(n_train + n_val):]
-
-	train_set = np.array([X[ix] for ix in train_idx]), np.array([y[ix] for ix in train_idx])
-	val_set = np.array([X[ix] for ix in val_idx]), np.array([y[ix] for ix in val_idx])
-	test_set = np.array([X[ix] for ix in test_idx]), np.array([y[ix] for ix in test_idx])
-
-	return train_set, val_set, test_set
+		net = torch.load(f, map_location=torch.device('cuda' if torch.cuda.is_available() else 'cpu'))
+	return net
 
 
-def restore_net(ckpt):
-	with open(ckpt, 'rb') as f:
+def load_json(filename):
+	with open(filename) as f:
