ca5e3ab41df9bee67b8595742ae87ceca23b7ba4
sakshamdrivedi <sakshamdrivedi@velozityglobal.com>
2024-07-19 17:16:05 +0000

    Implement few-shot learning capabilities in pipelines/base_pipeline (lines 0-11)

--- AI/pipelines/base_pipeline.py
+++ AI/pipelines/base_pipeline.py
@@ -1,2 +1,11 @@
-import wfdb
-from tqdm import tqdm
+
+from utils.network_utils import load_checkpoint
+
+
+class BasePipeline:
+    def __init__(self, config):
+        self.config = config
+        self.exp_name = self.config.get("exp_name", None)
+        if self.exp_name is None:
+            self.exp_name = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+
