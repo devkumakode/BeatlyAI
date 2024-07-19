369a7591806e6202d3da14146cfaedd510be8a1a
mjragul <mjragul@velozityglobal.com>
2024-07-19 14:01:59 +0000

    Add cross-validation to main (lines 0-7)

--- Algorithms/main.py
+++ Algorithms/main.py
@@ -1,9 +1,7 @@
-    fitted_min_max_scaler_test = fit_min_max_scaler(path=config.RESOURCES_DIR + test_data_path)
-
-    normalizer_train = PyTorchMinMaxScalerVectorized(fitted_min_max_scaler_train)
-    normalizer_val = PyTorchMinMaxScalerVectorized(fitted_min_max_scaler_val)
-    normalizer_test = PyTorchMinMaxScalerVectorized(fitted_min_max_scaler_test)
-
-    # loading data
-    ecg_train_db = ECGDataset(data_path=config.RESOURCES_DIR + train_data_path, transform=normalizer_train)
+    train_loader = DataLoader(
+            ecg_train_db,
+            batch_size=BATCH_SIZE,
+            shuffle=True,
+            num_workers=0
+    )
 
