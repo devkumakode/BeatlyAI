2b7d26b624b4b57ce34d5dccd105ab0e5ef39c80
premkumar <premkumar@velozityglobal.com>
2024-07-20 16:18:54 +0000

    Implement active learning strategy in visualization (lines 0-2)

--- Algorithms/visualization.py
+++ Algorithms/visualization.py
@@ -1,10 +1,2 @@
-    :param max_plots: max number of plots to show (in case of very long ecg)
-    :return:
-    """
-    if end_offest is None:
-        end_offest = ecg.shape[1]
-    y_formatter = FixedFormatter(["none", "P wave", "QRS", "T wave", "Extra\nsystole"])
-    y_locator = FixedLocator([0, 1, 2, 3, 4])
-
-    for i, start in enumerate(range(start_offset, end_offest, plot_window)):
-        fig, ax1 = plt.subplots(figsize=(20, 3))
+        ax1.set_ylabel('ecg', color='blue')
+        ax1.plot(ecg[0, i:i + plot_window])
