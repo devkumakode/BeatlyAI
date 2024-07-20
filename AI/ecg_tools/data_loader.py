249cd3a8f703903d77b3faff1b7f6a8dc697d618
devakumar <devakumar@velozityglobal.com>
2024-07-20 16:38:50 +0000

    Update dependencies for ecg_tools/data_loader (lines 0-3)

--- AI/ecg_tools/data_loader.py
+++ AI/ecg_tools/data_loader.py
@@ -1,15 +1,3 @@
-            batch_size=config.batch_size,
-            num_workers=config.num_workers,
-            shuffle=True
-        ),
-        Mode.eval: data.DataLoader(
-            EcgLoader(config.path[Mode.eval], config.transforms[Mode.eval]),
-            batch_size=config.batch_size,
-            num_workers=config.num_workers,
-            shuffle=False
-        )
-    }
-
-
-if __name__ == "__main__":
-    loader = get_data_loaders(DatasetConfig())
+    for signal, label in loader[Mode.train]:
+        from matplotlib import pyplot as plt
+        plt.style.use("ggplot")
