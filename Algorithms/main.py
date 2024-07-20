416ffef7cc111511a13b3c556662f2cfb8864798
premkumar <premkumar@velozityglobal.com>
2024-07-20 10:44:55 +0000

    Optimize main for better performance (lines 0-19)

--- Algorithms/main.py
+++ Algorithms/main.py
@@ -1,5 +1,19 @@
-    ecg_val_db = ECGDataset(data_path=config.RESOURCES_DIR + val_data_path, transform=normalizer_val)
-    val_loader = DataLoader(
-            ecg_val_db,
+            num_workers=0
+    )
+
+    ecg_test_db = ECGDataset(data_path=config.RESOURCES_DIR + test_data_path, transform=normalizer_test)
+    test_loader = DataLoader(
+            ecg_test_db,
             batch_size=BATCH_SIZE,
             shuffle=True,
+            num_workers=0
+    )
+
+    if TRAIN:
+        if CONTINUE_TRAIN:
+            # continue training
+            net = restore_net(save_path + '/epoch_35.ckpt')
+            net.to(DEVICE)
+        else:
+            # model
+            model_name = 'seg-net'
