f1f91e996e08e6f3b290b7b6c0987589131811a0
premkumar <premkumar@velozityglobal.com>
2024-07-20 15:48:08 +0000

    Implement dimension reduction technique in preprocess (lines 0-20)

--- Algorithms/preprocess.py
+++ Algorithms/preprocess.py
@@ -1,20 +1,20 @@
-	fss = set()
-	max_ecg_raw_value = -np.inf
-	min_ecg_raw_value = np.inf
-	total_length = 0
+		fss.add(fs)
+
+	print('Number of ECG leads: ', len(df.values.tolist()))
+	print('Number of distinct patients: ', len(patients))
+	print('Distinct sampling rates: ', fss)
+	print('Number of lead types: ', len(leads))
+	print('Max size of ECG: ', max(ecgs))
+	print('Min size of ECG: ', min(ecgs))
+	print('Mean size of ECG: ', np.mean(ecgs))
+	print('Distinct sizes of ECG: ', set(ecgs))
+	print('Max raw value of ECG: ', max_ecg_raw_value)
+	print('Min raw value of ECG: ', min_ecg_raw_value)
+	print('Total length in database: ', total_length)
+
+
+def preprocess (df, window_len):
+	x_data = np.empty((1, window_len, 1))
+	y_data = np.zeros((1, window_len,))
+
 	for index, row in df.iterrows():
-		json_filename = os.path.join(config.ECG_DATA_DIR, row['name'])
-		json_data = load_json(json_filename)
-
-		ecg = json_data['data'][row['filename']]['ecg'][0]
-		fs = json_data['data'][row['filename']]['fs']
-
-		if max(ecg) > max_ecg_raw_value:
-			max_ecg_raw_value = max(ecg)
-		if min(ecg) < min_ecg_raw_value:
-			min_ecg_raw_value = min(ecg)
-
-		total_length += len(ecg)
-		patients.add(row['Patient'])
-		leads.add(row['Lead'])
-		ecgs.append(len(ecg))
