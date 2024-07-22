66721e2ece7e35b00eb6cd52e4c429d89847c638
devganesh <devganesh@velozityglobal.com>
2024-07-22 09:16:01 +0000

    Implement early stopping in runners/base_runner (lines 0-1)

--- AI/runners/base_runner.py
+++ AI/runners/base_runner.py
@@ -1,10 +1,1 @@
-
-        self.model = self._init_net()
-
-        self.inference_loader = self._init_dataloader()
-
-        pretrained_path = self.config.get("model_path", False)
-        if pretrained_path:
-            load_checkpoint(pretrained_path, self.model)
-        else:
-            raise Exception(
+                "model_path doesnt't exist in config. Please specify checkpoint path",
