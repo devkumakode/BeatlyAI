386c0dce3d8d9e66125d7c276015049cb6064a6e
devganesh <devganesh@velozityglobal.com>
2024-07-22 10:04:08 +0000

    Implement online learning for engine/train (lines 0-17)

--- Algorithms/engine/train.py
+++ Algorithms/engine/train.py
@@ -1,4 +1,17 @@
-            target = target.to(device)
+            loss = criterion(output, target)
 
-            output = net(ecgs)
-            output = output.to(device)
+            _, predicted = torch.max(output.data, 1)
+
+            predicted = predicted.to(device)
+
+            total += target.size(0)
+            correct += (predicted == target).sum().item()
+
+            loss_item = loss.item()
+
+            running_loss += loss_item
+            loss_values.append(running_loss)
+
+            accuracy.append(correct / total)
+
+            if (i+1) % 20 == 0:
