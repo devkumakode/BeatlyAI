119c31d05360a56eb2a9633609b072ab04dc117c
maheshb <maheshb@velozityglobal.com>
2024-07-23 11:29:48 +0000

    Add logging to ecg/network (lines 0-2)

--- AI/ecg/network.py
+++ AI/ecg/network.py
@@ -1,17 +1,2 @@
-    from keras.layers import Activation
-    layer = BatchNormalization()(layer)
-    layer = Activation(params["conv_activation"])(layer)
-
-    if dropout > 0:
-        from keras.layers import Dropout
-        layer = Dropout(params["conv_dropout"])(layer)
-
-    return layer
-
-def add_conv_weight(
-        layer,
-        filter_length,
-        num_filters,
-        subsample_length=1,
-        **params):
-    from keras.layers import Conv1D 
+    layer = Conv1D(
+        filters=num_filters,
