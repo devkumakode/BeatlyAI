4d3c521b2639c2102f2a5d3d006643b8ba6cc13e
premkumar <premkumar@velozityglobal.com>
2024-07-23 14:25:33 +0000

    Add regularization to util/utils to prevent overfitting (lines 0-9)

--- Algorithms/util/utils.py
+++ Algorithms/util/utils.py
@@ -1,20 +1,9 @@
-	y = y[start:end]
-	cmap = ['k', 'r', 'g', 'b']
-	start = end = 0
-	for i in range(len(y) - 1):
-		if y[i] != y[i + 1]:
-			end = i
-			plt.plot(np.arange(start, end + 1), x[start:end + 1], cmap[int(y[i])])
-			start = i + 1
+
+	plt.tick_params('y', labelsize=20)
+	plt.tick_params('x', labelsize=20)
+
+	plt.xlabel(xlabel, size=20)
+	plt.ylabel(ylabel, size=20)
+
+	plt.legend(loc='best', prop={'size': 15})
 	plt.show()
-
-
-def plot_rewards_with_std(reward, std_reward_plus, std_reward_minus, xlabel, ylabel):
-	x = [i for i in range(len(reward))]
-	y = reward
-
-	avg_color = 'black'
-	std_color = '#DDDDDD'
-
-	plt.plot(x, y, color=avg_color, linewidth=1.5, label='mean')
-	plt.fill_between(x, std_reward_plus, std_reward_minus, color=std_color, label='std')
