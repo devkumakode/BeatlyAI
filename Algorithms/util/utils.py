ffe22109ed3353e17774921bb618841b4eba00e4
mjragul <mjragul@velozityglobal.com>
2024-07-23 15:22:04 +0000

    Implement boosting algorithm in util/utils (lines 0-20)

--- Algorithms/util/utils.py
+++ Algorithms/util/utils.py
@@ -1,14 +1,20 @@
-		data = json.load(f)
-		f.close()
+	y = y[start:end]
+	cmap = ['k', 'r', 'g', 'b']
+	start = end = 0
+	for i in range(len(y) - 1):
+		if y[i] != y[i + 1]:
+			end = i
+			plt.plot(np.arange(start, end + 1), x[start:end + 1], cmap[int(y[i])])
+			start = i + 1
+	plt.show()
 
-		return data
 
+def plot_rewards_with_std(reward, std_reward_plus, std_reward_minus, xlabel, ylabel):
+	x = [i for i in range(len(reward))]
+	y = reward
 
-def save_as_pkl(filename_path, data):
-	with open(filename_path, 'wb') as f:
-		pickle.dump(data, f)
-		f.close()
+	avg_color = 'black'
+	std_color = '#DDDDDD'
 
-
-def plotecg(x, y, start, end):
-	x = x[start:end, 0]
+	plt.plot(x, y, color=avg_color, linewidth=1.5, label='mean')
+	plt.fill_between(x, std_reward_plus, std_reward_minus, color=std_color, label='std')
