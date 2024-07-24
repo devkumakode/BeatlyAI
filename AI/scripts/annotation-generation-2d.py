d8bc76018572dc4fdfe8dfd05d6d69a46cc2e266
sakshamdrivedi <sakshamdrivedi@velozityglobal.com>
2024-07-24 10:03:31 +0000

    Implement few-shot learning capabilities in scripts/annotation-generation-2d (lines 0-8)

--- AI/scripts/annotation-generation-2d.py
+++ AI/scripts/annotation-generation-2d.py
@@ -1,19 +1,8 @@
-import json
-import os.path as osp
-from glob import glob
+val_size = 0.1  # [0, 1]
 
-import pandas as pd
+output_path = "/".join(data_path.split("/")[:-5])
+random_state = 7
 
-# 1. N - Normal
-# 2. V - PVC (Premature ventricular contraction)
-# 3. \ - PAB (Paced beat)
-# 4. R - RBB (Right bundle branch)
-# 5. L - LBB (Left bundle branch)
-# 6. A - APB (Atrial premature beat)
-# 7. ! - AFW (Ventricular flutter wave)
-# 8. E - VEB (Ventricular escape beat)
-
-classes = ["N", "V", "\\", "R", "L", "A", "!", "E"]
-lead = "MLII"
-extension = "png"  # or `npy` for 1D
-data_path = osp.abspath("../data/*/*/*/*/*.{}".format(extension))
+if __name__ == "__main__":
+    dataset = []
+    files = glob(data_path)
