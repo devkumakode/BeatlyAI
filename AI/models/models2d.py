b8830eb88882b0d0ef2c70f926a1a993cc9ed8a1
naveenkurra <naveenkurra@velozityglobal.com>
2024-07-25 14:20:58 +0000

    Add support for quantization in models/models2d (lines 0-17)

--- AI/models/models2d.py
+++ AI/models/models2d.py
@@ -1,20 +1,17 @@
-            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
-            nn.ELU(inplace=True),
-            nn.BatchNorm2d(128, eps=0.001),
-            nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1),
-            nn.ELU(inplace=True),
-            nn.BatchNorm2d(128, eps=0.001),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
-            nn.ELU(inplace=True),
-            nn.BatchNorm2d(256, eps=0.001),
-            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
-            nn.ELU(inplace=True),
-            nn.BatchNorm2d(256, eps=0.001),
-            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Dropout(0.5),
+            nn.Linear(2048, num_classes),
         )
 
-        self.classifier = nn.Sequential(
-            nn.Linear(16 * 16 * 256, 2048),
-            nn.ELU(inplace=True),
-            nn.BatchNorm1d(2048, eps=0.001),
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), 16 * 16 * 256)
+        x = self.classifier(x)
+        return x
+
+
+class MobileNetV2(models.MobileNetV2):
+    def __init__(self, num_classes=8):
+        super().__init__(num_classes=num_classes)
+
+
+class AlexNet(models.AlexNet):
