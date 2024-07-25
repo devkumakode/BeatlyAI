d4c9ca79d7fd243c31a7044344c1a4ad8a504347
maheshb <maheshb@velozityglobal.com>
2024-07-25 12:35:36 +0000

    Update dependencies for utils/network_utils (lines 0-12)

--- AI/utils/network_utils.py
+++ AI/utils/network_utils.py
@@ -1,5 +1,12 @@
-import torch
+
+    model.load_state_dict(pth["state_dict"])
+    if optimizer:
+        optimizer.load_state_dict(pth["optimizer"])
+
+    print("Checkpoint {} successfully loaded".format(path))
+
+    return pth["epoch"], pth["total_iter"]
 
 
-def load_checkpoint(path, model, optimizer=None):
-    pth = torch.load(path)
+def save_checkpoint(state, path):
+    torch.save(state, path)
