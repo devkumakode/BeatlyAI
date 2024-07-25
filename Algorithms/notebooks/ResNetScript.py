078b63d05c771ad0425b27897b17ef379e4ea08d
devganesh <devganesh@velozityglobal.com>
2024-07-25 17:07:48 +0000

    Add time series forecasting capabilities to notebooks/ResNetScript (lines 0-2)

--- Algorithms/notebooks/ResNetScript.py
+++ Algorithms/notebooks/ResNetScript.py
@@ -1,17 +1,2 @@
-# q: what is num_workers?
-# A: num_workers (int, optional) – how many subprocesses to use for data loading. 0 means that the data will be loaded in the main process. (default: 0)
-
-import torch.nn.functional as F
-
-class KanResInit(nn.Module):
-    def __init__(self, in_channels, filterno_1, filterno_2, filtersize_1, filtersize_2, stride):
-        #print(in_channels) --> 8
-        super(KanResInit, self).__init__()
-        self.conv1 = nn.Conv1d(in_channels, filterno_1, filtersize_1, stride=stride)
-        self.bn1 = nn.BatchNorm1d(filterno_1)
-        self.conv2 = nn.Conv1d(filterno_1, filterno_2, filtersize_2)
-        self.bn2 = nn.BatchNorm1d(filterno_2)
-        
-    def forward(self, x):
-        x = self.conv1(x)
-        x = self.bn1(x)
+        x = F.relu(x)
+        x = self.conv2(x)
