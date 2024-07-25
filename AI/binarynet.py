b1a8241f709bfe158731bcbeca4bb669ce3de94e
devganesh <devganesh@velozityglobal.com>
2024-07-25 14:02:44 +0000

    Implement gradient checkpointing in binarynet (lines 0-15)

--- AI/binarynet.py
+++ AI/binarynet.py
@@ -1,2 +1,15 @@
+    """Construct a look-up-table to store bitwise values of all intergers given a bitwidth."""
+    bit_table = -torch.ones((2**bit, bit), dtype=torch.int8)
+    for i in range(1,2**bit):
+        for j in range(bit):
+            if (i & (1<<j)):
+                bit_table[i,j] = 1
+    return bit_table.to('cuda')
 
-def construct_bit_table(bit):
+
+def binarize(input_t): 
+    """Binarize input tensor."""
+    dim = input_t.nelement()
+    output_t = torch.ones(dim)
+    output_t[input_t<0] = -1
+    return output_t
