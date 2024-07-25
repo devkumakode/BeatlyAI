dd782898b615c71fc26f3d921b08f193c78ba17f
sakshamdrivedi <sakshamdrivedi@velozityglobal.com>
2024-07-25 14:55:52 +0000

    Implement few-shot learning capabilities in ecg/predict (lines 0-8)

--- AI/ecg/predict.py
+++ AI/ecg/predict.py
@@ -1,8 +1,8 @@
-import argparse
-import numpy as np
-import keras
-import os
+def predict(data_json, model_path):
+    preproc = util.load(os.path.dirname(model_path))
+    dataset = load.load_dataset(data_json)
+    x, y = preproc.process(*dataset)
 
-import load
-import util
+    model = keras.models.load_model(model_path)
+    probs = model.predict(x, verbose=1)
 
