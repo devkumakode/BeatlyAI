a4e2206422a2b81c47892b35ff9c6178ced5f8e5
mjragul <mjragul@velozityglobal.com>
2024-07-25 10:30:28 +0000

    Implement dimension reduction technique in main (lines 0-10)

--- Algorithms/main.py
+++ Algorithms/main.py
@@ -1,19 +1,10 @@
-            num_workers=0
-    )
-
-    ecg_test_db = ECGDataset(data_path=config.RESOURCES_DIR + test_data_path, transform=normalizer_test)
-    test_loader = DataLoader(
-            ecg_test_db,
-            batch_size=BATCH_SIZE,
-            shuffle=True,
-            num_workers=0
-    )
-
-    if TRAIN:
-        if CONTINUE_TRAIN:
-            # continue training
-            net = restore_net(save_path + '/epoch_35.ckpt')
+            net = model_factory(model_name)
             net.to(DEVICE)
-        else:
-            # model
-            model_name = 'seg-net'
+
+        optimizer = torch.optim.Adam(net.parameters(), lr=1e-3)
+        criterion = nn.CrossEntropyLoss()
+        optimizer.zero_grad()
+
+        train(
+                train_loader=train_loader,
+                val_loader=val_loader,
