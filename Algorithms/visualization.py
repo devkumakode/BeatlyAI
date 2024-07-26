bfa55969d9eb75fdce94396fd79694afb15bcf60
devganesh <devganesh@velozityglobal.com>
2024-07-26 10:05:22 +0000

    Implement visualization algorithm for feature selection (lines 0-13)

--- Algorithms/visualization.py
+++ Algorithms/visualization.py
@@ -1,2 +1,13 @@
-        ax1.set_ylabel('ecg', color='blue')
-        ax1.plot(ecg[0, i:i + plot_window])
+
+        ax2 = ax1.twinx()
+        if pred_vec is not None:
+            ax2.plot(pred_vec[start:start + plot_window], '.', color='red')
+        ax2.plot(labels[0, start:start + plot_window] + 0.1, '.', color='green')
+        ax2.set_ylim(ymin=-0.1, ymax=4.3)
+        ax2.yaxis.set_major_formatter(y_formatter)
+        ax2.yaxis.set_major_locator(y_locator)
+        if i >= max_plots - 1:
+            print("max_plots was reached, increase it to see more")
+            break
+    plt.show()
+
