b7031012d97876c76542d3066f014ea7a4c20148
devganesh <devganesh@velozityglobal.com>
2024-07-27 11:58:35 +0000

    Implement continual learning in ECGnet (lines 0-19)

--- AI/ECGnet.py
+++ AI/ECGnet.py
@@ -1,20 +1,19 @@
-
-
-class ECGNet(nn.Module):
-    def __init__(self, in_channels=in_channels_):
-        super(ECGNet, self).__init__()
-        self.features = nn.Sequential(
-            nn.Conv1d(1, 8, 16, stride=2, padding=7),
+            nn.Conv1d(32, 64, 7, stride=1, padding=3),
             nn.ReLU(),
-            # nn.BatchNorm1d(8),
-            nn.MaxPool1d(kernel_size=8, stride=4),
-
-            nn.Conv1d(8, 12, 12, padding=5, stride=2),
-            nn.ReLU(),
-            # nn.BatchNorm1d(16),
             nn.MaxPool1d(4, stride=2),
 
-            nn.Conv1d(12, 32, 9, stride=1, padding=4),
+            nn.Conv1d(64, 64, 5, stride=1, padding=2),
             nn.ReLU(),
-            nn.MaxPool1d(5, stride=2),
+            nn.MaxPool1d(2, 2),
 
+            nn.Conv1d(64, 64, 3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool1d(2, 2),
+
+            nn.Conv1d(64, 72, 3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool1d(2, 2),
+        )
+        self.classifier = torch.nn.Sequential(
+            nn.Linear(in_features=216, out_features=64),
+            nn.ReLU(),
