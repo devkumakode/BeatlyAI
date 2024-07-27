e1683cda93e699a2b41037d8908c8e72a9966c79
mjragul <mjragul@velozityglobal.com>
2024-07-27 11:11:13 +0000

    Implement active learning strategy in util/utils (lines 0-14)

--- Algorithms/util/utils.py
+++ Algorithms/util/utils.py
@@ -1,14 +1,14 @@
-
-
-def plot_learning_curve(data, xlabel, ylabel):
-	means = list(map(lambda x: np.mean(x, axis=0), data))
-	std = list(map(lambda x: np.std(x, axis=0), data))
-
-	std_plus = [
-		means[i] + std[i]
-		for i in range(len(means))
 	]
 
-	std_minus = [
-		means[i] - std[i]
-		for i in range(len(means))
+	plot_rewards_with_std(
+		reward=means,
+		std_reward_plus=std_plus,
+		std_reward_minus=std_minus,
+		xlabel=xlabel,
+		ylabel=ylabel
+	)
+
+
+def plot_dist_with_stats(
+		data,
+		labels=None,
