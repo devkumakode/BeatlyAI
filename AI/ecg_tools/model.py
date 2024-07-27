59056c1c589f73802d09822f06a126bcf4e89471
sakshamdrivedi <sakshamdrivedi@velozityglobal.com>
2024-07-27 13:43:34 +0000

    Add logging to ecg_tools/model (lines 0-20)

--- AI/ecg_tools/model.py
+++ AI/ecg_tools/model.py
@@ -1,19 +1,20 @@
-        energy_term = torch.einsum("bqhe, bkhe -> bqhk", queries, keys)
-        divider = sqrt(self.embed_size)
-        mh_out = torch.softmax(energy_term, -1)
-        out = torch.einsum('bihv, bvhd -> bihd ', mh_out / divider, values)
-        out = einops.rearrange(out, "b n h e -> b n (h e)")
-        return self.final_projection(out)
-
-
-class TransformerEncoderLayer(torch.nn.Sequential):
-    def __init__(self, embed_size=768, expansion=4, num_heads=8, dropout=0.1):
-        super(TransformerEncoderLayer, self).__init__(
-            *[
-                ResidualAdd(nn.Sequential(*[
-                    nn.LayerNorm(embed_size),
-                    MultiHeadAttention(embed_size, num_heads),
+                    MLP(embed_size, expansion),
                     nn.Dropout(dropout)
-                ])),
-                ResidualAdd(nn.Sequential(*[
-                    nn.LayerNorm(embed_size),
+                ]))
+            ]
+        )
+
+
+class Classifier(nn.Sequential):
+    def __init__(self, embed_size, num_classes):
+        super().__init__(*[
+            Reduce("b n e -> b e", reduction="mean"),
+            nn.Linear(embed_size, embed_size),
+            nn.LayerNorm(embed_size),
+            nn.Linear(embed_size, num_classes)
+        ])
+
+
+class ECGformer(nn.Module):
+
+    def __init__(self, num_layers, signal_length, num_classes, input_channels, embed_size, num_heads, expansion) -> None:
