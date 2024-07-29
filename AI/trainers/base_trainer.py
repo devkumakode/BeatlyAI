2cf252fc1b1c9951d8805620b13fe1042fe073a6
maheshb <maheshb@velozityglobal.com>
2024-07-29 16:11:58 +0000

    Add explainability features to trainers/base_trainer (lines 0-3)

--- AI/trainers/base_trainer.py
+++ AI/trainers/base_trainer.py
@@ -1,14 +1,3 @@
-        if self.exp_name is None:
-            self.exp_name = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        self.train_loader, self.val_loader = self._init_dataloaders()
 
-        self.log_dir = osp.join(self.config["exp_dir"], self.exp_name, "logs")
-        self.pth_dir = osp.join(self.config["exp_dir"], self.exp_name, "checkpoints")
-        os.makedirs(self.log_dir, exist_ok=True)
-        os.makedirs(self.pth_dir, exist_ok=True)
-
-        self.writer = SummaryWriter(log_dir=self.log_dir)
-
-        self.model = self._init_net()
-        self.optimizer = self._init_optimizer()
-        self.criterion = nn.CrossEntropyLoss().to(self.config["device"])
-
+        pretrained_path = self.config.get("model_path", False)
