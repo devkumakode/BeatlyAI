8f730411b5987d6beb04d9bb77d9ea2855fd4320
naveenkurra <naveenkurra@velozityglobal.com>
2024-07-29 16:56:59 +0000

    Implement continual learning in ecg/load (lines 0-10)

--- AI/ecg/load.py
+++ AI/ecg/load.py
@@ -1,3 +1,10 @@
-import os
-import random
-import scipy.io as sio
+import tqdm
+
+STEP = 256
+
+def data_generator(batch_size, preproc, x, y):
+    num_examples = len(x)
+    examples = zip(x, y)
+    examples = sorted(examples, key = lambda x: x[0].shape[0])
+    end = num_examples - batch_size + 1
+    batches = [examples[i:i+batch_size]
