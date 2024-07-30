2e0a38905d3c475087f73934ce118258b827ed67
devganesh <devganesh@velozityglobal.com>
2024-07-30 12:08:35 +0000

    Implement ecg_tools/model in neural network architecture (lines 0-6)

--- AI/ecg_tools/model.py
+++ AI/ecg_tools/model.py
@@ -1,16 +1,6 @@
-        super().__init__()
-        self.encoder = nn.ModuleList([TransformerEncoderLayer(
-            embed_size=embed_size, num_heads=num_heads, expansion=expansion) for _ in range(num_layers)])
-        self.classifier = Classifier(embed_size, num_classes)
-        self.positional_encoding = nn.Parameter(torch.randn(signal_length + 1, embed_size))
-        self.embedding = LinearEmbedding(input_channels, embed_size)
-
-    def forward(self, x):
-        embedded = self.embedding(x)
-
-        for layer in self.encoder:
-            embedded = layer(embedded + self.positional_encoding)
-
-        return self.classifier(embedded)
-
-
+if __name__ == "__main__":
+    print(LinearEmbedding(3, 192)(torch.rand(2, 128, 3)).shape)
+    print(MLP(3)(torch.rand(2, 128, 3)).shape)
+    print(TransformerEncoderLayer(192, 8)(torch.rand(2, 128, 192)).shape)
+    print(ECGformer(
+        4, 128, 5, 3, 192, 8, 4
