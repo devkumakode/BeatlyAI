4cc9dc8b885f55ee2d309c241173361ca6da9118
aman <aman@velozityglobal.com>
2024-07-30 14:34:14 +0000

    Add type hints to notebooks/ResNetScript (lines 0-18)

--- Algorithms/notebooks/ResNetScript.py
+++ Algorithms/notebooks/ResNetScript.py
@@ -1,20 +1,18 @@
-    def forward(self, x):
-        identity = x
-        #print(x.shape)      
-        x = self.conv1(x)
-        #print(x.shape)
-        x = self.bn1(x)
-        x = F.relu(x)
-        x = self.conv2(x)
-        #print(x.shape)
-        x = self.bn2(x)
-        x = F.relu(x)
-        x = x + identity
-        return x
 
-class KanResWide_X2(nn.Module):
-    def __init__(self, input_shape, output_size):
-        super(KanResWide_X2, self).__init__()
-
-        #print(input_shape[0])
-        #print(input_shape[1])
+        self.input_shape = input_shape
+        self.output_size = output_size
+        
+        self.init_block = KanResInit(input_shape[0], 64, 64, 8, 3, 1)
+        self.pool = nn.AvgPool1d(kernel_size=2)
+        
+        self.module_blocks = nn.Sequential(
+            KanResModule(64, 64, 64, 50, 50, 1),
+            KanResModule(64, 64, 64, 50, 50, 1),
+            KanResModule(64, 64, 64, 50, 50, 1),
+            KanResModule(64, 64, 64, 50, 50, 1),
+            KanResModule(64, 64, 64, 50, 50, 1),
+            KanResModule(64, 64, 64, 50, 50, 1),
+            KanResModule(64, 64, 64, 50, 50, 1),
+            KanResModule(64, 64, 64, 50, 50, 1)
+        )
+        
