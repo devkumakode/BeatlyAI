d8402ac3efecfd87955f5ec15c322ef7d9b58998
aman <aman@velozityglobal.com>
2024-07-30 17:52:36 +0000

    Implement CI/CD pipeline for main (lines 0-1)

--- Algorithms/main.py
+++ Algorithms/main.py
@@ -1,10 +1,1 @@
-            net = model_factory(model_name)
-            net.to(DEVICE)
-
-        optimizer = torch.optim.Adam(net.parameters(), lr=1e-3)
-        criterion = nn.CrossEntropyLoss()
-        optimizer.zero_grad()
-
-        train(
-                train_loader=train_loader,
-                val_loader=val_loader,
+                net=net,
