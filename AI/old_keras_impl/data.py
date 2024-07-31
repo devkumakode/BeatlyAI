1df8556940eddc91e5723ec2d3d2c1b18c7680cb
devganesh <devganesh@velozityglobal.com>
2024-07-31 11:54:28 +0000

    Implement early stopping in old_keras_impl/data (lines 0-3)

--- AI/old_keras_impl/data.py
+++ AI/old_keras_impl/data.py
@@ -1,11 +1,3 @@
-    listdir = os.listdir('datasets/raw')
-    for d in listdir:
-        with open('datasets/processed/{}.csv'.format(d), 'w') as write_record_file:
-            csvwriter = csv.writer(write_record_file, delimiter=',')
-            csvwriter.writerow(['value'])
-            for record in os.listdir('datasets/raw/{}'.format(d)):
-                if record.endswith('.csv'):
-                    with open('datasets/raw/{}/{}'.format(d,record), 'r') as read_record_file:
-                        reader = csv.reader(read_record_file)
-                        # skip headers
                         reader.next()
+                        for row in reader:
+                            csvwriter.writerow([row[1]])
