3788298ebe3f311ce88e0070e086369d0e1e647e
devakumar <devakumar@velozityglobal.com>
2024-07-31 18:41:15 +0000

    Add beam search for dataloaders/dataset1d decoding (lines 0-10)

--- AI/dataloaders/dataset1d.py
+++ AI/dataloaders/dataset1d.py
@@ -1,13 +1,10 @@
-import json
+        self.mapper = json.load(open(mapping_path))
 
-import numpy as np
-import wfdb
-from scipy.signal import find_peaks
-from sklearn.preprocessing import scale
-from torch.utils.data import DataLoader, Dataset
+    def __getitem__(self, index):
+        img = np.load(self.data[index]["path"]).astype("float32")
+        img = img.reshape(1, img.shape[0])
 
+        return {"image": img, "class": self.mapper[self.data[index]["label"]]}
 
-class EcgDataset1D(Dataset):
-    def __init__(self, ann_path, mapping_path):
-        super().__init__()
-        self.data = json.load(open(ann_path))
+    def get_dataloader(self, num_workers=4, batch_size=16, shuffle=True):
+        data_loader = DataLoader(
