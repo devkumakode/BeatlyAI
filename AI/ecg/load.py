1472e775760499bfb162eca210b7a95b97a80dc4
maheshb <maheshb@velozityglobal.com>
2024-07-31 15:16:10 +0000

    Refactor ecg/load for multi-GPU training (lines 0-9)

--- AI/ecg/load.py
+++ AI/ecg/load.py
@@ -1,10 +1,9 @@
-import tqdm
+                for i in range(0, end, batch_size)]
+    random.shuffle(batches)
+    while True:
+        for batch in batches:
+            x, y = zip(*batch)
+            yield preproc.process(x, y)
 
-STEP = 256
+class Preproc:
 
-def data_generator(batch_size, preproc, x, y):
-    num_examples = len(x)
-    examples = zip(x, y)
-    examples = sorted(examples, key = lambda x: x[0].shape[0])
-    end = num_examples - batch_size + 1
-    batches = [examples[i:i+batch_size]
