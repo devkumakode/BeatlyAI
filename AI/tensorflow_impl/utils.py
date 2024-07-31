952ffc1a9b94ff23353876f2e059a16f35b76dc7
devakumar <devakumar@velozityglobal.com>
2024-07-31 14:36:10 +0000

    Add support for quantization in tensorflow_impl/utils (lines 0-4)

--- AI/tensorflow_impl/utils.py
+++ AI/tensorflow_impl/utils.py
@@ -1,19 +1,4 @@
 
-import tensorflow as tf
-import numpy as np
 
-
-class TensorBoardHandler(object):
-    def __init__(self, logs_path=None):
-        self.logs_path = logs_path if logs_path else "tensorboard_data/others/"
-        self.writer = tf.summary.FileWriter(self.logs_path)
-
-    def add_histograms(self, dct):
-        for k, v in dct.items():
-            tf.summary.histogram(str(k), v)
-
-    def add_scalar(self, name, obj):
-        return tf.summary.scalar(name, obj)
-
-    def merge_all(self):
-        return tf.summary.merge_all()
+class ModelSaver(object):
+    model_ext = ".ckpt"
