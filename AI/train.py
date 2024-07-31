d52f87f7de5f00b5e08d5dcfa76138f46e75c94c
devakumar <devakumar@velozityglobal.com>
2024-07-31 11:49:28 +0000

    Refactor train for multi-GPU training (lines 0-11)

--- AI/train.py
+++ AI/train.py
@@ -1,17 +1,11 @@
-    with torch.no_grad():
-        maxk = max(topk)
-        _, pred = output.topk(maxk, 1, True, True)
-        pred = pred.t()
-        correct = pred.eq(target.view(1, -1).expand_as(pred))
-
-        for (i,k) in enumerate(topk):
-            correct_sum[i] += (correct[:k].contiguous().view(-1).float().sum(0, keepdim=True)).item()
-        return 
-
-
-def get_accuracy(net, train_loader, loss_func):
-    """Get the training loss and training accuracy."""
-    net.eval()
-    with torch.no_grad():
-        train_loss = 0.
-        num_batches = 0
+        correct_sum = [0. for i in range(len(TOPK))]
+        total = 0
+        for (inputs, labels) in train_loader:               
+            inputs, labels = inputs.cuda(non_blocking=True), labels.cuda(non_blocking=True)
+            outputs = net(inputs)
+            loss = loss_func(outputs, labels)
+            accuracy(outputs, labels, correct_sum, topk=TOPK)
+            total += labels.size(0)
+            train_loss += loss.data.item()
+            num_batches += 1
+        print('training loss: ', train_loss/num_batches)
