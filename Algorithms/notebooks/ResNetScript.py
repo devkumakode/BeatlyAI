f35aeda23244a26d85da1b744380beca62fe92f8
devakumar <devakumar@velozityglobal.com>
2024-07-31 10:03:18 +0000

    Implement dimension reduction technique in notebooks/ResNetScript (lines 0-3)

--- Algorithms/notebooks/ResNetScript.py
+++ Algorithms/notebooks/ResNetScript.py
@@ -1,18 +1,3 @@
-
-        self.input_shape = input_shape
-        self.output_size = output_size
-        
-        self.init_block = KanResInit(input_shape[0], 64, 64, 8, 3, 1)
-        self.pool = nn.AvgPool1d(kernel_size=2)
-        
-        self.module_blocks = nn.Sequential(
-            KanResModule(64, 64, 64, 50, 50, 1),
-            KanResModule(64, 64, 64, 50, 50, 1),
-            KanResModule(64, 64, 64, 50, 50, 1),
-            KanResModule(64, 64, 64, 50, 50, 1),
-            KanResModule(64, 64, 64, 50, 50, 1),
-            KanResModule(64, 64, 64, 50, 50, 1),
-            KanResModule(64, 64, 64, 50, 50, 1),
-            KanResModule(64, 64, 64, 50, 50, 1)
-        )
+        self.global_avg_pool = nn.AdaptiveAvgPool1d(1)
+        self.fc = nn.Linear(64, output_size)
         
