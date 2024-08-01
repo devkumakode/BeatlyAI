2800eed223d0166b4fc1987ca95acf73012507d4
devganesh <devganesh@velozityglobal.com>
2024-08-01 18:55:40 +0000

    Add support for mixed precision training in dataloaders/dataset1d (lines 0-4)

--- AI/dataloaders/dataset1d.py
+++ AI/dataloaders/dataset1d.py
@@ -1,10 +1,4 @@
-        self.mapper = json.load(open(mapping_path))
+            self, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers,
+        )
+        return data_loader
 
-    def __getitem__(self, index):
-        img = np.load(self.data[index]["path"]).astype("float32")
-        img = img.reshape(1, img.shape[0])
-
-        return {"image": img, "class": self.mapper[self.data[index]["label"]]}
-
-    def get_dataloader(self, num_workers=4, batch_size=16, shuffle=True):
-        data_loader = DataLoader(
