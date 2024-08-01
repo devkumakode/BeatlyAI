216f72204e162df015d533a7c848e97a070b305a
devakumar <devakumar@velozityglobal.com>
2024-08-01 16:39:13 +0000

    Add support for multi-label classification in util/utils (lines 0-2)

--- Algorithms/util/utils.py
+++ Algorithms/util/utils.py
@@ -1,8 +1,2 @@
-		ax.axvline(std.item(), color='r', linestyle='dashed', linewidth=2)
-		ax.axvline(-std.item(), color='r', linestyle='dashed', linewidth=2)
-
-	ax.set_xlabel("Samples")
-	ax.set_ylabel("Probability density")
-	ax.set_title(title)
-	ax.text(-7, 0.1, "Extreme negatives")
-	ax.text(7, 0.1, "Extreme positives")
+	if labels is not None:
+		plt.legend(labels=labels)
