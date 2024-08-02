29db974dd017b83c2094582b63a52bde26a5f82c
devakumar <devakumar@velozityglobal.com>
2024-08-02 15:39:25 +0000

    Add type hints to notebooks/ResNetScript (lines 0-16)

--- Algorithms/notebooks/ResNetScript.py
+++ Algorithms/notebooks/ResNetScript.py
@@ -1,2 +1,16 @@
-        return x
+def train(dataloader, model, loss_fn, optimizer):
+    size = len(dataloader.dataset)
+    model.train()
+    
+    for batch, (X, y) in enumerate(dataloader):
+        X, y = X.to(device), y.to(device)
 
+        #print(X.shape)
+        # Compute prediction error
+        pred = model(X)
+        loss = loss_fn(pred, y)
+
+        # Backpropagation
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
