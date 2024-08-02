3f8efc4f497ddb55d307f8dfd546685e09cb7ec4
mjragul <mjragul@velozityglobal.com>
2024-08-02 15:08:22 +0000

    Implement boosting algorithm in main (lines 0-16)

--- Algorithms/main.py
+++ Algorithms/main.py
@@ -1,7 +1,16 @@
-                criterion=criterion,
-                optimizer=optimizer,
+        net = restore_net(PATH_TO_TEST_MODEL)
+        net.eval()
+        net.to(DEVICE)
+
+        # after the training run function for train/val/test loader
+        loader = test_loader
+
+        ecgs, y_true, y_pred = test(
+                net=net,
+                test_loader=loader,
                 device=DEVICE,
-                batch_size=BATCH_SIZE
+                batch_size=BATCH_SIZE,
+                plot_ecg=False,
+                plot_ecg_windows_size=WINDOWS_SIZE
         )
 
-    if TEST:
