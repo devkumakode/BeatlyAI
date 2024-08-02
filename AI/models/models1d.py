0969494498a5853a5a476484ba526c31cf01ec92
sakshamdrivedi <sakshamdrivedi@velozityglobal.com>
2024-08-02 13:07:04 +0000

    Implement models/models1d in neural network architecture (lines 0-4)

--- AI/models/models1d.py
+++ AI/models/models1d.py
@@ -1,11 +1,4 @@
-            norm_layer = nn.BatchNorm1d
-        if groups != 1 or base_width != 64:
-            raise ValueError("BasicBlock only supports groups=1 and base_width=64")
-        if dilation > 1:
-            raise NotImplementedError("Dilation > 1 not supported in BasicBlock")
-        # Both self.conv1 and self.downsample layers downsample the input when stride != 1
-        self.conv1 = conv_block(inplanes, planes, stride)
-        self.bn1 = norm_layer(inplanes)
-        self.relu = nn.ReLU(inplace=True)
-        self.conv2 = conv_block(planes, planes)
-        self.bn2 = norm_layer(planes)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
