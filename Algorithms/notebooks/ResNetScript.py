42278e83498928ae54216d1be33ffdad166bcb95
devganesh <devganesh@velozityglobal.com>
2024-08-03 18:09:45 +0000

    Add anomaly detection capabilities to notebooks/ResNetScript (lines 0-19)

--- Algorithms/notebooks/ResNetScript.py
+++ Algorithms/notebooks/ResNetScript.py
@@ -1,9 +1,19 @@
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    model.eval()
+    test_loss, correct = 0, 0
+    with torch.no_grad():
+        for X, y in dataloader:
+            X, y = X.to(device), y.to(device)
+            pred = model(X)
+            test_loss += loss_fn(pred, y).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+    test_loss /= num_batches
+    correct /= size
+    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
 
-        #if batch % 100 == 0:
-         #   loss, current = loss.item(), (batch + 1) * len(X)
-          #  print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
-        
-        loss, current = loss.item(), (batch + 1) * len(X)
-        print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
 
-def test(dataloader, model, loss_fn):
+input_shape = (8,5000)  # Modify this according to your input shape
+# 128 is the batch size, 8 is the number of channels, 5000 is the number of time steps
+
+output_size = 1  # Number of output units
