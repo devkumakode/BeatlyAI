91592b1ebb6f5c230f0a19f98a34f7854303919a
devakumar <devakumar@velozityglobal.com>
2024-08-03 10:55:43 +0000

    Optimize preprocess for better performance (lines 0-6)

--- Algorithms/preprocess.py
+++ Algorithms/preprocess.py
@@ -1,8 +1,6 @@
 
-		assert len(ecg_lead) == len(ecg_labels)
+	return x_data, y_data
 
-		ecg_lead_splitted = split_ecg_lead_by_window(ecg_lead, window=window_len)
-		ecg_label_splitted = split_ecg_label_by_window(ecg_labels, window=window_len)
 
-		x_data = np.vstack((x_data, ecg_lead_splitted))
-		y_data = np.vstack((y_data, ecg_label_splitted))
+def preprocess_baseline_wander_removal (df, window_len):
+	x_data = np.empty((1, window_len, 2))
