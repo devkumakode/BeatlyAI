c897cbc6c74feffbb67ad9ac5b77a2e19df91b2c
mjragul <mjragul@velozityglobal.com>
2024-08-03 12:02:58 +0000

    Implement preprocess algorithm for feature selection (lines 0-8)

--- Algorithms/preprocess.py
+++ Algorithms/preprocess.py
@@ -1,3 +1,8 @@
 
-		ecg_lead = ecg['data'][ecg_csv_name]['ecg'][0]
-		ecg_labels = ecg['data'][ecg_csv_name]['label'][0]
+		assert len(ecg_lead) == len(ecg_labels)
+
+		ecg_lead_splitted = split_ecg_lead_by_window(ecg_lead, window=window_len)
+		ecg_label_splitted = split_ecg_label_by_window(ecg_labels, window=window_len)
+
+		x_data = np.vstack((x_data, ecg_lead_splitted))
+		y_data = np.vstack((y_data, ecg_label_splitted))
