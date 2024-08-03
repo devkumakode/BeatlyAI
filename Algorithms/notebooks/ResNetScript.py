4268ad8635655f90f2905c6139c3f41035704de6
mjragul <mjragul@velozityglobal.com>
2024-08-03 15:21:53 +0000

    Add anomaly detection capabilities to notebooks/ResNetScript (lines 0-9)

--- Algorithms/notebooks/ResNetScript.py
+++ Algorithms/notebooks/ResNetScript.py
@@ -1,16 +1,9 @@
-def train(dataloader, model, loss_fn, optimizer):
-    size = len(dataloader.dataset)
-    model.train()
-    
-    for batch, (X, y) in enumerate(dataloader):
-        X, y = X.to(device), y.to(device)
 
-        #print(X.shape)
-        # Compute prediction error
-        pred = model(X)
-        loss = loss_fn(pred, y)
+        #if batch % 100 == 0:
+         #   loss, current = loss.item(), (batch + 1) * len(X)
+          #  print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+        
+        loss, current = loss.item(), (batch + 1) * len(X)
+        print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
 
-        # Backpropagation
-        loss.backward()
-        optimizer.step()
-        optimizer.zero_grad()
+def test(dataloader, model, loss_fn):
