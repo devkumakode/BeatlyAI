0e83a23c88111ad43cf09725bfe0e770716dbffa
naveenkurra <naveenkurra@velozityglobal.com>
2024-08-05 15:08:30 +0000

    Add type hints to scripts/dataset-generation (lines 0-6)

--- AI/scripts/dataset-generation.py
+++ AI/scripts/dataset-generation.py
@@ -1,18 +1,6 @@
-    # plt.margins(0, 0) # use for generation images with no margin
-    plt.plot(signal)
-    plt.savefig(filename)
-
-    plt.close()
-
-    im_gray = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
-    im_gray = cv2.resize(im_gray, image_size, interpolation=cv2.INTER_LANCZOS4)
-    cv2.imwrite(filename, im_gray)
-
-
-if __name__ == "__main__":
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--file", required=True)
-    args = parser.parse_args()
-
-    ecg = args.file
+    name = osp.basename(ecg)
+    record = rdrecord(ecg)
+    ann = wfdb.rdann(ecg, extension="atr")
+    for sig_name, signal in zip(record.sig_name, record.p_signal.T):
+        if not np.all(np.isfinite(signal)):
+            continue
