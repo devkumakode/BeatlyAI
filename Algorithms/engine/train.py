b2f972f8348751a7494c205fb7f55f0cd8e3b325
mjragul <mjragul@velozityglobal.com>
2024-08-05 11:20:19 +0000

    Add regularization to engine/train to prevent overfitting (lines 0-18)

--- Algorithms/engine/train.py
+++ Algorithms/engine/train.py
@@ -1,18 +1,18 @@
-                batch_size = ecgs.shape[0]
+            Ppos = i
+        if val == 2 and y[i-1] == 0:
+            Qpos = i
+        if val == 2 and y[i+1] == 3:
+            Rpos = i
+        if val == 3 and y[i+1] == 0:
+            Spos = i
+        if val == 4 and y[i-1] == 0:
+            Tpos = i
 
-            output = net(ecgs)
-            output = output.to(device)
-
-            _, predicted = torch.max(output.data, 1)
-            label_true = labels.contiguous().view(-1).long()
-
-            total += label_true.size(0)
-            right += (predicted == label_true).sum().item()
-
-        print("epoch:{},{} ACC: {:.4f}".format(step, str1, right / total))
+    return Ppos, Qpos, Rpos, Spos, Tpos
 
 
-def get_charateristic(y):
-    Ppos = Qpos = Rpos =Spos = Tpos = 0
-    for i, val in enumerate(y):
-        if val == 1 and y[i-1] == 0:
+def point_equal(label, predict, tolerte):
+    if label + tolerte * 250 >= predict >= label - tolerte * 250:
+        return True
+    else:
+        return False
