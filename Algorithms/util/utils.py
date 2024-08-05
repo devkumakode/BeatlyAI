912536c5ef1288cc026167da8e0306a3cd3edca0
mjragul <mjragul@velozityglobal.com>
2024-08-05 13:00:28 +0000

    Implement ensemble method for util/utils (lines 0-13)

--- Algorithms/util/utils.py
+++ Algorithms/util/utils.py
@@ -1,7 +1,13 @@
-	plt.show()
+	source: https://www.kaggle.com/grfiv4/plot-a-confusion-matrix
 
-	return ax
+	given a sklearn confusion matrix (cm), make a nice plot
 
+	Arguments
+	---------
+	confusion_matrix: confusion matrix from sklearn.metrics.confusion_matrix
 
-def plot_confusion_matrix(confusion_matrix, target_names, title='Confusion matrix', cmap=None, normalize=True):
-	"""
+	target_names: given classification classes such as [0, 1, 2]
+				  the class names, for example: ['high', 'medium', 'low']
+
+	title:        the text to display at the top of the matrix
+
