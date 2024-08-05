e7e119b414dd9d7f1ff5739a6cf7971d293235d6
aman <aman@velozityglobal.com>
2024-08-05 14:17:05 +0000

    Add anomaly detection capabilities to notebooks/ResNetScript (lines 0-1)

--- Algorithms/notebooks/ResNetScript.py
+++ Algorithms/notebooks/ResNetScript.py
@@ -1,18 +1,1 @@
-
-model = KanResWide_X2(input_shape, output_size)
-model.to(device)
-print(model)
-
-import torch.optim as optim
-
-# Loss function for linear values (e.g., regression)
-loss_fn = nn.MSELoss()  # Mean Squared Error loss
-
-# Adam optimizer
-optimizer = optim.Adam(model.parameters(), lr=1e-3)  # You can adjust lr and other hyperparameters
-
-epochs = 50
-for t in range(epochs):
-    print(f"Epoch {t+1}\n-------------------------------")
-    train(train_dataloader, model, loss_fn, optimizer)
-    #test(validate_dataloader, model, loss_fn)
+print("Done!")
