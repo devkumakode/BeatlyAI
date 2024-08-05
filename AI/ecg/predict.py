419b0203e106a03881eed3f063f0eba844e5b781
devakumar <devakumar@velozityglobal.com>
2024-08-05 15:14:32 +0000

    Update dependencies for ecg/predict (lines 0-8)

--- AI/ecg/predict.py
+++ AI/ecg/predict.py
@@ -1,8 +1,8 @@
-def predict(data_json, model_path):
-    preproc = util.load(os.path.dirname(model_path))
-    dataset = load.load_dataset(data_json)
-    x, y = preproc.process(*dataset)
+    return probs
 
-    model = keras.models.load_model(model_path)
-    probs = model.predict(x, verbose=1)
-
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("data_json", help="path to data json")
+    parser.add_argument("model_path", help="path to model")
+    args = parser.parse_args()
+    probs = predict(args.data_json, args.model_path)
