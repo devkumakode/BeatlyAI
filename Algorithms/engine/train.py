3be58329cb7180dfda1662fe5c7b00a72487dfd6
aman <aman@velozityglobal.com>
2024-08-05 11:47:26 +0000

    Add logging to engine/train (lines 0-7)

--- Algorithms/engine/train.py
+++ Algorithms/engine/train.py
@@ -1,18 +1,7 @@
-            Ppos = i
-        if val == 2 and y[i-1] == 0:
-            Qpos = i
-        if val == 2 and y[i+1] == 3:
-            Rpos = i
-        if val == 3 and y[i+1] == 0:
-            Spos = i
-        if val == 4 and y[i-1] == 0:
-            Tpos = i
-
-    return Ppos, Qpos, Rpos, Spos, Tpos
 
 
-def point_equal(label, predict, tolerte):
-    if label + tolerte * 250 >= predict >= label - tolerte * 250:
-        return True
-    else:
-        return False
+def right_point(label_tuple, predict_tuple, tolerte):
+    n = np.array([0, 0, 0, 0, 0])
+    for i, (x, x_p) in enumerate(zip(label_tuple, predict_tuple)):
+        if point_equal(x, x_p, tolerte):
+            n[i] = 1
