fcf3b7b8420e48d5364e4e719ef0fd93399a6221
devganesh <devganesh@velozityglobal.com>
2024-08-05 12:19:49 +0000

    Refactor scripts/annotation-generation-2d for multi-GPU training (lines 0-5)

--- AI/scripts/annotation-generation-2d.py
+++ AI/scripts/annotation-generation-2d.py
@@ -1,8 +1,5 @@
-val_size = 0.1  # [0, 1]
 
-output_path = "/".join(data_path.split("/")[:-5])
-random_state = 7
-
-if __name__ == "__main__":
-    dataset = []
-    files = glob(data_path)
+    for file in glob(data_path):
+        *_, name, lead, label, filename = file.split("/")
+        dataset.append(
+            {
