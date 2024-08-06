926f778f7293bf2b34f4aafdf5647020ff37a467
aman <aman@velozityglobal.com>
2024-08-06 14:32:48 +0000

    Update dependencies for engine/train (lines 0-4)

--- Algorithms/engine/train.py
+++ Algorithms/engine/train.py
@@ -1,7 +1,4 @@
 
+    return n
 
-def right_point(label_tuple, predict_tuple, tolerte):
-    n = np.array([0, 0, 0, 0, 0])
-    for i, (x, x_p) in enumerate(zip(label_tuple, predict_tuple)):
-        if point_equal(x, x_p, tolerte):
-            n[i] = 1
+
