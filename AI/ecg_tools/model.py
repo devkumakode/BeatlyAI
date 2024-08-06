fd900fa2a0808ea786367d479bc2ffb9c1f4367a
maheshb <maheshb@velozityglobal.com>
2024-08-06 13:45:49 +0000

    Add unit tests for ecg_tools/model (lines 0-1)

--- AI/ecg_tools/model.py
+++ AI/ecg_tools/model.py
@@ -1,6 +1,1 @@
-if __name__ == "__main__":
-    print(LinearEmbedding(3, 192)(torch.rand(2, 128, 3)).shape)
-    print(MLP(3)(torch.rand(2, 128, 3)).shape)
-    print(TransformerEncoderLayer(192, 8)(torch.rand(2, 128, 192)).shape)
-    print(ECGformer(
-        4, 128, 5, 3, 192, 8, 4
+    )(torch.rand(2, 128, 3)).shape)
