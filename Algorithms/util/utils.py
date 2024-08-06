bc62ddf12ed0dd8e75e602afcabc624e6aaa6a69
mjragul <mjragul@velozityglobal.com>
2024-08-06 09:30:38 +0000

    Add type hints to util/utils (lines 0-5)

--- Algorithms/util/utils.py
+++ Algorithms/util/utils.py
@@ -1,15 +1,5 @@
-	cmap:         the gradient of the values displayed from matplotlib.pyplot.cm
-				  see http://matplotlib.org/examples/color/colormaps_reference.html
-				  plt.get_cmap('jet') or plt.cm.Blues
+	Citiation
+	---------
+	http://scikit-learn.org/stable/auto_examples/model_selection/plot_confusion_matrix.html
 
-	normalize:    If False, plot the raw numbers
-				  If True, plot the proportions
-
-	Usage
-	-----
-	plot_confusion_matrix(cm           = cm,                  # confusion matrix created by
-															  # sklearn.metrics.confusion_matrix
-						  normalize    = True,                # show proportions
-						  target_names = y_labels_vals,       # list of names of the classes
-						  title        = best_estimator_name) # title of graph
-
+	"""
