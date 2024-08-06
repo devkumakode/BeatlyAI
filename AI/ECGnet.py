f05fb4a76253d764450293ed34a844a8c09c075e
devganesh <devganesh@velozityglobal.com>
2024-08-06 10:05:58 +0000

    Optimize ECGnet for better performance (lines 0-5)

--- AI/ECGnet.py
+++ AI/ECGnet.py
@@ -1,19 +1,5 @@
-            nn.Conv1d(32, 64, 7, stride=1, padding=3),
-            nn.ReLU(),
-            nn.MaxPool1d(4, stride=2),
-
-            nn.Conv1d(64, 64, 5, stride=1, padding=2),
-            nn.ReLU(),
-            nn.MaxPool1d(2, 2),
-
-            nn.Conv1d(64, 64, 3, stride=1, padding=1),
-            nn.ReLU(),
-            nn.MaxPool1d(2, 2),
-
-            nn.Conv1d(64, 72, 3, stride=1, padding=1),
-            nn.ReLU(),
-            nn.MaxPool1d(2, 2),
+            nn.Dropout(p=.1),
+            nn.Linear(in_features=64, out_features=17),
         )
-        self.classifier = torch.nn.Sequential(
-            nn.Linear(in_features=216, out_features=64),
-            nn.ReLU(),
+
+    def forward(self, x, ex_features=None):
