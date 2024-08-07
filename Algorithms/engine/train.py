fa59c5ada9edb8af800a4fb2cc5f7267fcf3518e
devakumar <devakumar@velozityglobal.com>
2024-08-07 17:13:51 +0000

    Add support for sparse matrices in engine/train (lines 0-10)

--- Algorithms/engine/train.py
+++ Algorithms/engine/train.py
@@ -1,4 +1,10 @@
-
-    return n
-
-
+def plotlabel(y, bias):
+    cmap = ['k', 'r', 'g', 'b', 'c', 'y']
+    start = end = 0
+    for i in range(len(y) - 1):
+        if y[i] != y[i + 1]:
+            end = i
+            plt.plot(np.arange(start, end), y[start:end] - bias, cmap[int(y[i])])
+            start = i + 1
+        if i == len(y) - 2:
+            end = len(y) - 1
