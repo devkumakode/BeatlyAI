a247f527f64c73e7a2e210b7fc69974d06316821
maheshb <maheshb@velozityglobal.com>
2024-08-07 15:58:32 +0000

    Improve error handling in train (lines 0-12)

--- AI/train.py
+++ AI/train.py
@@ -1,6 +1,12 @@
-        print('training accuracy: ', [ci/total for ci in correct_sum])
-
-
-def train_fullprecision(net, train_loader, loss_func, optimizer, epoch):
-    """Train the original full precision network for one epoch."""
-    net.train()
+    train_loss = 0.
+    num_batches = 0
+    correct_sum = [0. for i in range(len(TOPK))]
+    total = 0
+    for (inputs, labels) in train_loader:               
+        inputs, labels = inputs.cuda(non_blocking=True), labels.cuda(non_blocking=True)
+        optimizer.zero_grad()    
+        outputs = net(inputs)
+        loss = loss_func(outputs, labels)
+        loss.backward()   
+        optimizer.step()
+        accuracy(outputs, labels, correct_sum, topk=TOPK)
