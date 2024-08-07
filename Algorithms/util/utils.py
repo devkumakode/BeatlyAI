fb8b83d11e60c28bb9b553d92ec3a5ef3ceb044b
premkumar <premkumar@velozityglobal.com>
2024-08-07 10:06:38 +0000

    Implement reinforcement learning algorithm in util/utils (lines 0-13)

--- Algorithms/util/utils.py
+++ Algorithms/util/utils.py
@@ -1,5 +1,13 @@
-	Citiation
-	---------
-	http://scikit-learn.org/stable/auto_examples/model_selection/plot_confusion_matrix.html
+	import matplotlib.pyplot as plt
+	import numpy as np
+	import itertools
 
-	"""
+	accuracy = np.trace(confusion_matrix) / float(np.sum(confusion_matrix))
+	misclass = 1 - accuracy
+
+	if cmap is None:
+		cmap = plt.get_cmap('Blues')
+
+	plt.figure(figsize=(8, 6))
+	plt.imshow(confusion_matrix, interpolation='nearest', cmap=cmap)
+	plt.title(title)
