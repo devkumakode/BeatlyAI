3a14c3de903f3f64a0576192fbc728bac1ae1053
aman <aman@velozityglobal.com>
2024-08-07 12:45:48 +0000

    Implement reinforcement learning algorithm in main (lines 0-16)

--- Algorithms/main.py
+++ Algorithms/main.py
@@ -1,16 +1,16 @@
-        net = restore_net(PATH_TO_TEST_MODEL)
-        net.eval()
-        net.to(DEVICE)
-
-        # after the training run function for train/val/test loader
-        loader = test_loader
-
-        ecgs, y_true, y_pred = test(
-                net=net,
-                test_loader=loader,
-                device=DEVICE,
-                batch_size=BATCH_SIZE,
-                plot_ecg=False,
-                plot_ecg_windows_size=WINDOWS_SIZE
+        eval(
+            ecgs=ecgs,
+            y_true=y_true,
+            y_pred=y_pred,
+            labels=[0, 1, 2, 3, 4],
+            target_names=['none', 'p_wave', 'qrs', 't_wave', 'extrasystole'],
+            plot_acc=True,
+            plot_loss=True,
+            plot_conf_matrix=True,
+            plot_ecg=True,
+            plot_ecg_windows_size=WINDOWS_SIZE
         )
+
+
+
 
