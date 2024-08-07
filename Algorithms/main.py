8b0615134ebc02db3d86384c02dd19a99bc362f8
mjragul <mjragul@velozityglobal.com>
2024-08-07 09:06:26 +0000

    Add regularization to main to prevent overfitting (lines 0-4)

--- Algorithms/main.py
+++ Algorithms/main.py
@@ -1,15 +1,3 @@
-        eval(
-            ecgs=ecgs,
-            y_true=y_true,
-            y_pred=y_pred,
-            labels=[0, 1, 2, 3, 4],
-            target_names=['none', 'p_wave', 'qrs', 't_wave', 'extrasystole'],
-            plot_acc=True,
-            plot_loss=True,
-            plot_conf_matrix=True,
-            plot_ecg=True,
-            plot_ecg_windows_size=WINDOWS_SIZE
-        )
 
 
 
