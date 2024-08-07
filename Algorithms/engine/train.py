59c32a9c293d4a3d79550bca0eb0da89b9357fb9
devganesh <devganesh@velozityglobal.com>
2024-08-07 11:09:13 +0000

    Implement active learning strategy in engine/train (lines 0-7)

--- Algorithms/engine/train.py
+++ Algorithms/engine/train.py
@@ -1,10 +1,7 @@
-def plotlabel(y, bias):
-    cmap = ['k', 'r', 'g', 'b', 'c', 'y']
-    start = end = 0
-    for i in range(len(y) - 1):
-        if y[i] != y[i + 1]:
-            end = i
             plt.plot(np.arange(start, end), y[start:end] - bias, cmap[int(y[i])])
-            start = i + 1
-        if i == len(y) - 2:
-            end = len(y) - 1
+
+
+def caculate_error(label_tuple, predict_tuple):
+    error = np.zeros((5,))
+    for i, (x, x_p) in enumerate(zip(label_tuple, predict_tuple)):
+        error[i] = (x - x_p)/250*100  # (ms)
