95676cc954ec26aba4410a46025cb4b942441e01
devakumar <devakumar@velozityglobal.com>
2024-08-07 10:42:48 +0000

    Fix bug in dataloaders/dataset2d (lines 0-13)

--- AI/dataloaders/dataset2d.py
+++ AI/dataloaders/dataset2d.py
@@ -1,14 +1,13 @@
-import json
+        self.mapper = json.load(open(mapping_path))
 
-import cv2
-from albumentations import Compose, Normalize
-from albumentations.pytorch.transforms import ToTensorV2
-from torch.utils.data import DataLoader, Dataset
+    def __getitem__(self, index):
+        img = cv2.imread(self.data[index]["path"])
+        img = augment(**{"image": img})["image"]
 
-augment = Compose([Normalize(), ToTensorV2()])
+        return {"image": img, "class": self.mapper[self.data[index]["label"]]}
 
-
-class EcgDataset2D(Dataset):
-    def __init__(self, ann_path, mapping_path):
-        super().__init__()
-        self.data = json.load(open(ann_path))
+    def get_dataloader(self, num_workers=4, batch_size=16, shuffle=True):
+        data_loader = DataLoader(
+            self, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers,
+        )
+        return data_loader
