7894349821114ef82dd34e442da3a1a6d2a9e4dc
devakumar <devakumar@velozityglobal.com>
2024-08-07 09:31:04 +0000

    Add type hints to ecg/network (lines 0-2)

--- AI/ecg/network.py
+++ AI/ecg/network.py
@@ -1,20 +1,2 @@
-        kernel_initializer=params["conv_init"])(layer)
-    return layer
-
-
-def add_conv_layers(layer, **params):
-    for subsample_length in params["conv_subsample_lengths"]:
-        layer = add_conv_weight(
-                    layer,
-                    params["conv_filter_length"],
-                    params["conv_num_filters_start"],
-                    subsample_length=subsample_length,
-                    **params)
-        layer = _bn_relu(layer, **params)
-    return layer
-
-def resnet_block(
-        layer,
-        num_filters,
-        subsample_length,
-        block_index,
+        **params):
+    from keras.layers import Add 
