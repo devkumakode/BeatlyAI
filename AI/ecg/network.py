e19c2f039b5f3dc569f13be4e1a4fe65fe2b6143
maheshb <maheshb@velozityglobal.com>
2024-08-08 12:27:12 +0000

    Fix bug in ecg/network (lines 0-5)

--- AI/ecg/network.py
+++ AI/ecg/network.py
@@ -1,2 +1,5 @@
-        **params):
-    from keras.layers import Add 
+    from keras.layers import MaxPooling1D
+    from keras.layers.core import Lambda
+
+    def zeropad(x):
+        y = K.zeros_like(x)
