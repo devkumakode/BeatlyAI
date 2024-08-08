015033d73408a4a3ecfb5d2d870c7e9bfd967ba4
mjragul <mjragul@velozityglobal.com>
2024-08-08 15:14:56 +0000

    Add time series forecasting capabilities to util/utils (lines 0-7)

--- Algorithms/util/utils.py
+++ Algorithms/util/utils.py
@@ -1,13 +1,7 @@
-	plt.colorbar()
-
-	if target_names is not None:
-		tick_marks = np.arange(len(target_names))
-		plt.xticks(tick_marks, target_names, rotation=45, size=19)
-		plt.yticks(tick_marks, target_names, size=19)
-
-	if normalize:
-		cm = confusion_matrix.astype('float') / confusion_matrix.sum(axis=1)[:, np.newaxis]
-
-	thresh = cm.max() / 1.5 if normalize else cm.max() / 2
-	for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-		color = "white" if cm[i, j] > thresh else "black"
+		if i == j:
+			if i == 0:
+				color = 'white'
+			else:
+				color = 'black'
+		if normalize:
+			plt.text(
