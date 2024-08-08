46159bf53d301db0333acb33b8d75fb746f0c23a
naveenkurra <naveenkurra@velozityglobal.com>
2024-08-08 18:56:41 +0000

    Implement scripts/dataset-generation in neural network architecture (lines 0-13)

--- AI/scripts/dataset-generation.py
+++ AI/scripts/dataset-generation.py
@@ -1,6 +1,13 @@
-    name = osp.basename(ecg)
-    record = rdrecord(ecg)
-    ann = wfdb.rdann(ecg, extension="atr")
-    for sig_name, signal in zip(record.sig_name, record.p_signal.T):
-        if not np.all(np.isfinite(signal)):
-            continue
+        signal = scale(signal)
+        for i, (label, peak) in enumerate(zip(ann.symbol, ann.sample)):
+            if label == "/":
+                label = "\\"
+            print("\r{} [{}/{}]".format(sig_name, i + 1, len(ann.symbol)), end="")
+            if isinstance(mode, list):
+                if np.all([i > 0, i + 1 < len(ann.sample)]):
+                    left = ann.sample[i - 1] + mode[0]
+                    right = ann.sample[i + 1] - mode[1]
+                else:
+                    continue
+            elif isinstance(mode, int):
+                left, right = peak - mode // 2, peak + mode // 2
