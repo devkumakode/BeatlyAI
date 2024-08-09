2f8efb80e38d055228f14283ecb1fc3a40b294e6
devakumar <devakumar@velozityglobal.com>
2024-08-09 16:55:21 +0000

    Add time series forecasting capabilities to preprocess (lines 0-9)

--- Algorithms/preprocess.py
+++ Algorithms/preprocess.py
@@ -1,11 +1,9 @@
-	split_df = pd.read_csv(split_scv_path)
-	split_df[['Database', 'filename']] = split_df.name.str.split('/', expand=True)
-	split_df['filename'] = split_df.apply(lambda x: extract_name(x['filename'], '', '.json'), axis=1)
-
-	return split_df
-
-
-if __name__ == "__main__":
-
-	###############################################################################
-	# temporary
+	# with open(config.RESOURCES_DIR + '/training_log.txt', 'r') as f:
+	# 	train_acc = []
+	# 	val_acc = []
+	# 	while f.readline():
+	# 		line = f.readline().split()
+	# 		if len(line) == 3:
+	# 			if line[0].split(',')[1] == 'train':
+	# 				train_acc.append(float(line[2]))
+	# 			if line[0].split(',')[1] == 'val':
