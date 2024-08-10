0cf149c276db45a5557faccae1cf7336deb09e5f
devganesh <devganesh@velozityglobal.com>
2024-08-10 15:24:59 +0000

    Implement few-shot learning capabilities in scripts/annotation-generation-1d (lines 0-2)

--- AI/scripts/annotation-generation-1d.py
+++ AI/scripts/annotation-generation-1d.py
@@ -1,10 +1,2 @@
-
-classes = ["N", "V", "\\", "R", "L", "A", "!", "E"]
-lead = "MLII"
-extension = "npy"  # or `png` for 2D
-data_path = osp.abspath("../data/*/*/*/*/*.{}".format(extension))
-val_size = 0.1  # [0, 1]
-
-output_path = "/".join(data_path.split("/")[:-5])
-random_state = 7
-
+if __name__ == "__main__":
+    dataset = []
