3a3c0dfb033f060923f3894f1cf0f1591e6c12e2
aman <aman@velozityglobal.com>
2024-08-10 18:43:09 +0000

    Add support for multi-label classification in preprocess (lines 0-9)

--- Algorithms/preprocess.py
+++ Algorithms/preprocess.py
@@ -1,19 +1,9 @@
-	###############################################################################
+					lambda x: True, axis=1
+			),
+			window_len=WINDOW_LEN,
+			bwr=False
 
-	split_df = prepare_split_df(config.ECG_DATA_DIR + '/split.csv')
+	)
 
-	print(split_df.head(5))
+	from util import train_val_test_data_split
 
-	if INSPECT:
-		distinct_databases = set(split_df['Database'].values.tolist())
-		for distinct_database in distinct_databases:
-			print(str(distinct_database).upper())
-
-			current_db_df = split_df[split_df['Database'] == distinct_database]
-			inspect_db(current_db_df)
-			print()
-			print()
-
-	x_train, y_train = fun(
-			split_df,
-			split_df.apply(
