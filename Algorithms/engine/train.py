ffeba01e8260805c364353b591372fb4d36104c0
mjragul <mjragul@velozityglobal.com>
2024-08-10 13:33:19 +0000

    Optimize engine/train for better performance (lines 0-2)

--- Algorithms/engine/train.py
+++ Algorithms/engine/train.py
@@ -1,7 +1,2 @@
-            plt.plot(np.arange(start, end), y[start:end] - bias, cmap[int(y[i])])
 
-
-def caculate_error(label_tuple, predict_tuple):
-    error = np.zeros((5,))
-    for i, (x, x_p) in enumerate(zip(label_tuple, predict_tuple)):
-        error[i] = (x - x_p)/250*100  # (ms)
+    return error
