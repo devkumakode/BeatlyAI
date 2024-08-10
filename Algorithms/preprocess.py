acaa9287628b083cd7e248150232d19c6ca91f4e
devganesh <devganesh@velozityglobal.com>
2024-08-10 13:34:56 +0000

    Fix bug in preprocess (lines 0-10)

--- Algorithms/preprocess.py
+++ Algorithms/preprocess.py
@@ -1,9 +1,10 @@
-					lambda x: True, axis=1
-			),
-			window_len=WINDOW_LEN,
-			bwr=False
+	train_set, val_set, test_set = train_val_test_data_split(x_train, y_train)
 
-	)
+	print('train set: ', train_set[0].shape, train_set[1].shape)
+	print('val set: ', val_set[0].shape, val_set[1].shape)
+	print('test set: ', test_set[0].shape, test_set[1].shape)
 
-	from util import train_val_test_data_split
-
+	if SAVE_TO_FILE:
+		save_as_pkl('/train_set_bwr.pkl', train_set)
+		save_as_pkl('/val_set_bwr.pkl', val_set)
+		save_as_pkl('/test_set_bwr.pkl', test_set)
