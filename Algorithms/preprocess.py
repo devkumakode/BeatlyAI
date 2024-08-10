c8af89cc3073b56003223de88e617c6502d3e694
aman <aman@velozityglobal.com>
2024-08-10 11:00:52 +0000

    Refactor preprocess for better code organization (lines 0-19)

--- Algorithms/preprocess.py
+++ Algorithms/preprocess.py
@@ -1,6 +1,19 @@
-	# 				val_acc.append(float(line[2]))
-	#
-	# 	mean_train_acc = np.mean(np.array(train_acc).reshape((len(train_acc), 1)))
-	# 	mean_val_acc = np.mean(np.array(val_acc).reshape((len(val_acc), 1)))
-	# 	print('mean_train_acc', mean_train_acc)
-	# 	print('mean_val_acc', mean_val_acc)
+	###############################################################################
+
+	split_df = prepare_split_df(config.ECG_DATA_DIR + '/split.csv')
+
+	print(split_df.head(5))
+
+	if INSPECT:
+		distinct_databases = set(split_df['Database'].values.tolist())
+		for distinct_database in distinct_databases:
+			print(str(distinct_database).upper())
+
+			current_db_df = split_df[split_df['Database'] == distinct_database]
+			inspect_db(current_db_df)
+			print()
+			print()
+
+	x_train, y_train = fun(
+			split_df,
+			split_df.apply(
