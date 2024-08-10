21d5b38af97e09974583594a5bdfebb368b6d652
naveenkurra <naveenkurra@velozityglobal.com>
2024-08-10 16:21:25 +0000

    Update dependencies for binarynet (lines 0-17)

--- AI/binarynet.py
+++ AI/binarynet.py
@@ -1,15 +1,17 @@
-    """Construct a look-up-table to store bitwise values of all intergers given a bitwidth."""
-    bit_table = -torch.ones((2**bit, bit), dtype=torch.int8)
-    for i in range(1,2**bit):
-        for j in range(bit):
-            if (i & (1<<j)):
-                bit_table[i,j] = 1
-    return bit_table.to('cuda')
 
 
-def binarize(input_t): 
-    """Binarize input tensor."""
-    dim = input_t.nelement()
-    output_t = torch.ones(dim)
-    output_t[input_t<0] = -1
-    return output_t
+def transform_bin_basis(w_vec, max_dim, rel_norm_thres=REL_NORM_THRES):
+    """Transform a full precision weight vector into multi-bit form, i.e. binary bases and coordiantes."""
+    # Reshape the coordinates vector in w domain
+    crd_w = w_vec.detach().view(-1,1) # 展开成一个列向量
+    #print(crd_w)    
+    # Get the dimensionality in w domain
+    dim_w = crd_w.nelement()  #列向量的长度（16）
+    #print(dim_w)
+    # Determine the max number of dimensionality in alpha domain
+    if dim_w <= max_dim:
+        max_dim_alpha = dim_w
+    else:
+        max_dim_alpha = max_dim
+    # Initialize binary basis matrix in alpha domain
+    bin_basis_alpha = torch.zeros((dim_w, max_dim_alpha))
