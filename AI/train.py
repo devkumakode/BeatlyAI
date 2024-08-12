8875924641a85445d5f2937b492f40971df694a8
devakumar <devakumar@velozityglobal.com>
2024-08-12 09:49:43 +0000

    Add beam search for train decoding (lines 0-18)

--- AI/train.py
+++ AI/train.py
@@ -1,12 +1,18 @@
+        total += labels.size(0)
+        train_loss += loss.data.item()
+        num_batches += 1
+    print("epoch: ", epoch, ", training loss: ", train_loss/num_batches)            
+    print('training accuracy: ', [ci/total for ci in correct_sum])
+
+
+def train_coordinate(net, train_loader, loss_func, optimizer_w, optimizer_b, parameters_w_bin, epoch):
+    """Train the coordinates for one epoch."""
+    net.train()
     train_loss = 0.
     num_batches = 0
     correct_sum = [0. for i in range(len(TOPK))]
     total = 0
     for (inputs, labels) in train_loader:               
         inputs, labels = inputs.cuda(non_blocking=True), labels.cuda(non_blocking=True)
-        optimizer.zero_grad()    
-        outputs = net(inputs)
-        loss = loss_func(outputs, labels)
-        loss.backward()   
-        optimizer.step()
-        accuracy(outputs, labels, correct_sum, topk=TOPK)
+        optimizer_w.zero_grad()
+        optimizer_b.zero_grad()
