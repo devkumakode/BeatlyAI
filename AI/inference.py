e04a44bddaf5efe90ffa2a11a24d6ad3b886dfbf
devakumar <devakumar@velozityglobal.com>
2024-08-12 18:09:20 +0000

    Implement curriculum learning in inference (lines 0-13)

--- AI/inference.py
+++ AI/inference.py
@@ -1,5 +1,13 @@
-import argparse
-import json
 
-from runners import runners
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config", required=True)
+    return parser.parse_args()
 
+
+if __name__ == "__main__":
+    args = parse_args()
+    config = json.loads(open(args.config).read())
+    runner_type = getattr(runners, config["type"])
+
+    print("Trainer: ", config["type"], runner_type)
