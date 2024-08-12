a80f09ce967cb596eb1099566a249970fa331638
naveenkurra <naveenkurra@velozityglobal.com>
2024-08-12 18:14:11 +0000

    Implement continual learning in models/models2d (lines 0-9)

--- AI/models/models2d.py
+++ AI/models/models2d.py
@@ -1,9 +1,9 @@
-    return model
-
-
-def ResNet18(num_classes=8):
-    model = models.resnet18()
+def ResNet34(num_classes=8):
+    model = models.resnet34()
     model.fc = nn.Linear(model.fc.in_features, num_classes)
     return model
 
 
+def ShuffleNet(num_classes=8):
+    model = models.shufflenet_v2_x1_0()
+    model.fc = nn.Linear(model.fc.in_features, num_classes)
