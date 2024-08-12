48bd23a01528d327043ea2593c14ecd1ad8e3ce3
maheshb <maheshb@velozityglobal.com>
2024-08-12 15:17:55 +0000

    Add support for mixed precision training in ecg/network (lines 0-13)

--- AI/ecg/network.py
+++ AI/ecg/network.py
@@ -1,5 +1,13 @@
-    from keras.layers import MaxPooling1D
-    from keras.layers.core import Lambda
+        return K.concatenate([x, y], axis=2)
 
-    def zeropad(x):
-        y = K.zeros_like(x)
+    def zeropad_output_shape(input_shape):
+        shape = list(input_shape)
+        assert len(shape) == 3
+        shape[2] *= 2
+        return tuple(shape)
+
+    shortcut = MaxPooling1D(pool_size=subsample_length)(layer)
+    zero_pad = (block_index % params["conv_increase_channels_at"]) == 0 \
+        and block_index > 0
+    if zero_pad is True:
+        shortcut = Lambda(zeropad, output_shape=zeropad_output_shape)(shortcut)
