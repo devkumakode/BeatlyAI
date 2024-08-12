b57cfc54ddd0e09cf97f3c60dc5c539b4a121298
devganesh <devganesh@velozityglobal.com>
2024-08-12 12:23:50 +0000

    Add support for model compression in dataloaders/dataset1d (lines 0-13)

--- AI/dataloaders/dataset1d.py
+++ AI/dataloaders/dataset1d.py
@@ -1,4 +1,13 @@
-            self, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers,
-        )
-        return data_loader
+    def __len__(self):
+        return len(self.data)
 
+
+def callback_get_label(dataset, idx):
+    return dataset[idx]["class"]
+
+
+class EcgPipelineDataset1D(Dataset):
+    def __init__(self, path, mode=128):
+        super().__init__()
+        record = wfdb.rdrecord(path)
+        self.signal = None
