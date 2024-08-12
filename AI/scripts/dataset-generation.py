eaaa6ac823bfc987a0bf7cbfa5d6a53e69008bfd
naveenkurra <naveenkurra@velozityglobal.com>
2024-08-12 13:27:28 +0000

    Add support for model compression in scripts/dataset-generation (lines 0-6)

--- AI/scripts/dataset-generation.py
+++ AI/scripts/dataset-generation.py
@@ -1,13 +1,6 @@
-        signal = scale(signal)
-        for i, (label, peak) in enumerate(zip(ann.symbol, ann.sample)):
-            if label == "/":
-                label = "\\"
-            print("\r{} [{}/{}]".format(sig_name, i + 1, len(ann.symbol)), end="")
-            if isinstance(mode, list):
-                if np.all([i > 0, i + 1 < len(ann.sample)]):
-                    left = ann.sample[i - 1] + mode[0]
-                    right = ann.sample[i + 1] - mode[1]
-                else:
-                    continue
-            elif isinstance(mode, int):
-                left, right = peak - mode // 2, peak + mode // 2
+            else:
+                raise Exception("Wrong mode in script beginning")
+
+            if np.all([left > 0, right < len(signal)]):
+                one_dim_data_dir = osp.join(output_dir, "1D", name, sig_name, label)
+                two_dim_data_dir = osp.join(output_dir, "2D", name, sig_name, label)
