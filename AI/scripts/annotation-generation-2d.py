39d0e4de90ec6ab1d2dbedf3e7c3167a5c2645f3
sakshamdrivedi <sakshamdrivedi@velozityglobal.com>
2024-08-12 17:17:02 +0000

    Optimize scripts/annotation-generation-2d for better performance (lines 0-7)

--- AI/scripts/annotation-generation-2d.py
+++ AI/scripts/annotation-generation-2d.py
@@ -1,5 +1,7 @@
-
-    for file in glob(data_path):
-        *_, name, lead, label, filename = file.split("/")
-        dataset.append(
-            {
+                "name": name,
+                "lead": lead,
+                "label": label,
+                "filename": osp.splitext(filename)[0],
+                "path": file,
+            },
+        )
