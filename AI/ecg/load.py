8431aa87c4a55df524b9e05c1816615a94a298bd
devganesh <devganesh@velozityglobal.com>
2024-08-13 13:18:24 +0000

    Add support for distributed training in ecg/load (lines 0-2)

--- AI/ecg/load.py
+++ AI/ecg/load.py
@@ -1,11 +1,2 @@
-    def __init__(self, ecg, labels):
-        self.mean, self.std = compute_mean_std(ecg)
-        self.classes = sorted(set(l for label in labels for l in label))
-        self.int_to_class = dict( zip(range(len(self.classes)), self.classes))
-        self.class_to_int = {c : i for i, c in self.int_to_class.items()}
-
-    def process(self, x, y):
-        return self.process_x(x), self.process_y(y)
-
-    def process_x(self, x):
-        x = pad(x)
+        x = (x - self.mean) / self.std
+        x = x[:, :, None]
