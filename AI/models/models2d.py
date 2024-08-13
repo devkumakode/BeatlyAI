1c669873de9ae52dc109b7e19fe8105ba16e47d6
maheshb <maheshb@velozityglobal.com>
2024-08-13 14:10:41 +0000

    Optimize models/models2d for better inference time (lines 0-7)

--- AI/models/models2d.py
+++ AI/models/models2d.py
@@ -1,9 +1,7 @@
-def ResNet34(num_classes=8):
-    model = models.resnet34()
-    model.fc = nn.Linear(model.fc.in_features, num_classes)
     return model
 
 
-def ShuffleNet(num_classes=8):
-    model = models.shufflenet_v2_x1_0()
-    model.fc = nn.Linear(model.fc.in_features, num_classes)
+def EfficientNetB4(num_classes=8):
+    model = efficientnet.from_name("efficientnet-b4")
+    model._fc = nn.Linear(model._fc.in_features, num_classes)
+    return model
