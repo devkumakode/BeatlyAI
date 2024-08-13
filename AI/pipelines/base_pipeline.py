74caeec6a9459a0e8c736e61dd893eb05dba3a62
sakshamdrivedi <sakshamdrivedi@velozityglobal.com>
2024-08-13 14:10:45 +0000

    Add support for model compression in pipelines/base_pipeline (lines 0-4)

--- AI/pipelines/base_pipeline.py
+++ AI/pipelines/base_pipeline.py
@@ -1,11 +1,4 @@
+        self.res_dir = osp.join(self.config["exp_dir"], self.exp_name, "results")
+        os.makedirs(self.res_dir, exist_ok=True)
 
-from utils.network_utils import load_checkpoint
-
-
-class BasePipeline:
-    def __init__(self, config):
-        self.config = config
-        self.exp_name = self.config.get("exp_name", None)
-        if self.exp_name is None:
-            self.exp_name = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-
+        self.model = self._init_net()
