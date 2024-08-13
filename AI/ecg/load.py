d9a4bf7ca5ee7d8a49238b80f96b8c80321001ef
naveenkurra <naveenkurra@velozityglobal.com>
2024-08-13 15:56:04 +0000

    Implement CI/CD pipeline for ecg/load (lines 0-11)

--- AI/ecg/load.py
+++ AI/ecg/load.py
@@ -1,9 +1,11 @@
-                for i in range(0, end, batch_size)]
-    random.shuffle(batches)
-    while True:
-        for batch in batches:
-            x, y = zip(*batch)
-            yield preproc.process(x, y)
+    def __init__(self, ecg, labels):
+        self.mean, self.std = compute_mean_std(ecg)
+        self.classes = sorted(set(l for label in labels for l in label))
+        self.int_to_class = dict( zip(range(len(self.classes)), self.classes))
+        self.class_to_int = {c : i for i, c in self.int_to_class.items()}
 
-class Preproc:
+    def process(self, x, y):
+        return self.process_x(x), self.process_y(y)
 
+    def process_x(self, x):
+        x = pad(x)
