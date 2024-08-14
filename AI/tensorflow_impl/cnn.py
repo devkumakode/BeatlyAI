8266b6291cc5a74fd6b21a2699fe376c69de1547
sakshamdrivedi <sakshamdrivedi@velozityglobal.com>
2024-08-14 11:38:15 +0000

    Optimize memory usage in tensorflow_impl/cnn (lines 0-16)

--- AI/tensorflow_impl/cnn.py
+++ AI/tensorflow_impl/cnn.py
@@ -1,11 +1,16 @@
-dropout = args.dropout
-restore_model = args.restore_model
-freeze = args.freeze
-heart_diseases = args.heart_diseases
-verbose = args.verbose
+x = tf.placeholder(tf.float32, shape=[None, nr_inputs], name="X_input")
+y = tf.placeholder(tf.float32, shape=[None, nr_classes], name="Y_classes")
+keep_prob = tf.placeholder(tf.float32)
 
-# Network Parameters
-nr_inputs = 350 # changing this will also have to change the shape from wdense1
-nr_classes = len(heart_diseases)
+check_processed_dir_existance()
 
-# TF Graph input
+
+class CNN(object):
+    weights = {
+        # 10x1 conv filter, 1 input, 64 outputs
+        'wconv1': tf.Variable(tf.random_normal([10, 1, 64])),
+        # 10x64 conv filter, 64 inputs, 128 outputs
+        'wconv2': tf.Variable(tf.random_normal([10, 64, 128])),
+        # 10x128 conv filter, 128 inputs, 128 outputs
+        'wconv3': tf.Variable(tf.random_normal([10, 128, 128])),
+        # 10x128 conv filter, 128 inputs, 256 outputs
