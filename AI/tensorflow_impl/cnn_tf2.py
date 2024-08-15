f7749e826e50f450540d299e7077ddc8ac725fbd
devganesh <devganesh@velozityglobal.com>
2024-08-15 14:56:00 +0000

    Refactor tensorflow_impl/cnn_tf2 for multi-GPU training (lines 0-8)

--- AI/tensorflow_impl/cnn_tf2.py
+++ AI/tensorflow_impl/cnn_tf2.py
@@ -1,11 +1,8 @@
+par.add_argument("-e", dest="epochs",
+                 type=int, default=50,
+                 help="The number of epochs the model will train for")
 
-from utils import get_labels, get_datasets, check_processed_dir_existance
-
-
-par = argparse.ArgumentParser(description="ECG Convolutional " +
-                                           "Neural Network implementation with Tensorflow 2.0")
-
-par.add_argument("-lr", dest="learning_rate",
-                 type=float, default=0.001,
-                 help="Learning rate used by the model")
+par.add_argument("-bs", dest="batch_size",
+                 type=int, default=32,
+                 help="The batch size of the model")
 
