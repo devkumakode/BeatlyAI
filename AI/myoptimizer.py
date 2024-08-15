7381125a1ace3203bf7411f7b37883997ea77cf1
devakumar <devakumar@velozityglobal.com>
2024-08-15 17:40:26 +0000

    Implement myoptimizer in neural network architecture (lines 0-16)

--- AI/myoptimizer.py
+++ AI/myoptimizer.py
@@ -1,15 +1,16 @@
-        params (iterable): iterable of parameters to optimize or dicts defining parameter groups.
-        lr (float, optional): learning rate (default: 1e-3).
-        betas (Tuple[float, float], optional): coefficients used for computing running averages of gradient and its square (default: (0.9, 0.999)).
-        eps (float, optional): term added to the denominator to improve numerical stability (default: 1e-8).
-        weight_decay (float, optional): weight decay (L2 regularization) (default: 0).
+        if not 0.0 <= lr:
+            raise ValueError("Invalid learning rate: {}".format(lr))
+        if not 0.0 <= eps:
+            raise ValueError("Invalid epsilon value: {}".format(eps))
+        if not 0.0 <= betas[0] < 1.0:
+            raise ValueError("Invalid beta parameter at index 0: {}".format(betas[0]))
+        if not 0.0 <= betas[1] < 1.0:
+            raise ValueError("Invalid beta parameter at index 1: {}".format(betas[1]))
+        if not 0.0 <= weight_decay:
+            raise ValueError("Invalid weight_decay value: {}".format(weight_decay))
+        defaults = dict(lr=lr, betas=betas, eps=eps, weight_decay=weight_decay)
+        super(ALQ_optimizer, self).__init__(params, defaults)
         
-    Reference:
-        Adam optimizer by Pytorch:
-        https://pytorch.org/docs/stable/_modules/torch/optim/adam.html#Adam
-        On the Convergence of Adam and Beyond:
-        https://openreview.net/forum?id=ryQu7f-RZ
-    """
-    
-    def __init__(self, params, lr=1e-3, betas=(0.9, 0.999), eps=1e-8, weight_decay=0.):
-        # Check the validity 
+    def __setstate__(self, state):
+        super(ALQ_optimizer, self).__setstate__(state)
+           
