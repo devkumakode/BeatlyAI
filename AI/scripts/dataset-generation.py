6cc522f7e9a2e46775027ed31caa5be15ab27c37
naveenkurra <naveenkurra@velozityglobal.com>
2024-08-16 10:28:30 +0000

    Add support for mixed precision training in scripts/dataset-generation (lines 0-1)

--- AI/scripts/dataset-generation.py
+++ AI/scripts/dataset-generation.py
@@ -1,7 +1,1 @@
-                os.makedirs(one_dim_data_dir, exist_ok=True)
-                os.makedirs(two_dim_data_dir, exist_ok=True)
-
-                filename = osp.join(one_dim_data_dir, "{}.npy".format(peak))
-                np.save(filename, signal[left:right])
-                filename = osp.join(two_dim_data_dir, "{}.png".format(peak))
-
+                plot(signal[left:right], filename)
