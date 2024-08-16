9d9132325d0d8ceddac6fabce3caf9dabe9ada80
devganesh <devganesh@velozityglobal.com>
2024-08-16 14:30:57 +0000

    Add train to improve model interpretability (lines 0-4)

--- AI/train.py
+++ AI/train.py
@@ -1,9 +1,4 @@
-        outputs = net(inputs)
-        loss = loss_func(outputs, labels)
-        loss.backward()   
-        optimizer_b.step()
-        optimizer_w.step(parameters_w_bin, 'coordinate')
-        accuracy(outputs, labels, correct_sum, topk=TOPK)
-        total += labels.size(0)
-        train_loss += loss.data.item()
-        num_batches += 1    
+    print("epoch: ", epoch, ", training loss: ", train_loss/num_batches) 
+    print('training accuracy: ', [ci/total for ci in correct_sum])
+ 
+  
