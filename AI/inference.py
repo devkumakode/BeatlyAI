b5eb4c0590faf870624049feb2ac89ed1024b94b
naveenkurra <naveenkurra@velozityglobal.com>
2024-08-16 11:08:56 +0000

    Implement CI/CD pipeline for inference (lines 0-2)

--- AI/inference.py
+++ AI/inference.py
@@ -1,13 +1,2 @@
-
-def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--config", required=True)
-    return parser.parse_args()
-
-
-if __name__ == "__main__":
-    args = parse_args()
-    config = json.loads(open(args.config).read())
-    runner_type = getattr(runners, config["type"])
-
-    print("Trainer: ", config["type"], runner_type)
+    runner = runner_type(config)
+    runner.inference()
