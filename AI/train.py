561972d956283f12344ab4172f757e4c8067055f
maheshb <maheshb@velozityglobal.com>
2024-08-16 16:24:09 +0000

    Add unit tests for train (lines 0-9)

--- AI/train.py
+++ AI/train.py
@@ -1,18 +1,9 @@
+        outputs = net(inputs)
+        loss = loss_func(outputs, labels)
+        loss.backward()   
+        optimizer_b.step()
+        optimizer_w.step(parameters_w_bin, 'coordinate')
+        accuracy(outputs, labels, correct_sum, topk=TOPK)
         total += labels.size(0)
         train_loss += loss.data.item()
-        num_batches += 1
-    print("epoch: ", epoch, ", training loss: ", train_loss/num_batches)            
-    print('training accuracy: ', [ci/total for ci in correct_sum])
-
-
-def train_coordinate(net, train_loader, loss_func, optimizer_w, optimizer_b, parameters_w_bin, epoch):
-    """Train the coordinates for one epoch."""
-    net.train()
-    train_loss = 0.
-    num_batches = 0
-    correct_sum = [0. for i in range(len(TOPK))]
-    total = 0
-    for (inputs, labels) in train_loader:               
-        inputs, labels = inputs.cuda(non_blocking=True), labels.cuda(non_blocking=True)
-        optimizer_w.zero_grad()
-        optimizer_b.zero_grad()
+        num_batches += 1    
