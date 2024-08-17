c835b6fe39aa21358c7795fb58f1920fac6d8d07
naveenkurra <naveenkurra@velozityglobal.com>
2024-08-17 18:39:18 +0000

    Implement continual learning in scripts/annotation-generation-1d (lines 0-16)

--- AI/scripts/annotation-generation-1d.py
+++ AI/scripts/annotation-generation-1d.py
@@ -1,2 +1,16 @@
-if __name__ == "__main__":
-    dataset = []
+    files = glob(data_path)
+
+    for file in glob(data_path):
+        *_, name, lead, label, filename = file.split("/")
+        dataset.append(
+            {
+                "name": name,
+                "lead": lead,
+                "label": label,
+                "filename": osp.splitext(filename)[0],
+                "path": file,
+            },
+        )
+
+    data = pd.DataFrame(dataset)
+    data = data[data["lead"] == lead]
