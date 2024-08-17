079208ae1cc58c49ce7f418d901788e839348958
maheshb <maheshb@velozityglobal.com>
2024-08-17 10:47:41 +0000

    Refactor tensorflow_impl/cnn for multi-GPU training (lines 0-15)

--- AI/tensorflow_impl/cnn.py
+++ AI/tensorflow_impl/cnn.py
@@ -1,10 +1,15 @@
-        'wconv4': tf.Variable(tf.random_normal([10, 128, 256])),
-        # fully connected, 1024 outputs
-        'wdense1': tf.Variable(tf.random_normal([5376, 1024])),
-        # fully connected, 1024 inputs, 2048 outputs
-        'wdense2': tf.Variable(tf.random_normal([1024, 2048])),
-        # 2048 inputs, class prediction
-        'wout': tf.Variable(tf.random_normal([2048, nr_classes]))
+        'bconv1': tf.Variable(tf.random_normal([64])),
+        'bconv2': tf.Variable(tf.random_normal([128])),
+        'bconv3': tf.Variable(tf.random_normal([128])),
+        'bconv4': tf.Variable(tf.random_normal([256])),
+        'bdense1': tf.Variable(tf.random_normal([1024])),
+        'bdense2': tf.Variable(tf.random_normal([2048])),
+        'bout': tf.Variable(tf.random_normal([nr_classes]))
     }
 
-    biases = {
+    def __init__(self, weights=None, biases=None):
+        self.weights = weights if weights else self.weights
+        self.biases = biases if biases else self.biases
+        self.datasets = get_datasets(heart_diseases, nr_inputs)
+        self.label_data = get_labels(self.datasets)
+
