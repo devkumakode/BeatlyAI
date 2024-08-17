68917f538ce517dc2a899d9cf4782a92f8e142ec
sakshamdrivedi <sakshamdrivedi@velozityglobal.com>
2024-08-17 15:35:15 +0000

    Fix bug in myoptimizer (lines 0-12)

--- AI/myoptimizer.py
+++ AI/myoptimizer.py
@@ -1,3 +1,12 @@
-                # Initialize the state parameters in both w domain and alpha domain
-                if len(state) == 0:
-                    state['step_alpha'] = 0
+                    state['exp_avg_alpha'] = torch.zeros_like(p_bin.alpha)
+                    state['exp_avg_sq_alpha'] = torch.zeros_like(p_bin.alpha)
+                    state['max_exp_avg_sq_alpha'] = torch.zeros_like(p_bin.alpha)
+                    
+                    state['step'] = 0
+                    state['exp_avg'] = torch.zeros_like(p.data)
+                    state['exp_avg_sq'] = torch.zeros_like(p.data)
+                    state['max_exp_avg_sq'] = torch.zeros_like(p.data)
+
+                if mode == 'coordinate':
+                    # Update the state parameters in w domain
+                    exp_avg, exp_avg_sq = state['exp_avg'], state['exp_avg_sq']
