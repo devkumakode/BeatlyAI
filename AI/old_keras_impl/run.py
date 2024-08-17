96f5d848549081264768342d6b74018d2267e98b
sakshamdrivedi <sakshamdrivedi@velozityglobal.com>
2024-08-17 14:12:00 +0000

    Implement transfer learning for old_keras_impl/run (lines 0-14)

--- AI/old_keras_impl/run.py
+++ AI/old_keras_impl/run.py
@@ -1,6 +1,14 @@
-    if not (counter % batch_size):
-        X_train.append(inter_X_train)
-        y_train.append(inter_y_train)
-        inter_X_train = []
-        inter_y_train = []
+    inter_X_train.append(np.asarray(arrhy_data[i:i+500]))
+    inter_y_train.append(0)
+    inter_X_train.append(np.asarray(malignant_data[i:i+500]))
+    inter_y_train.append(1)
+    i += 500
 
+validation_size = int(0.1  * len(X_train))
+
+# remove the bugged batch
+X_train.pop(0)
+y_train.pop(0)
+
+# split training and testing sets
+X_train, X_test = np.split(X_train, [len(X_train)-validation_size])
