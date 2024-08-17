fc896f195847cefa02f499f30dd92bdd16d5e5a3
naveenkurra <naveenkurra@velozityglobal.com>
2024-08-17 09:11:23 +0000

    Optimize ecg/load for better performance (lines 0-15)

--- AI/ecg/load.py
+++ AI/ecg/load.py
@@ -1,11 +1,15 @@
-    def process_y(self, y):
-        # TODO, awni, fix hack pad with noise for cinc
-        y = pad([[self.class_to_int[c] for c in s] for s in y], val=3, dtype=np.int32) 
-        y = keras.utils.np_utils.to_categorical(
-                y, num_classes=len(self.classes))
-        return y
+        padded[e, :len(i)] = i
+    return padded
 
-def pad(x, val=0, dtype=np.float32):
-    max_len = max(len(i) for i in x)
-    padded = np.full((len(x), max_len), val, dtype=dtype)
-    for e, i in enumerate(x):
+def compute_mean_std(x):
+    x = np.hstack(x)
+    return (np.mean(x).astype(np.float32),
+           np.std(x).astype(np.float32))
+
+def load_dataset(data_json):
+    with open(data_json, 'r') as fid:
+        data = [json.loads(l) for l in fid]
+    labels = []; ecgs = []
+    for d in tqdm.tqdm(data):
+        labels.append(d['labels'])
+        ecgs.append(load_ecg(d['ecg']))
