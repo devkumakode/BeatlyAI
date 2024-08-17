1f34abefec981cd70b3e5abfff8e489f62877809
devganesh <devganesh@velozityglobal.com>
2024-08-17 13:30:08 +0000

    Implement few-shot learning capabilities in ecg_tools/train (lines 0-8)

--- AI/ecg_tools/train.py
+++ AI/ecg_tools/train.py
@@ -1,1 +1,8 @@
-                confusion_matrices_image_eval.append(self.validate_epoch(epoch))
+        return confusion_matrices_image_train, confusion_matrices_image_eval
+
+    def train_epoch(self, epoch):
+        self.model.train()
+        loader = tqdm(self.data_loader[Mode.train])
+        accuracy = 0
+        self.metrics[Mode.train].reset()
+        for index, data in enumerate(loader):
