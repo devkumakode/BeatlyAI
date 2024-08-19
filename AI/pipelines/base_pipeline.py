13ec391a70179b8c66ff4224170d167906198155
sakshamdrivedi <sakshamdrivedi@velozityglobal.com>
2024-08-19 09:59:29 +0000

    Implement CI/CD pipeline for pipelines/base_pipeline (lines 0-5)

--- AI/pipelines/base_pipeline.py
+++ AI/pipelines/base_pipeline.py
@@ -1,18 +1,5 @@
-
-        self.pipeline_loader = self._init_dataloader()
-
-        self.mapper = json.load(open(config["mapping_json"]))
-        self.mapper = {j: i for i, j in self.mapper.items()}
-
-        pretrained_path = self.config.get("model_path", False)
-        if pretrained_path:
-            load_checkpoint(pretrained_path, self.model)
-        else:
-            raise Exception(
-                "model_path doesnt't exist in config. Please specify checkpoint path",
-            )
-
-    def _init_net(self):
         raise NotImplemented
 
-    def _init_dataloader(self):
+    def run_pipeline(self):
+        self.model.eval()
+        pd_class = np.empty(0)
