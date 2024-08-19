18b5b1ebab941029e3ee50843e3daae90c8000d2
devakumar <devakumar@velozityglobal.com>
2024-08-19 18:27:15 +0000

    Implement early stopping in runners/runners (lines 0-4)

--- AI/runners/runners.py
+++ AI/runners/runners.py
@@ -1,12 +1,4 @@
-    def __init__(self, config):
-        super().__init__(config)
-
-    def _init_net(self):
-        model = getattr(models1d, self.config["model"])(
-            num_classes=self.config["num_classes"],
-        )
-        model = model.to(self.config["device"])
-        return model
-
-    def _init_dataloader(self):
-        inference_loader = EcgDataset1D(
+            self.config["json"], self.config["mapping_json"],
+        ).get_dataloader(
+            batch_size=self.config["batch_size"],
+            num_workers=self.config["num_workers"],
