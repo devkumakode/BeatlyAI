800a9555d9b5cc9747f5c6ccf9c2028b071c084b
devakumar <devakumar@velozityglobal.com>
2024-08-19 14:10:19 +0000

    Refactor ecg/load for multi-GPU training (lines 0-13)

--- AI/ecg/load.py
+++ AI/ecg/load.py
@@ -1,15 +1,13 @@
-        padded[e, :len(i)] = i
-    return padded
+    return ecgs, labels
 
-def compute_mean_std(x):
-    x = np.hstack(x)
-    return (np.mean(x).astype(np.float32),
-           np.std(x).astype(np.float32))
+def load_ecg(record):
+    if os.path.splitext(record)[1] == ".npy":
+        ecg = np.load(record)
+    elif os.path.splitext(record)[1] == ".mat":
+        ecg = sio.loadmat(record)['val'].squeeze()
+    else: # Assumes binary 16 bit integers
+        with open(record, 'r') as fid:
+            ecg = np.fromfile(fid, dtype=np.int16)
 
-def load_dataset(data_json):
-    with open(data_json, 'r') as fid:
-        data = [json.loads(l) for l in fid]
-    labels = []; ecgs = []
-    for d in tqdm.tqdm(data):
-        labels.append(d['labels'])
-        ecgs.append(load_ecg(d['ecg']))
+    trunc_samp = STEP * int(len(ecg) / STEP)
+    return ecg[:trunc_samp]
