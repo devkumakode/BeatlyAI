4b49ccb6e05079f0567014c2aef40c3d6c08f0de
naveenkurra <naveenkurra@velozityglobal.com>
2024-08-19 16:13:42 +0000

    Improve error handling in pipelines/base_pipeline (lines 0-18)

--- AI/pipelines/base_pipeline.py
+++ AI/pipelines/base_pipeline.py
@@ -1,4 +1,18 @@
-        self.res_dir = osp.join(self.config["exp_dir"], self.exp_name, "results")
-        os.makedirs(self.res_dir, exist_ok=True)
 
-        self.model = self._init_net()
+        self.pipeline_loader = self._init_dataloader()
+
+        self.mapper = json.load(open(config["mapping_json"]))
+        self.mapper = {j: i for i, j in self.mapper.items()}
+
+        pretrained_path = self.config.get("model_path", False)
+        if pretrained_path:
+            load_checkpoint(pretrained_path, self.model)
+        else:
+            raise Exception(
+                "model_path doesnt't exist in config. Please specify checkpoint path",
+            )
+
+    def _init_net(self):
+        raise NotImplemented
+
+    def _init_dataloader(self):
