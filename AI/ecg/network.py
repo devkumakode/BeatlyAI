fa1f31db2ba46b7576a8661e7d82cb91e0b2d7a5
maheshb <maheshb@velozityglobal.com>
2024-08-19 12:28:17 +0000

    Update dependencies for ecg/network (lines 0-3)

--- AI/ecg/network.py
+++ AI/ecg/network.py
@@ -1,19 +1,3 @@
 
-    for i in range(params["conv_num_skip"]):
-        if not (block_index == 0 and i == 0):
-            layer = _bn_relu(
-                layer,
-                dropout=params["conv_dropout"] if i > 0 else 0,
-                **params)
-        layer = add_conv_weight(
-            layer,
-            params["conv_filter_length"],
-            num_filters,
-            subsample_length if i == 0 else 1,
-            **params)
-    layer = Add()([shortcut, layer])
-    return layer
-
-def get_num_filters_at_index(index, num_start_filters, **params):
-    return 2**int(index / params["conv_increase_channels_at"]) \
-        * num_start_filters
+def add_resnet_layers(layer, **params):
+    layer = add_conv_weight(
