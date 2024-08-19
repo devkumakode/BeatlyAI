3c890c7b4db46e93b9d013f0b6ced42c435594f4
naveenkurra <naveenkurra@velozityglobal.com>
2024-08-19 17:18:33 +0000

    Implement train in neural network architecture (lines 0-10)

--- AI/train.py
+++ AI/train.py
@@ -1,4 +1,10 @@
-    print("epoch: ", epoch, ", training loss: ", train_loss/num_batches) 
-    print('training accuracy: ', [ci/total for ci in correct_sum])
- 
-  
+def train_basis(net, train_loader, loss_func, optimizer_w, optimizer_b, parameters_w_bin, epoch):
+    """Train the binary bases (with speedup) for one epoch."""
+    net.train()
+    train_loss = 0.
+    num_batches = 0
+    correct_sum = [0. for i in range(len(TOPK))]
+    total = 0
+    for inputs, labels in train_loader:               
+        inputs, labels = inputs.cuda(non_blocking=True), labels.cuda(non_blocking=True)
+        optimizer_w.zero_grad()
