471f62595920ae653f6b79b92b69e7a2700e1745
devakumar <devakumar@velozityglobal.com>
2024-08-20 10:48:47 +0000

    Fix bug in myoptimizer (lines 0-4)

--- AI/myoptimizer.py
+++ AI/myoptimizer.py
@@ -1,12 +1,4 @@
-                    state['exp_avg_alpha'] = torch.zeros_like(p_bin.alpha)
-                    state['exp_avg_sq_alpha'] = torch.zeros_like(p_bin.alpha)
-                    state['max_exp_avg_sq_alpha'] = torch.zeros_like(p_bin.alpha)
-                    
-                    state['step'] = 0
-                    state['exp_avg'] = torch.zeros_like(p.data)
-                    state['exp_avg_sq'] = torch.zeros_like(p.data)
-                    state['max_exp_avg_sq'] = torch.zeros_like(p.data)
-
-                if mode == 'coordinate':
-                    # Update the state parameters in w domain
-                    exp_avg, exp_avg_sq = state['exp_avg'], state['exp_avg_sq']
+                    max_exp_avg_sq = state['max_exp_avg_sq']
+                    beta1, beta2 = group['betas']
+                    state['step'] += 1
+                    # Decay the first and second moment running average coefficient
