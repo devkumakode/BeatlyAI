a31482849329839882e77f886b705d00d433f1b9
sakshamdrivedi <sakshamdrivedi@velozityglobal.com>
2024-08-20 12:56:38 +0000

    Refactor trainers/trainers for multi-GPU training (lines 0-17)

--- AI/trainers/trainers.py
+++ AI/trainers/trainers.py
@@ -1,15 +1,17 @@
-        model = getattr(models2d, self.config["model"])(
-            num_classes=self.config["num_classes"],
-        )
-        model = model.to(self.config["device"])
-        return model
-
-    def _init_dataloaders(self):
-        train_loader = EcgDataset2D(
-            self.config["train_json"], self.config["mapping_json"],
         ).get_dataloader(
             batch_size=self.config["batch_size"],
             num_workers=self.config["num_workers"],
         )
-        val_loader = EcgDataset2D(
-            self.config["val_json"], self.config["mapping_json"],
+
+        return train_loader, val_loader
+
+
+class Trainer1D(BaseTrainer):
+    def __init__(self, config):
+        super().__init__(config)
+
+    def _init_net(self):
+        model = getattr(models1d, self.config["model"])(
+            num_classes=self.config["num_classes"],
+        )
+        model = model.to(self.config["device"])
