6692fd6c1a3980a848200666a0b54c964a37987a
naveenkurra <naveenkurra@velozityglobal.com>
2024-08-20 12:03:01 +0000

    Implement CI/CD pipeline for ecg/network (lines 0-18)

--- AI/ecg/network.py
+++ AI/ecg/network.py
@@ -1,3 +1,18 @@
+        layer,
+        params["conv_filter_length"],
+        params["conv_num_filters_start"],
+        subsample_length=1,
+        **params)
+    layer = _bn_relu(layer, **params)
+    for index, subsample_length in enumerate(params["conv_subsample_lengths"]):
+        num_filters = get_num_filters_at_index(
+            index, params["conv_num_filters_start"], **params)
+        layer = resnet_block(
+            layer,
+            num_filters,
+            subsample_length,
+            index,
+            **params)
+    layer = _bn_relu(layer, **params)
+    return layer
 
-def add_resnet_layers(layer, **params):
-    layer = add_conv_weight(
