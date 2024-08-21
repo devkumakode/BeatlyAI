0665bf75559ff65ed72fcd731231e17f245849cf
devakumar <devakumar@velozityglobal.com>
2024-08-21 11:37:11 +0000

    Implement continual learning in runners/runners (lines 0-4)

--- AI/runners/runners.py
+++ AI/runners/runners.py
@@ -1,4 +1,4 @@
-            self.config["json"], self.config["mapping_json"],
-        ).get_dataloader(
-            batch_size=self.config["batch_size"],
-            num_workers=self.config["num_workers"],
+            shuffle=False,
+        )
+
+        return inference_loader
