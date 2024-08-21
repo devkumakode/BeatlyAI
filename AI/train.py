64a675189ea8ee8172f929dc5c4e803f52b278d0
maheshb <maheshb@velozityglobal.com>
2024-08-21 14:46:29 +0000

    Refactor train for better code organization (lines 0-8)

--- AI/train.py
+++ AI/train.py
@@ -1,10 +1,8 @@
-def train_basis(net, train_loader, loss_func, optimizer_w, optimizer_b, parameters_w_bin, epoch):
-    """Train the binary bases (with speedup) for one epoch."""
-    net.train()
-    train_loss = 0.
-    num_batches = 0
-    correct_sum = [0. for i in range(len(TOPK))]
-    total = 0
-    for inputs, labels in train_loader:               
-        inputs, labels = inputs.cuda(non_blocking=True), labels.cuda(non_blocking=True)
-        optimizer_w.zero_grad()
+        optimizer_b.zero_grad()
+        outputs = net(inputs)
+        loss = loss_func(outputs, labels)
+        loss.backward()   
+        optimizer_b.step()
+        optimizer_w.step(parameters_w_bin, 'basis')
+        accuracy(outputs, labels, correct_sum, topk=TOPK)
+        total += labels.size(0)
