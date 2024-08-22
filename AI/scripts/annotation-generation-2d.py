b3ce40ab7751aa9d4df052cf084b25c011be0c6f
sakshamdrivedi <sakshamdrivedi@velozityglobal.com>
2024-08-22 12:41:31 +0000

    Implement transfer learning for scripts/annotation-generation-2d (lines 0-11)

--- AI/scripts/annotation-generation-2d.py
+++ AI/scripts/annotation-generation-2d.py
@@ -1,2 +1,11 @@
+    data = data[data["lead"] == lead]
+    data = data[data["label"].isin(classes)]
+    data = data.sample(frac=1, random_state=random_state)
 
-    data = pd.DataFrame(dataset)
+    val_ids = []
+    for cl in classes:
+        val_ids.extend(
+            data[data["label"] == cl]
+            .sample(frac=val_size, random_state=random_state)
+            .index,
+        )
