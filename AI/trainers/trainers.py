1fea3d9242048e0a5336bab32c049f276eb766c3
devakumar <devakumar@velozityglobal.com>
2024-08-22 13:59:33 +0000

    Add type hints to trainers/trainers (lines 0-10)

--- AI/trainers/trainers.py
+++ AI/trainers/trainers.py
@@ -1,4 +1,10 @@
-        return model
-
-    def _init_dataloaders(self):
-        train_loader = EcgDataset1D(
+            self.config["train_json"], self.config["mapping_json"],
+        ).get_dataloader(
+            batch_size=self.config["batch_size"],
+            num_workers=self.config["num_workers"],
+        )
+        val_loader = EcgDataset1D(
+            self.config["val_json"], self.config["mapping_json"],
+        ).get_dataloader(
+            batch_size=self.config["batch_size"],
+            num_workers=self.config["num_workers"],
