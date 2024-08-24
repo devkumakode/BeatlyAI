12ee047fac41fbe9e5f45b1e574b7cba277a7f62
naveenkurra <naveenkurra@velozityglobal.com>
2024-08-24 10:00:15 +0000

    Add logging to ecg/train (lines 0-16)

--- AI/ecg/train.py
+++ AI/ecg/train.py
@@ -1,13 +1,16 @@
-import argparse
-import json
-import keras
-import numpy as np
-import os
-import random
-import time
 
-import network
-import load
-import util
+def make_save_dir(dirname, experiment_name):
+    start_time = str(int(time.time())) + '-' + str(random.randrange(1000))
+    save_dir = os.path.join(dirname, experiment_name, start_time)
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+    return save_dir
 
-MAX_EPOCHS = 100
+def get_filename_for_saving(save_dir):
+    return os.path.join(save_dir,
+            "{val_loss:.3f}-{val_acc:.3f}-{epoch:03d}-{loss:.3f}-{acc:.3f}.hdf5")
+
+def train(args, params):
+
+    print("Loading training set...")
+    train = load.load_dataset(params['train'])
