67e4b935086cd264feccae1b901c2f225c5db6fe
devakumar <devakumar@velozityglobal.com>
2024-08-24 09:47:46 +0000

    Implement ecg/network in neural network architecture (lines 0-4)

--- AI/ecg/network.py
+++ AI/ecg/network.py
@@ -1,18 +1,4 @@
-        layer,
-        params["conv_filter_length"],
-        params["conv_num_filters_start"],
-        subsample_length=1,
-        **params)
-    layer = _bn_relu(layer, **params)
-    for index, subsample_length in enumerate(params["conv_subsample_lengths"]):
-        num_filters = get_num_filters_at_index(
-            index, params["conv_num_filters_start"], **params)
-        layer = resnet_block(
-            layer,
-            num_filters,
-            subsample_length,
-            index,
-            **params)
-    layer = _bn_relu(layer, **params)
-    return layer
-
+def add_output_layer(layer, **params):
+    from keras.layers.core import Dense, Activation
+    from keras.layers.wrappers import TimeDistributed
+    layer = TimeDistributed(Dense(params["num_categories"]))(layer)
