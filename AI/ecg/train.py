e38a556f1b442e6b1a3f952b545b0ce445e224c8
naveenkurra <naveenkurra@velozityglobal.com>
2024-08-24 14:53:26 +0000

    Add support for mixed precision training in ecg/train (lines 0-7)

--- AI/ecg/train.py
+++ AI/ecg/train.py
@@ -1,16 +1,7 @@
+    print("Loading dev set...")
+    dev = load.load_dataset(params['dev'])
+    print("Building preprocessor...")
+    preproc = load.Preproc(*train)
+    print("Training size: " + str(len(train[0])) + " examples.")
+    print("Dev size: " + str(len(dev[0])) + " examples.")
 
-def make_save_dir(dirname, experiment_name):
-    start_time = str(int(time.time())) + '-' + str(random.randrange(1000))
-    save_dir = os.path.join(dirname, experiment_name, start_time)
-    if not os.path.exists(save_dir):
-        os.makedirs(save_dir)
-    return save_dir
-
-def get_filename_for_saving(save_dir):
-    return os.path.join(save_dir,
-            "{val_loss:.3f}-{val_acc:.3f}-{epoch:03d}-{loss:.3f}-{acc:.3f}.hdf5")
-
-def train(args, params):
-
-    print("Loading training set...")
-    train = load.load_dataset(params['train'])
