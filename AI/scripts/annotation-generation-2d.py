edf711aa38cfe400d285717d26ff8ddd0ab442a6
sakshamdrivedi <sakshamdrivedi@velozityglobal.com>
2024-08-26 16:18:50 +0000

    Implement transfer learning for scripts/annotation-generation-2d (lines 0-1)

--- AI/scripts/annotation-generation-2d.py
+++ AI/scripts/annotation-generation-2d.py
@@ -1,12 +1,1 @@
-
-    val = data.loc[val_ids, :]
-    train = data[~data.index.isin(val.index)]
-
-    train.to_json(osp.join(output_path, "train.json"), orient="records")
-    val.to_json(osp.join(output_path, "val.json"), orient="records")
-
-    d = {}
-    for label in train.label.unique():
-        d[label] = len(d)
-
-    with open(osp.join(output_path, "class-mapper.json"), "w") as file:
+        file.write(json.dumps(d, indent=1))
