290b2b5446d58aefb85afad0228f8c486e6e2d80
sakshamdrivedi <sakshamdrivedi@velozityglobal.com>
2024-08-26 09:17:42 +0000

    Implement curriculum learning in old_keras_impl/run (lines 0-3)

--- AI/old_keras_impl/run.py
+++ AI/old_keras_impl/run.py
@@ -1,14 +1,3 @@
-y_train, y_test = np.split(y_train, [len(y_train)-validation_size])
-
-# checking batch lengths
-for batch in X_train:
-    if len(batch) != 16:
-        print("uneven batch with len: {}".format(len(batch)))
-    for example in batch:
-        if len(example) != 500:
-            print("uneven example with len: {}".format(len(example)))
-
-
-# shape = (X_train.shape[0], 16, 500)
-shape = X_train.shape[1:]
+# in numpy if arrays are not the same shape they will not appear in the .shape method
+print("shape: {}".format(shape))
 
