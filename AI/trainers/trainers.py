69c02abb8dc4105cfa5ff2d7970aa10698b3eca6
sakshamdrivedi <sakshamdrivedi@velozityglobal.com>
2024-08-26 17:08:11 +0000

    Add unit tests for trainers/trainers (lines 0-3)

--- AI/trainers/trainers.py
+++ AI/trainers/trainers.py
@@ -1,10 +1,3 @@
-            self.config["train_json"], self.config["mapping_json"],
-        ).get_dataloader(
-            batch_size=self.config["batch_size"],
-            num_workers=self.config["num_workers"],
         )
-        val_loader = EcgDataset1D(
-            self.config["val_json"], self.config["mapping_json"],
-        ).get_dataloader(
-            batch_size=self.config["batch_size"],
-            num_workers=self.config["num_workers"],
+
+        return train_loader, val_loader
