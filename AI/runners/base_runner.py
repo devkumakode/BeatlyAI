ef4237a9a9ba9348e1360b1865eed5115fd1d8d6
maheshb <maheshb@velozityglobal.com>
2024-08-26 15:31:27 +0000

    Implement early stopping in runners/base_runner (lines 0-7)

--- AI/runners/base_runner.py
+++ AI/runners/base_runner.py
@@ -1,1 +1,7 @@
-        self.model.eval()
+
+        gt_class = np.empty(0)
+        pd_class = np.empty(0)
+
+        with torch.no_grad():
+            for i, batch in tqdm(enumerate(self.inference_loader)):
+                inputs = batch["image"].to(self.config["device"])
