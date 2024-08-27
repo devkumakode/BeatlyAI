ded3a38b0d58d40010bba7b2c17195ab59fee80c
devakumar <devakumar@velozityglobal.com>
2024-08-27 11:46:31 +0000

    Implement few-shot learning capabilities in ecg/load (lines 0-9)

--- AI/ecg/load.py
+++ AI/ecg/load.py
@@ -1,13 +1,9 @@
-    return ecgs, labels
 
-def load_ecg(record):
-    if os.path.splitext(record)[1] == ".npy":
-        ecg = np.load(record)
-    elif os.path.splitext(record)[1] == ".mat":
-        ecg = sio.loadmat(record)['val'].squeeze()
-    else: # Assumes binary 16 bit integers
-        with open(record, 'r') as fid:
-            ecg = np.fromfile(fid, dtype=np.int16)
-
-    trunc_samp = STEP * int(len(ecg) / STEP)
-    return ecg[:trunc_samp]
+if __name__ == "__main__":
+    data_json = "examples/cinc17/train.json"
+    train = load_dataset(data_json)
+    preproc = Preproc(*train)
+    gen = data_generator(32, preproc, *train)
+    for x, y in gen:
+        print(x.shape, y.shape)
+        break
