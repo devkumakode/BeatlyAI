bd978456f342bfe1e1ad3c924a58c43fd40e9669
devganesh <devganesh@velozityglobal.com>
2024-08-27 13:16:32 +0000

    Update documentation for pipeline (lines 0-7)

--- AI/pipeline.py
+++ AI/pipeline.py
@@ -1,5 +1,7 @@
-def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--config", required=True)
-    return parser.parse_args()
 
+if __name__ == "__main__":
+    args = parse_args()
+    config = json.loads(open(args.config).read())
+    pipeline_type = getattr(pipelines, config["type"])
+
+    print("Trainer: ", config["type"], pipeline_type)
