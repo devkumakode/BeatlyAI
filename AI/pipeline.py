e3bff9944d3632eb54fa3bfeb4c1de422406718f
naveenkurra <naveenkurra@velozityglobal.com>
2024-08-28 11:22:54 +0000

    Add support for distributed training in pipeline (lines 0-2)

--- AI/pipeline.py
+++ AI/pipeline.py
@@ -1,7 +1,2 @@
-
-if __name__ == "__main__":
-    args = parse_args()
-    config = json.loads(open(args.config).read())
-    pipeline_type = getattr(pipelines, config["type"])
-
-    print("Trainer: ", config["type"], pipeline_type)
+    pipeline = pipeline_type(config)
+    pipeline.run_pipeline()
