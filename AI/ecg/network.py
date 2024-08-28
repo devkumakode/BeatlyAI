73599a4ddf4c49df84c0ccd60f829844ed229b07
maheshb <maheshb@velozityglobal.com>
2024-08-28 14:49:30 +0000

    Add explainability features to ecg/network (lines 0-12)

--- AI/ecg/network.py
+++ AI/ecg/network.py
@@ -1,4 +1,12 @@
-    return Activation('softmax')(layer)
+    optimizer = Adam(
+        lr=params["learning_rate"],
+        clipnorm=params.get("clipnorm", 1))
 
-def add_compile(model, **params):
-    from keras.optimizers import Adam
+    model.compile(loss='categorical_crossentropy',
+                  optimizer=optimizer,
+                  metrics=['accuracy'])
+
+def build_network(**params):
+    from keras.models import Model
+    from keras.layers import Input
+    inputs = Input(shape=params['input_shape'],
