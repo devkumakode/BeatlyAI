fa2ecf465d0f87d6da4f2554525f7d83f4f2238a
devakumar <devakumar@velozityglobal.com>
2024-08-28 13:45:14 +0000

    Optimize dataloaders/dataset1d for better inference time (lines 0-13)

--- AI/dataloaders/dataset1d.py
+++ AI/dataloaders/dataset1d.py
@@ -1,5 +1,13 @@
-        self.mode = mode
-        for sig_name, signal in zip(record.sig_name, record.p_signal.T):
-            if sig_name in ["MLII", "II"] and np.all(np.isfinite(signal)):
-                self.signal = scale(signal).astype("float32")
-        if self.signal is None:
+            raise Exception("No MLII LEAD")
+
+        self.peaks = find_peaks(self.signal, distance=180)[0]
+        mask_left = (self.peaks - self.mode // 2) > 0
+        mask_right = (self.peaks + self.mode // 2) < len(self.signal)
+        mask = mask_left & mask_right
+        self.peaks = self.peaks[mask]
+
+    def __getitem__(self, index):
+        peak = self.peaks[index]
+        left, right = peak - self.mode // 2, peak + self.mode // 2
+
+        img = self.signal[left:right]
