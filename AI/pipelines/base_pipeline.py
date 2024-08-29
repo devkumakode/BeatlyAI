3ff201c16ce418d445948bc6dc415d080f2f2288
sakshamdrivedi <sakshamdrivedi@velozityglobal.com>
2024-08-29 18:59:54 +0000

    Add support for model compression in pipelines/base_pipeline (lines 0-19)

--- AI/pipelines/base_pipeline.py
+++ AI/pipelines/base_pipeline.py
@@ -1,5 +1,19 @@
-        raise NotImplemented
+        pd_peaks = np.empty(0)
 
-    def run_pipeline(self):
-        self.model.eval()
-        pd_class = np.empty(0)
+        with torch.no_grad():
+            for i, batch in tqdm(enumerate(self.pipeline_loader)):
+                inputs = batch["image"].to(self.config["device"])
+
+                predictions = self.model(inputs)
+
+                classes = predictions.topk(k=1)[1].view(-1).cpu().numpy()
+
+                pd_class = np.concatenate((pd_class, classes))
+                pd_peaks = np.concatenate((pd_peaks, batch["peak"]))
+
+        pd_class = pd_class.astype(int)
+        pd_peaks = pd_peaks.astype(int)
+
+        annotations = []
+        for label, peak in zip(pd_class, pd_peaks):
+            if (
