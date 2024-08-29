42f21bb071f066c38a36d744520e755bf8148ff8
devakumar <devakumar@velozityglobal.com>
2024-08-29 10:34:40 +0000

    Add type hints to tensorflow_impl/cnn_tf2 (lines 0-12)

--- AI/tensorflow_impl/cnn_tf2.py
+++ AI/tensorflow_impl/cnn_tf2.py
@@ -1,4 +1,12 @@
-par.add_argument("--display-step", dest="display_step",
-                 type=int, default=10,
-                 help="The display step")
+par.add_argument("--dropout", type=float, default=0.5,
+                 help="Dropout probability")
 
+par.add_argument("--restore", dest="restore_model",
+                 action="store_true", default=False,
+                 help="Restore the model previously saved")
+
+par.add_argument("--freeze", dest="freeze",
+                 action="store_true", default=False,
+                 help="Freezes the model")
+
+par.add_argument("--heart-diseases", nargs="+",
