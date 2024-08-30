c752ebf7d866fccda5cb335a7ec7556f0f315a5e
devganesh <devganesh@velozityglobal.com>
2024-08-30 13:25:36 +0000

    Implement few-shot learning capabilities in pipelines/base_pipeline (lines 0-10)

--- AI/pipelines/base_pipeline.py
+++ AI/pipelines/base_pipeline.py
@@ -1,19 +1,10 @@
-        pd_peaks = np.empty(0)
-
-        with torch.no_grad():
-            for i, batch in tqdm(enumerate(self.pipeline_loader)):
-                inputs = batch["image"].to(self.config["device"])
-
-                predictions = self.model(inputs)
-
-                classes = predictions.topk(k=1)[1].view(-1).cpu().numpy()
-
-                pd_class = np.concatenate((pd_class, classes))
-                pd_peaks = np.concatenate((pd_peaks, batch["peak"]))
-
-        pd_class = pd_class.astype(int)
-        pd_peaks = pd_peaks.astype(int)
-
-        annotations = []
-        for label, peak in zip(pd_class, pd_peaks):
-            if (
+                peak < len(self.pipeline_loader.dataset.signal)
+                and self.mapper[label] != "N"
+            ):
+                annotations.append(
+                    {
+                        "x": peak,
+                        "y": self.pipeline_loader.dataset.signal[peak],
+                        "text": self.mapper[label],
+                        "xref": "x",
+                        "yref": "y",
