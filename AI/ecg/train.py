0fa148bf967a1326e83304fbe1653992300ffb4f
naveenkurra <naveenkurra@velozityglobal.com>
2024-08-30 13:36:50 +0000

    Implement curriculum learning in ecg/train (lines 0-1)

--- AI/ecg/train.py
+++ AI/ecg/train.py
@@ -1,7 +1,1 @@
-    print("Loading dev set...")
-    dev = load.load_dataset(params['dev'])
-    print("Building preprocessor...")
-    preproc = load.Preproc(*train)
-    print("Training size: " + str(len(train[0])) + " examples.")
-    print("Dev size: " + str(len(dev[0])) + " examples.")
 
