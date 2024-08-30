361bf457fe3c0e3029b4bc86cc0d4ed91e956163
naveenkurra <naveenkurra@velozityglobal.com>
2024-08-30 18:52:39 +0000

    Add support for distributed training in dataloaders/dataset1d (lines 0-3)

--- AI/dataloaders/dataset1d.py
+++ AI/dataloaders/dataset1d.py
@@ -1,13 +1,3 @@
-            raise Exception("No MLII LEAD")
+        img = img.reshape(1, img.shape[0])
 
-        self.peaks = find_peaks(self.signal, distance=180)[0]
-        mask_left = (self.peaks - self.mode // 2) > 0
-        mask_right = (self.peaks + self.mode // 2) < len(self.signal)
-        mask = mask_left & mask_right
-        self.peaks = self.peaks[mask]
-
-    def __getitem__(self, index):
-        peak = self.peaks[index]
-        left, right = peak - self.mode // 2, peak + self.mode // 2
-
-        img = self.signal[left:right]
+        return {"image": img, "peak": peak}
