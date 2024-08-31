b1e8e8c1230309e78f03b461d55809ec63007efa
naveenkurra <naveenkurra@velozityglobal.com>
2024-08-31 18:40:34 +0000

    Implement old_keras_impl/run in neural network architecture (lines 0-9)

--- AI/old_keras_impl/run.py
+++ AI/old_keras_impl/run.py
@@ -1,3 +1,9 @@
-# in numpy if arrays are not the same shape they will not appear in the .shape method
-print("shape: {}".format(shape))
+model = Sequential()
+model.add(Convolution1D(nb_filters, 3, input_shape=shape, activation='relu'))
+model.add(Dropout(0.25))
+model.add(Convolution1D(nb_filters, 3, activation='relu'))
+model.add(Dropout(0.25))
 
+model.add(Flatten())
+model.add(Dense(256, activation='relu'))
+model.add(Dropout(0.5))
