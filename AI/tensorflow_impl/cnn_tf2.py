7600f5d89dec4f652631ab8f3f9990111414b914
naveenkurra <naveenkurra@velozityglobal.com>
2024-09-02 18:19:22 +0000

    Refactor tensorflow_impl/cnn_tf2 for better code organization (lines 0-1)

--- AI/tensorflow_impl/cnn_tf2.py
+++ AI/tensorflow_impl/cnn_tf2.py
@@ -1,17 +1,1 @@
-                 dest="heart_diseases",
-                 default=["apnea-ecg", "svdb", "afdb"],
-                 choices=["apnea-ecg", "mitdb", "nsrdb", "svdb", "afdb"],
-                 help="Select the ECG diseases for the model")
-
-par.add_argument("--verbose", dest="verbose",
-                 action="store_true", default=False,
-                 help="Display information about minibatches")
-
-args = par.parse_args()
-
-# Parameters
-learning_rate = args.learning_rate
-epochs = args.epochs
-batch_size = args.batch_size
-display_step = args.display_step
-dropout = args.dropout
+restore_model = args.restore_model
