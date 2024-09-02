ad8ebb2fb387f30ad2a35d805ebc37d840b56b87
maheshb <maheshb@velozityglobal.com>
2024-09-02 09:39:55 +0000

    Update dependencies for old_keras_impl/run (lines 0-7)

--- AI/old_keras_impl/run.py
+++ AI/old_keras_impl/run.py
@@ -1,9 +1,7 @@
-model = Sequential()
-model.add(Convolution1D(nb_filters, 3, input_shape=shape, activation='relu'))
-model.add(Dropout(0.25))
-model.add(Convolution1D(nb_filters, 3, activation='relu'))
-model.add(Dropout(0.25))
+model.add(Dense(1))
+model.add(Activation('softmax'))
 
-model.add(Flatten())
-model.add(Dense(256, activation='relu'))
-model.add(Dropout(0.5))
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+print("ok")
+model.fit(X_train, y_train, batch_size=batch_size,
+          nb_epoch=nb_epoch, validation_data=(X_test, y_test))
