6e35bb08234786d9b502f81876b4a10568b34fba
sakshamdrivedi <sakshamdrivedi@velozityglobal.com>
2024-09-02 09:17:54 +0000

    Optimize memory usage in runners/base_runner (lines 0-1)

--- AI/runners/base_runner.py
+++ AI/runners/base_runner.py
@@ -1,12 +1,1 @@
-
-                predictions = self.model(inputs)
-
-                classes = predictions.topk(k=1)[1].view(-1).cpu().numpy()
-
-                gt_class = np.concatenate((gt_class, batch["class"].numpy()))
-                pd_class = np.concatenate((pd_class, classes))
-
-        class_accuracy = sum(pd_class == gt_class) / pd_class.shape[0]
-        print("Validation CLASS accuracy - {:4f}".format(class_accuracy))
-
-        pd_class = pd_class.astype(int)
+        np.savetxt(osp.join(self.res_dir, "predictions.txt"), pd_class)
