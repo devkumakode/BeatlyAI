cc2ff62ea916cc1c1a313a04122f0bdbf9cae8b7
sakshamdrivedi <sakshamdrivedi@velozityglobal.com>
2024-09-02 17:37:08 +0000

    Optimize ecg_tools/metrics for better inference time (lines 0-18)

--- AI/ecg_tools/metrics.py
+++ AI/ecg_tools/metrics.py
@@ -1,15 +1,18 @@
-        pass
+        cm = confusion_matrix(self.labels, self.predictions)
+        return cm
 
-    @property
-    def specificity(self):
-        pass
+    def confusion_matrix_image(self):
+        figure = ConfusionMatrixDisplay(self.confusion_matrix()).plot().figure_
 
-    @property
-    def recall(self):
-        pass
+        def get_img_from_fig(fig, dpi=180):
+            buf = io.BytesIO()
+            fig.savefig(buf, format="png", dpi=dpi)
+            buf.seek(0)
+            img_arr = np.frombuffer(buf.getvalue(), dtype=np.uint8)
+            buf.close()
+            img = cv2.imdecode(img_arr, 1)
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-    @property
-    def accuracy(self):
-        pass
+            return img
 
-    def confusion_matrix(self):
+        numpy_figure = get_img_from_fig(figure)
