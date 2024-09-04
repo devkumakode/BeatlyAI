f49b5920b7b6c10afc4b3efba1ddfba26f068c9b
devganesh <devganesh@velozityglobal.com>
2024-09-04 15:15:40 +0000

    Add unit tests for dataloaders/dataset2d (lines 0-7)

--- AI/dataloaders/dataset2d.py
+++ AI/dataloaders/dataset2d.py
@@ -1,13 +1,7 @@
-        self.mapper = json.load(open(mapping_path))
 
-    def __getitem__(self, index):
-        img = cv2.imread(self.data[index]["path"])
-        img = augment(**{"image": img})["image"]
+    def __len__(self):
+        return len(self.data)
 
-        return {"image": img, "class": self.mapper[self.data[index]["label"]]}
 
-    def get_dataloader(self, num_workers=4, batch_size=16, shuffle=True):
-        data_loader = DataLoader(
-            self, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers,
-        )
-        return data_loader
+def callback_get_label(dataset, idx):
+    return dataset[idx]["class"]
