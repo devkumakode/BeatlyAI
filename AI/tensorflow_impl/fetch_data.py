c7bb1b8941c07097c1680387cacb1ead4d72de50
naveenkurra <naveenkurra@velozityglobal.com>
2024-09-04 18:23:37 +0000

    Implement transfer learning for tensorflow_impl/fetch_data (lines 0-17)

--- AI/tensorflow_impl/fetch_data.py
+++ AI/tensorflow_impl/fetch_data.py
@@ -1,20 +1,17 @@
-    dataset_dir = "datasets/raws"
-    
-    def check_folder_existance():
-        if not os.path.isdir(dataset_dir):
-            print("Directory {} not found".format(dataset_dir))
-            print("Creating now...")
-            os.makedirs(dataset_dir)
+        except OSError as e:
+            if e.errno == errno.ENOENT:
+                print("rdsamp not installed, link to the installation guide in the README")
+                return False
 
-        for database in physionet:
-            folder = os.path.join(dataset_dir, database)
-            if not os.path.isdir(folder):
-                print("Directory {} not found".format(folder))
-                print("Creating now...")
-                os.makedirs(folder)
+        print("rdsamp installed check failed")
+        return False
 
-    def rdsamp_installed():
-        try:
-            subprocess.call(["rdsamp", "-h"], stdout=subprocess.DEVNULL,
-                                              stderr=subprocess.DEVNULL)
-            return True
+    def remove_unwanted_datasets():
+        if dataset_list:
+            unwanted_ds = physionet.keys() - dataset_list
+            for ds in unwanted_ds:
+                physionet.pop(ds, None)
+
+
+    remove_unwanted_datasets()
+    check_folder_existance()
