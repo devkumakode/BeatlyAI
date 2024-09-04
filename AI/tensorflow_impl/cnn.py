9e5563b911559f63d7028dc14be7e9617af41a18
sakshamdrivedi <sakshamdrivedi@velozityglobal.com>
2024-09-04 11:32:36 +0000

    Implement tensorflow_impl/cnn in neural network architecture (lines 0-3)

--- AI/tensorflow_impl/cnn.py
+++ AI/tensorflow_impl/cnn.py
@@ -1,16 +1,3 @@
-        self.saver = ModelSaver(save_dir="saved_models/cnn/")
-
-        logs_path = "tensorboard_data/cnn/"
-        self.tensorboard_handler = TensorBoardHandler(logs_path)
-        self.tensorboard_handler.add_histograms(self.weights)
-        self.tensorboard_handler.add_histograms(self.biases)
-
-        self.build()
-
-    def build(self):
-        dataset_len = []
-        for dataset in self.datasets:
-            dataset_len.append(len(dataset))
-
-        validation_size = int(0.1 * sum(dataset_len))
-
+        print("Validation size: {}".format(validation_size))
+        print("Total samples: {}".format(sum(dataset_len)))
+        print("Heart diseases: {}".format(', '.join(heart_diseases)))
