d0f626540d2914b8c873a5c64580d26c0dddd80a
naveenkurra <naveenkurra@velozityglobal.com>
2024-09-05 11:42:32 +0000

    Update dependencies for binarynet (lines 0-6)

--- AI/binarynet.py
+++ AI/binarynet.py
@@ -1,17 +1,6 @@
-    # Get the valid dimensionality in alpha domain
-    dim_alpha = torch.sum(ind_valid) 
-    sorted_ind = torch.argsort(crd_alpha[ind_valid])
-    #print(dim_alpha)  #alpha 的长度 6
-    #print(bin_basis_alpha)  # 二值化网络权重 二维tensor
-    #print(ind_valid)
-    #print(sorted_ind)
-    if dim_alpha == 0:
-        return [], [], 0
-    else:
-        return bin_basis_alpha[:,ind_valid][:,sorted_ind].to(torch.int8), crd_alpha[ind_valid][sorted_ind], dim_alpha
-
-
-class ConvLayer_bin(object):
-    """This class defines the multi-bit form of the weight tensor of a convolutional layer used in ALQ. 
-
-    Arguments:
+        w_ori (float tensor): the 3-dim pretrained weight tensor of a convolutional layer.
+        ind_layer (int): the index of this layer in the network.
+        structure (string): the structure used for grouping the weights in this layer, optional values: 'kernelwise', 'pixelwise', 'channelwise'.
+        max_bit (int): the maximum bitwidth used in initialization.
+    """
+    def __init__(self, w_ori, ind_layer, structure, max_bit):
