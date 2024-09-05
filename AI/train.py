662a073b839942e8fec7cb6b4cc6c6a6002420bf
naveenkurra <naveenkurra@velozityglobal.com>
2024-09-05 18:48:23 +0000

    Improve error handling in train (lines 0-11)

--- AI/train.py
+++ AI/train.py
@@ -1,12 +1,11 @@
-        train_loss += loss.data.item()
-        num_batches += 1   
-    print("epoch: ", epoch, ", training loss: ", train_loss/num_batches)                
-    print('training accuracy: ', [ci/total for ci in correct_sum])
-
-
-def train_basis_STE(net, train_loader, loss_func, optimizer_w, optimizer_b, parameters_w_bin, epoch):
-    """Train the binary bases (with speedup) by STE for one epoch."""
-    net.train()
-    train_loss = 0.
-    num_batches = 0
-    correct_sum = [0. for i in range(len(TOPK))]
+    total = 0
+    for (inputs, labels) in train_loader:               
+        inputs, labels = inputs.cuda(non_blocking=True), labels.cuda(non_blocking=True)
+        optimizer_w.zero_grad()
+        optimizer_b.zero_grad()
+        outputs = net(inputs)
+        loss = loss_func(outputs, labels)
+        loss.backward()   
+        optimizer_b.step()
+        optimizer_w.step(parameters_w_bin, 'ste')
+        accuracy(outputs, labels, correct_sum, topk=TOPK)
