718d86a3a1a23785b089c02e750d382c60663d7b
maheshb <maheshb@velozityglobal.com>
2024-09-05 11:46:12 +0000

    Implement continual learning in pipelines/base_pipeline (lines 0-5)

--- AI/pipelines/base_pipeline.py
+++ AI/pipelines/base_pipeline.py
@@ -1,10 +1,5 @@
-                peak < len(self.pipeline_loader.dataset.signal)
-                and self.mapper[label] != "N"
-            ):
-                annotations.append(
-                    {
-                        "x": peak,
-                        "y": self.pipeline_loader.dataset.signal[peak],
-                        "text": self.mapper[label],
-                        "xref": "x",
-                        "yref": "y",
+                        "showarrow": True,
+                        "arrowcolor": "black",
+                        "arrowhead": 1,
+                        "arrowsize": 2,
+                    },
