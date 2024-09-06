637d5fb58446760a65184f035f7dcdcbfcf5d5b8
sakshamdrivedi <sakshamdrivedi@velozityglobal.com>
2024-09-06 11:50:18 +0000

    Implement adversarial training in scripts/annotation-generation-1d (lines 0-4)

--- AI/scripts/annotation-generation-1d.py
+++ AI/scripts/annotation-generation-1d.py
@@ -1,5 +1,4 @@
-    data = data[data["label"].isin(classes)]
-    data = data.sample(frac=1, random_state=random_state)
-
-    val_ids = []
-    for cl in classes:
+        val_ids.extend(
+            data[data["label"] == cl]
+            .sample(frac=val_size, random_state=random_state)
+            .index,
