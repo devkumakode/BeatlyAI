b1fedb554e6b4d0a3db6906a95939892117902e6
devganesh <devganesh@velozityglobal.com>
2024-09-06 18:18:13 +0000

    Fix bug in tensorflow_impl/utils (lines 0-20)

--- AI/tensorflow_impl/utils.py
+++ AI/tensorflow_impl/utils.py
@@ -1,9 +1,20 @@
+    return np.array(labels)
 
-def get_labels(datasets):
-    nr_classes = len(datasets)
-    labels = []
-    for i in range(nr_classes):
-         for _ in range(len(datasets[i])):
-             class_label = [0] * nr_classes
-             class_label[i] = 1
-             labels.append(class_label)
+def get_datasets(diseases, nr_inputs):
+    datasets = []
+    for disease in diseases:
+        with open('datasets/processed/{}.csv'.format(disease)) as dis:
+            dataset = np.loadtxt(dis)
+            datasets.append(dataset)
+
+    for idx, dataset in enumerate(datasets):
+        _cutoff_val = reduce_ds(len(dataset), nr_inputs)
+        _dataset = dataset[:_cutoff_val]
+        datasets[idx] = _dataset.reshape(-1, nr_inputs)
+        
+    return datasets
+
+def check_processed_dir_existance():
+    if not os.listdir('datasets/processed'):
+        print("You have to first process the data, " +
+              "please call the download_data script")
