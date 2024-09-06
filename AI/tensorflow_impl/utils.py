7cf91d60b2d2e4fbcfcf26ad3d2192c0e81d3655
devganesh <devganesh@velozityglobal.com>
2024-09-06 11:39:11 +0000

    Add type hints to tensorflow_impl/utils (lines 0-9)

--- AI/tensorflow_impl/utils.py
+++ AI/tensorflow_impl/utils.py
@@ -1,19 +1,9 @@
-        model_dir = self.save_dir + str(model_name) + self.model_ext
-        self.saver.restore(sess, model_dir)
-        print("Model restored from {}".format(model_dir))
 
-
-def shuffle_tensors(x, y):
-    assert len(x) == len(y), "Lengths don't match"
-    if type(x) == list or type(y) == list:
-        x = np.array(x)
-        y = np.array(y)
-
-    perm = np.random.permutation(len(x))
-    return x[perm], y[perm]
-
-def next_minibatch(x, y, minibatch_size):
-    assert x.shape[0] == y.shape[0], "Shapes don't match"
-    for i in range(0, x.shape[0] - minibatch_size + 1, minibatch_size):
-        slice_range = slice(i, i + minibatch_size)
-        yield x[slice_range], y[slice_range]
+def get_labels(datasets):
+    nr_classes = len(datasets)
+    labels = []
+    for i in range(nr_classes):
+         for _ in range(len(datasets[i])):
+             class_label = [0] * nr_classes
+             class_label[i] = 1
+             labels.append(class_label)
