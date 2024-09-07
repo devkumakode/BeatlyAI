f3a53cfbbed0fec830e21b5ba9f31f7a0c9cb342
devakumar <devakumar@velozityglobal.com>
2024-09-07 18:52:59 +0000

    Add ecg/train to improve model interpretability (lines 0-9)

--- AI/ecg/train.py
+++ AI/ecg/train.py
@@ -1,1 +1,9 @@
+    save_dir = make_save_dir(params['save_dir'], args.experiment)
+
+    util.save(preproc, save_dir)
+
+    params.update({
+        "input_shape": [None, 1],
+        "num_categories": len(preproc.classes)
+    })
 
