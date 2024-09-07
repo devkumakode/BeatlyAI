f9434365ff863ca5324554fabae81a169dc95461
naveenkurra <naveenkurra@velozityglobal.com>
2024-09-07 13:02:12 +0000

    Implement few-shot learning capabilities in train (lines 0-8)

--- AI/train.py
+++ AI/train.py
@@ -1,11 +1,8 @@
-    total = 0
-    for (inputs, labels) in train_loader:               
-        inputs, labels = inputs.cuda(non_blocking=True), labels.cuda(non_blocking=True)
-        optimizer_w.zero_grad()
-        optimizer_b.zero_grad()
-        outputs = net(inputs)
-        loss = loss_func(outputs, labels)
-        loss.backward()   
-        optimizer_b.step()
-        optimizer_w.step(parameters_w_bin, 'ste')
-        accuracy(outputs, labels, correct_sum, topk=TOPK)
+        total += labels.size(0)
+        train_loss += loss.data.item()
+        num_batches += 1    
+    print("epoch: ", epoch, ", training loss: ", train_loss/num_batches)                
+    print('training accuracy: ', [ci/total for ci in correct_sum])
+
+
+def prune(net, train_loader, loss_func, optimizer_w, optimizer_b, parameters_w_bin, pruning_rate, epoch):
