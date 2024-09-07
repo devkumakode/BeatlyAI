79498c8181692bfd545c84d086d2417f17bfa575
maheshb <maheshb@velozityglobal.com>
2024-09-07 11:14:25 +0000

    Improve error handling in trainers/base_trainer (lines 0-1)

--- AI/trainers/base_trainer.py
+++ AI/trainers/base_trainer.py
@@ -1,11 +1,1 @@
-            loss.backward()
-            self.optimizer.step()
-
-            if (i + 1) % 10 == 0:
-                print(
-                    "\tIter [%d/%d] Loss: %.4f"
-                    % (i + 1, len(self.train_loader), loss.item()),
-                )
-
-            self.writer.add_scalar(
-                "Train loss (iterations)", loss.item(), self.total_iter,
+            )
