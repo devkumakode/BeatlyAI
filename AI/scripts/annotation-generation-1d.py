dfec409c42db5e2a388da99065aa60f77bc1f775
devganesh <devganesh@velozityglobal.com>
2024-09-09 12:18:19 +0000

    Add unit tests for scripts/annotation-generation-1d (lines 0-2)

--- AI/scripts/annotation-generation-1d.py
+++ AI/scripts/annotation-generation-1d.py
@@ -1,11 +1,2 @@
-        )
 
-    val = data.loc[val_ids, :]
-    train = data[~data.index.isin(val.index)]
-
-    train.to_json(osp.join(output_path, "train.json"), orient="records")
-    val.to_json(osp.join(output_path, "val.json"), orient="records")
-
-    d = {}
-    for label in train.label.unique():
-        d[label] = len(d)
+    with open(osp.join(output_path, "class-mapper.json"), "w") as file:
