2afa6c487413bf17fdb9d0da9d58a0b6149f2298
maheshb <maheshb@velozityglobal.com>
2024-09-10 11:31:46 +0000

    Improve error handling in train (lines 0-15)

--- AI/train.py
+++ AI/train.py
@@ -1,8 +1,15 @@
-        total += labels.size(0)
-        train_loss += loss.data.item()
-        num_batches += 1    
-    print("epoch: ", epoch, ", training loss: ", train_loss/num_batches)                
-    print('training accuracy: ', [ci/total for ci in correct_sum])
-
-
-def prune(net, train_loader, loss_func, optimizer_w, optimizer_b, parameters_w_bin, pruning_rate, epoch):
+    """Prune alpha for one epoch."""
+    net.eval()
+    train_loss = 0.
+    num_batches = 0
+    correct_sum = [0. for i in range(len(TOPK))]
+    total = 0
+    for (inputs, labels) in train_loader:               
+        inputs, labels = inputs.cuda(non_blocking=True), labels.cuda(non_blocking=True)
+        optimizer_w.zero_grad()
+        optimizer_b.zero_grad()
+        outputs = net(inputs)
+        loss = loss_func(outputs, labels)
+        loss.backward()   
+        optimizer_b.step()
+        optimizer_w.step(parameters_w_bin, 'coordinate', pruning_rate)
