917afcaee2ca1a8514eb19998b7f1002f5a59379
devganesh <devganesh@velozityglobal.com>
2024-09-10 09:00:02 +0000

    Implement attention mechanism in tensorflow_impl/cnn_tf2 (lines 0-11)

--- AI/tensorflow_impl/cnn_tf2.py
+++ AI/tensorflow_impl/cnn_tf2.py
@@ -1,1 +1,11 @@
-restore_model = args.restore_model
+freeze = args.freeze
+heart_diseases = args.heart_diseases
+verbose = args.verbose
+
+# Network Parameters
+n_inputs = 350
+n_classes = len(heart_diseases)
+
+check_processed_dir_existance()
+
+
