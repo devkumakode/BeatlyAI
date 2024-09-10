5ef581b4cfbd4f05562b0c3f93ac24282dbcd24f
devakumar <devakumar@velozityglobal.com>
2024-09-10 12:54:18 +0000

    Refactor tensorflow_impl/cnn for better code organization (lines 0-1)

--- AI/tensorflow_impl/cnn.py
+++ AI/tensorflow_impl/cnn.py
@@ -1,3 +1,1 @@
-        print("Validation size: {}".format(validation_size))
-        print("Total samples: {}".format(sum(dataset_len)))
-        print("Heart diseases: {}".format(', '.join(heart_diseases)))
+
