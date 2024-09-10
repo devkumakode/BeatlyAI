51c6bb7da028525ee60fb7f48b697c63f5827544
naveenkurra <naveenkurra@velozityglobal.com>
2024-09-10 16:34:06 +0000

    Add type hints to ecg_tools/metrics (lines 0-4)

--- AI/ecg_tools/metrics.py
+++ AI/ecg_tools/metrics.py
@@ -1,18 +1,4 @@
-        cm = confusion_matrix(self.labels, self.predictions)
-        return cm
-
-    def confusion_matrix_image(self):
-        figure = ConfusionMatrixDisplay(self.confusion_matrix()).plot().figure_
-
-        def get_img_from_fig(fig, dpi=180):
-            buf = io.BytesIO()
-            fig.savefig(buf, format="png", dpi=dpi)
-            buf.seek(0)
-            img_arr = np.frombuffer(buf.getvalue(), dtype=np.uint8)
-            buf.close()
-            img = cv2.imdecode(img_arr, 1)
-            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
-            return img
-
-        numpy_figure = get_img_from_fig(figure)
+        plt.close()
+        plt.clf()
+        plt.cla()
+        return numpy_figure
