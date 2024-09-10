16c768dbfca5898663718416e18ef950dfdbd95b
devganesh <devganesh@velozityglobal.com>
2024-09-10 18:19:13 +0000

    Fix bug in pipelines/base_pipeline (lines 0-13)

--- AI/pipelines/base_pipeline.py
+++ AI/pipelines/base_pipeline.py
@@ -1,5 +1,13 @@
-                        "showarrow": True,
-                        "arrowcolor": "black",
-                        "arrowhead": 1,
-                        "arrowsize": 2,
-                    },
+                )
+
+        if osp.exists(self.config["ecg_data"] + ".atr"):
+            ann = wfdb.rdann(self.config["ecg_data"], extension="atr")
+            for label, peak in zip(ann.symbol, ann.sample):
+                if peak < len(self.pipeline_loader.dataset.signal) and label != "N":
+                    annotations.append(
+                        {
+                            "x": peak,
+                            "y": self.pipeline_loader.dataset.signal[peak] - 0.1,
+                            "text": label,
+                            "xref": "x",
+                            "yref": "y",
