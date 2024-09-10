6d542f605f722d24d914de221d35af811eb8abda
sakshamdrivedi <sakshamdrivedi@velozityglobal.com>
2024-09-10 09:18:35 +0000

    Update documentation for ECGnet (lines 0-1)

--- AI/ECGnet.py
+++ AI/ECGnet.py
@@ -1,8 +1,1 @@
-        ClassesNum = len(classes)
-        X = list()
-        y = list()
 
-        for root, dirs, files in os.walk(dataset_path, topdown=False):
-            for name in files:
-                data_train = scio.loadmat(
-                    os.path.join(root, name))  # 取出字典里的value
