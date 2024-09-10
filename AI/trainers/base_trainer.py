c1b578924c96b9b13c2b71d282c001be03621690
sakshamdrivedi <sakshamdrivedi@velozityglobal.com>
2024-09-10 17:50:55 +0000

    Add data augmentation techniques in trainers/base_trainer (lines 0-12)

--- AI/trainers/base_trainer.py
+++ AI/trainers/base_trainer.py
@@ -1,1 +1,12 @@
-            )
+            self.total_iter += 1
+
+        total_loss /= len(self.train_loader)
+        class_accuracy = sum(pd_class == gt_class) / pd_class.shape[0]
+
+        print("Train loss - {:4f}".format(total_loss))
+        print("Train CLASS accuracy - {:4f}".format(class_accuracy))
+
+        self.writer.add_scalar("Train loss (epochs)", total_loss, self.training_epoch)
+        self.writer.add_scalar(
+            "Train CLASS accuracy", class_accuracy, self.training_epoch,
+        )
