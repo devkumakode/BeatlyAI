d3ed76a4f684360a07964f0bb91aca66f0cb0976
devganesh <devganesh@velozityglobal.com>
2024-09-11 14:36:28 +0000

    Refactor tensorflow_impl/utils for better code organization (lines 0-2)

--- AI/tensorflow_impl/utils.py
+++ AI/tensorflow_impl/utils.py
@@ -1,20 +1,2 @@
-    return np.array(labels)
+        sys.exit(1)
 
-def get_datasets(diseases, nr_inputs):
-    datasets = []
-    for disease in diseases:
-        with open('datasets/processed/{}.csv'.format(disease)) as dis:
-            dataset = np.loadtxt(dis)
-            datasets.append(dataset)
-
-    for idx, dataset in enumerate(datasets):
-        _cutoff_val = reduce_ds(len(dataset), nr_inputs)
-        _dataset = dataset[:_cutoff_val]
-        datasets[idx] = _dataset.reshape(-1, nr_inputs)
-        
-    return datasets
-
-def check_processed_dir_existance():
-    if not os.listdir('datasets/processed'):
-        print("You have to first process the data, " +
-              "please call the download_data script")
