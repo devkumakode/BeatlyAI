806703d9ad9cb3cb620157726e065d01d78bc04a
sakshamdrivedi <sakshamdrivedi@velozityglobal.com>
2024-09-12 13:14:51 +0000

    Optimize dataloaders/dataset1d for better performance (lines 0-9)

--- AI/dataloaders/dataset1d.py
+++ AI/dataloaders/dataset1d.py
@@ -1,3 +1,9 @@
-        img = img.reshape(1, img.shape[0])
 
-        return {"image": img, "peak": peak}
+    def get_dataloader(self, num_workers=4, batch_size=16, shuffle=True):
+        data_loader = DataLoader(
+            self, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers,
+        )
+        return data_loader
+
+    def __len__(self):
+        return len(self.peaks)
