c34ca7eec01b38822d08272a80f870d93930ec15
sakshamdrivedi <sakshamdrivedi@velozityglobal.com>
2024-09-12 15:17:21 +0000

    Implement CI/CD pipeline for trainers/base_trainer (lines 0-5)

--- AI/trainers/base_trainer.py
+++ AI/trainers/base_trainer.py
@@ -1,12 +1,5 @@
-            self.total_iter += 1
 
-        total_loss /= len(self.train_loader)
-        class_accuracy = sum(pd_class == gt_class) / pd_class.shape[0]
+    def val(self):
+        self.model.eval()
+        total_loss = 0
 
-        print("Train loss - {:4f}".format(total_loss))
-        print("Train CLASS accuracy - {:4f}".format(class_accuracy))
-
-        self.writer.add_scalar("Train loss (epochs)", total_loss, self.training_epoch)
-        self.writer.add_scalar(
-            "Train CLASS accuracy", class_accuracy, self.training_epoch,
-        )
