981bf5139ea4cf887832942944628e4df1d11910
sakshamdrivedi <sakshamdrivedi@velozityglobal.com>
2024-09-12 09:42:55 +0000

    Add data augmentation techniques in tensorflow_impl/cnn_tf2 (lines 0-20)

--- AI/tensorflow_impl/cnn_tf2.py
+++ AI/tensorflow_impl/cnn_tf2.py
@@ -1,11 +1,20 @@
-freeze = args.freeze
-heart_diseases = args.heart_diseases
-verbose = args.verbose
+class CNN:
+    def __init__(self):
+        self.datasets = get_datasets(heart_diseases, n_inputs)
+        self.label_data = get_labels(self.datasets)
+        self.callbacks = []
 
-# Network Parameters
-n_inputs = 350
-n_classes = len(heart_diseases)
+        # Initialize callbacks
+        tensorboard_logs_path = "tensorboard_data/cnn/"
+        tb_callback = tf.keras.callbacks.TensorBoard(log_dir=tensorboard_logs_path,
+                                                     histogram_freq=1, write_graph=True,
+                                                     embeddings_freq=1)
 
-check_processed_dir_existance()
+        # load_weights_on_restart will read the filepath of the weights if it exists and it will
+        # load the weights into the model
+        cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath="saved_models/cnn/model.hdf5",
+                                                         save_best_only=True,
+                                                         save_weights_only=True,
+                                                         load_weights_on_restart=restore_model)
 
-
+        self.callbacks.extend([tb_callback, cp_callback])
