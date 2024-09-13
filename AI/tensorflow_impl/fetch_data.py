4c83fbefc1161dbef8b459250b56f7a288841759
sakshamdrivedi <sakshamdrivedi@velozityglobal.com>
2024-09-13 16:46:47 +0000

    Optimize memory usage in tensorflow_impl/fetch_data (lines 0-8)

--- AI/tensorflow_impl/fetch_data.py
+++ AI/tensorflow_impl/fetch_data.py
@@ -1,1 +1,8 @@
-                print("Downloading with command {}...".format(cmd))
+                subprocess.check_call(cmd, shell=True)
+            except Exception as e:
+                print("Failed to execute command: {} with exception: {}".format(cmd, e))
+                if os.path.exists(csv_file_path):
+                    os.remove(csv_file_path)
+
+        if os.path.isdir(database_dir) and not os.listdir(database_dir):
+            cmd = "rm -rf {}".format(database_dir)
