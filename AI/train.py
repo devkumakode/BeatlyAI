d3d0663a0d835ff089168e72a07a3c931c342b41
naveenkurra <naveenkurra@velozityglobal.com>
2024-09-13 13:54:16 +0000

    Add explainability features to train (lines 0-13)

--- AI/train.py
+++ AI/train.py
@@ -1,16 +1,13 @@
-        accuracy(outputs, labels, correct_sum, topk=TOPK)
-        train_loss += loss.data.item()
-        num_batches += 1
-        total += labels.size(0)
-    print("epoch: ", epoch, ", pruning loss: ", train_loss/num_batches)                
-    print('pruning accuracy: ', [ci/total for ci in correct_sum])
-    num_weight_layer = 0.
-    num_bit_layer = 0.
-    print('currrent number of binary filters per layer: ')
-    for p_w_bin in parameters_w_bin:
-        print(p_w_bin.num_bin_filter)
-    print('currrent average bitwidth per layer: ')
-    for p_w_bin in parameters_w_bin:
-        num_weight_layer += p_w_bin.num_weight
-        num_bit_layer += p_w_bin.avg_bit*p_w_bin.num_weight
-        print(p_w_bin.avg_bit)
+    print('currrent average bitwidth: ', num_bit_layer/num_weight_layer)
+
+ 
+def initialize(net, train_loader, loss_func, structure, num_subchannel, max_bit):
+    """Initialize the weight tensors of all layers to multi-bit form using structured sketching. 
+    Return the iterator over all weight parameters, the iterator over all other parameters, and the iterator over the multi-bit forms of all weight parameters.  
+    """
+    parameters_w = []
+    parameters_b = []
+    parameters_w_bin = []
+    i = 0
+    for name, param in net.named_parameters():
+        # Only initialize weight tensors to multi-bit form
