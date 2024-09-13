c68c122778e55db8da9960e6c9d827ea4c71cfbd
maheshb <maheshb@velozityglobal.com>
2024-09-13 18:27:53 +0000

    Implement gradient checkpointing in tensorflow_impl/fetch_data (lines 0-1)

--- AI/tensorflow_impl/fetch_data.py
+++ AI/tensorflow_impl/fetch_data.py
@@ -1,16 +1,1 @@
-    if not rdsamp_installed():
-        sys.exit(1)
-
-    for database, samples in physionet.items():
-        print("Downloading {}".format(database))
-        database_dir = os.path.join(dataset_dir, database)
-        for sample in samples:
-            csv_file_path = os.path.join(database_dir, sample) + ".csv"
-            if os.path.exists(csv_file_path):
-                print("File {} exists. Skipping download...".format(csv_file_path))
-                continue
-
-            sample_path = os.path.join(database, sample)
-            cmd = ("rdsamp -r {} -c -H -f 0" +
-                   " -t 60 -v -pe > {}").format(sample_path, csv_file_path)
-            try:
+                print("Downloading with command {}...".format(cmd))
