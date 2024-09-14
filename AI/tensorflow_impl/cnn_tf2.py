d4130eab91ef21304c93ec7c0a3e67aed07b990e
devakumar <devakumar@velozityglobal.com>
2024-09-14 09:08:21 +0000

    Implement early stopping in tensorflow_impl/cnn_tf2 (lines 0-5)

--- AI/tensorflow_impl/cnn_tf2.py
+++ AI/tensorflow_impl/cnn_tf2.py
@@ -1,17 +1,5 @@
 
-        self.set_data()
-        self.define_model()
+        self.split_data(concat_dataset, validation_size)
 
-    def set_data(self):
-        dataset_len = []
-        for dataset in self.datasets:
-            dataset_len.append(len(dataset))
-
-        # validation on 10% of the training data
-        validation_size = 0.1
-
-        print("Validation percentage: {}%".format(validation_size*100))
-        print("Total samples: {}".format(sum(dataset_len)))
-        print("Heart diseases: {}".format(', '.join(heart_diseases)))
-
-        concat_dataset = np.concatenate(self.datasets)
+        # Reshape input so that we can feed it to the conv layer
+        self.X_train = tf.reshape(self.X_train, shape=[-1, n_inputs, 1])
