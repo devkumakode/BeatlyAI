6fceede396d4ffa4b4bc7d45f8ddf25afca822b7
maheshb <maheshb@velozityglobal.com>
2024-09-14 18:24:10 +0000

    Optimize memory usage in tensorflow_impl/cnn (lines 0-12)

--- AI/tensorflow_impl/cnn.py
+++ AI/tensorflow_impl/cnn.py
@@ -1,1 +1,12 @@
+        # Shuffle the input, helps training
+        concat_dataset = np.concatenate(self.datasets)
+        concat_dataset, self.label_data = shuffle_tensors(concat_dataset, self.label_data)
 
+        # split training and testing sets
+        self.X_train, self.X_test = np.split(concat_dataset,
+                                             [len(concat_dataset)-validation_size])
+
+        self.Y_train, self.Y_test = np.split(self.label_data,
+                                             [len(self.label_data)-validation_size])
+
+        if verbose:
