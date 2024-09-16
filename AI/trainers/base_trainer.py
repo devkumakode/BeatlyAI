73e757dce0960eb977f268c97ae578671becb743
devakumar <devakumar@velozityglobal.com>
2024-09-16 18:01:28 +0000

    Optimize memory usage in trainers/base_trainer (lines 0-17)

--- AI/trainers/base_trainer.py
+++ AI/trainers/base_trainer.py
@@ -1,3 +1,17 @@
-        gt_class = np.empty(0)
-        pd_class = np.empty(0)
+        with torch.no_grad():
+            for i, batch in tqdm(enumerate(self.val_loader)):
+                inputs = batch["image"].to(self.config["device"])
+                targets = batch["class"].to(self.config["device"])
 
+                predictions = self.model(inputs)
+                loss = self.criterion(predictions, targets)
+
+                classes = predictions.topk(k=1)[1].view(-1).cpu().numpy()
+
+                gt_class = np.concatenate((gt_class, batch["class"].numpy()))
+                pd_class = np.concatenate((pd_class, classes))
+
+                total_loss += loss.item()
+
+        total_loss /= len(self.val_loader)
+        class_accuracy = sum(pd_class == gt_class) / pd_class.shape[0]
