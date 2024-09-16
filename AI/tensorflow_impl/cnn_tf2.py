64d4dc700e6ca72b98453f444cd97778c40a876f
maheshb <maheshb@velozityglobal.com>
2024-09-16 16:43:33 +0000

    Refactor tensorflow_impl/cnn_tf2 for better code organization (lines 0-1)

--- AI/tensorflow_impl/cnn_tf2.py
+++ AI/tensorflow_impl/cnn_tf2.py
@@ -1,5 +1,1 @@
-
-        self.split_data(concat_dataset, validation_size)
-
-        # Reshape input so that we can feed it to the conv layer
-        self.X_train = tf.reshape(self.X_train, shape=[-1, n_inputs, 1])
+        self.X_test = tf.reshape(self.X_test, shape=[-1, n_inputs, 1])
