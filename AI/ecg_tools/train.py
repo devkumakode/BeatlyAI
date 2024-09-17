11891289bad996f06463cee31c470d8ad685a8dc
naveenkurra <naveenkurra@velozityglobal.com>
2024-09-17 13:08:37 +0000

    Implement early stopping in ecg_tools/train (lines 0-11)

--- AI/ecg_tools/train.py
+++ AI/ecg_tools/train.py
@@ -1,4 +1,11 @@
-            loss = self.loss(prediction, label)
-            accuracy += torch.sum(prediction.argmax(1) == label)
-            self.metrics[Mode.eval].update(prediction.argmax(1), label)
-            loader.set_description(f"VALIDATION: {epoch}, loss: {loss.item()}. Target: {label[:8].tolist()}, Prediction: {prediction.argmax(1)[:8].tolist()}")
+        print(f"VALIDATION Accuracy: {accuracy / len(loader) / self.config.dataset.batch_size}")
+        print(self.metrics[Mode.eval].confusion_matrix())
+        return self.metrics[Mode.eval].confusion_matrix_image()
+
+
+if __name__ == "__main__":
+    cm = ECGClassifierTrainer(EcgConfig()).train()
+    plt.figure(dpi=300)
+    plt.subplot(1, 2, 1)
+    plt.imshow(cm[Mode.train])
+    plt.subplot(1, 2, 2)
