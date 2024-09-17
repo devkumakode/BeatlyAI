7481c91207c59431a14f9557900fd78ea94452fe
devakumar <devakumar@velozityglobal.com>
2024-09-17 17:22:58 +0000

    Add logging to ecg_tools/train (lines 0-2)

--- AI/ecg_tools/train.py
+++ AI/ecg_tools/train.py
@@ -1,11 +1,2 @@
-        print(f"VALIDATION Accuracy: {accuracy / len(loader) / self.config.dataset.batch_size}")
-        print(self.metrics[Mode.eval].confusion_matrix())
-        return self.metrics[Mode.eval].confusion_matrix_image()
-
-
-if __name__ == "__main__":
-    cm = ECGClassifierTrainer(EcgConfig()).train()
-    plt.figure(dpi=300)
-    plt.subplot(1, 2, 1)
-    plt.imshow(cm[Mode.train])
-    plt.subplot(1, 2, 2)
+    plt.imshow(cm[Mode.eval])
+    plt.savefig("training_results.png")
