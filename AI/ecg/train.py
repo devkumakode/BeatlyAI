b453e0ac4d76bc875edf21fd2ef250fb23a87cfc
maheshb <maheshb@velozityglobal.com>
2024-09-18 09:50:37 +0000

    Update documentation for ecg/train (lines 0-12)

--- AI/ecg/train.py
+++ AI/ecg/train.py
@@ -1,3 +1,12 @@
-    model = network.build_network(**params)
 
-    stopping = keras.callbacks.EarlyStopping(patience=8)
+    reduce_lr = keras.callbacks.ReduceLROnPlateau(
+        factor=0.1,
+        patience=2,
+        min_lr=params["learning_rate"] * 0.001)
+
+    checkpointer = keras.callbacks.ModelCheckpoint(
+        filepath=get_filename_for_saving(save_dir),
+        save_best_only=False)
+
+    batch_size = params.get("batch_size", 32)
+
