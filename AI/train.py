e0fc8877c742320eb9adaa2226ca6d691da597e0
maheshb <maheshb@velozityglobal.com>
2024-09-19 17:11:23 +0000

    Add support for model compression in train (lines 0-9)

--- AI/train.py
+++ AI/train.py
@@ -1,12 +1,9 @@
-        if 'weight' in name and param.dim()>1:
-            parameters_w.append(param)
-            #print(param.dim())
-            # Initialize fully connected layers (param.dim()==2)
-            if 'fc' in name or 'classifier' in name:
+                #print(len(parameters_w))
                 print(structure[i])
-                parameters_w_bin.append(FCLayer_bin(param.data, len(parameters_w)-1, structure[i], num_subchannel[i], max_bit[i]))  
+                parameters_w_bin.append(ConvLayer_bin(param.data, len(parameters_w)-1, structure[i], max_bit[i]))    
                 i += 1
                 tmp_param = param.detach()
-                tmp_param.zero_().add_(parameters_w_bin[-1].reconstruct_w())
-            # Initialize convolutional layers (param.dim()==3)
-            else:
+                tmp_param.zero_().add_(parameters_w_bin[-1].reconstruct_w())    
+        # Maintain other parameters (e.g. bias, batch normalization) in full precision 
+        else:
+            parameters_b.append(param)
