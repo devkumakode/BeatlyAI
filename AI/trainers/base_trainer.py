db0699cd85b8756d2b78db532bda0c478bc83b41
maheshb <maheshb@velozityglobal.com>
2024-09-20 12:05:58 +0000

    Implement CI/CD pipeline for trainers/base_trainer (lines 0-15)

--- AI/trainers/base_trainer.py
+++ AI/trainers/base_trainer.py
@@ -1,17 +1,15 @@
-        with torch.no_grad():
-            for i, batch in tqdm(enumerate(self.val_loader)):
-                inputs = batch["image"].to(self.config["device"])
-                targets = batch["class"].to(self.config["device"])
 
-                predictions = self.model(inputs)
-                loss = self.criterion(predictions, targets)
+        print("Validation loss - {:4f}".format(total_loss))
+        print("Validation CLASS accuracy - {:4f}".format(class_accuracy))
 
-                classes = predictions.topk(k=1)[1].view(-1).cpu().numpy()
+        self.writer.add_scalar("Validation loss", total_loss, self.training_epoch)
+        self.writer.add_scalar(
+            "Validation CLASS accuracy", class_accuracy, self.training_epoch,
+        )
 
-                gt_class = np.concatenate((gt_class, batch["class"].numpy()))
-                pd_class = np.concatenate((pd_class, classes))
-
-                total_loss += loss.item()
-
-        total_loss /= len(self.val_loader)
-        class_accuracy = sum(pd_class == gt_class) / pd_class.shape[0]
+    def loop(self):
+        for epoch in range(self.training_epoch, self.epochs):
+            print("Epoch - {}".format(self.training_epoch + 1))
+            self.train_epoch()
+            save_checkpoint(
+                {
