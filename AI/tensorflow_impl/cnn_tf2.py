7e7cf86ac13dee969f4c4248369ec9b8266193b5
naveenkurra <naveenkurra@velozityglobal.com>
2024-09-20 14:28:05 +0000

    Add support for mixed precision training in tensorflow_impl/cnn_tf2 (lines 0-17)

--- AI/tensorflow_impl/cnn_tf2.py
+++ AI/tensorflow_impl/cnn_tf2.py
@@ -1,1 +1,17 @@
-        self.X_val = tf.reshape(self.X_val, shape=[-1, n_inputs, 1])
+
+        if verbose:
+            print("X_train shape: {}".format(self.X_train.shape))
+            print("Y_train shape: {}".format(self.Y_train.shape))
+            print("X_test shape: {}".format(self.X_test.shape))
+            print("Y_test shape: {}".format(self.Y_test.shape))
+            print("X_val shape: {}".format(self.X_val.shape))
+            print("Y_val shape: {}".format(self.Y_val.shape))
+
+    def define_model(self):
+
+         inputs = tf.keras.Input(shape=(n_inputs, 1), name='input')
+
+         # 64 filters, 10 kernel size
+         x = Conv1D(64, 10, activation='relu')(inputs)
+         x = MaxPool1D()(x)
+         x = BatchNormalization()(x)
