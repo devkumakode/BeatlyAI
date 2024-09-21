d3a350c8bc0558fc4f352558e7357a25b8c23630
devganesh <devganesh@velozityglobal.com>
2024-09-21 10:49:13 +0000

    Add logging to trainers/base_trainer (lines 0-3)

--- AI/trainers/base_trainer.py
+++ AI/trainers/base_trainer.py
@@ -1,7 +1,3 @@
-                    "state_dict": self.model.state_dict(),
-                    "optimizer": self.optimizer.state_dict(),
-                    "epoch": epoch,
-                    "total_iter": self.total_iter,
-                },
-                osp.join(self.pth_dir, "{:0>8}.pth".format(epoch)),
-            )
+            self.val()
+
+            self.training_epoch += 1
