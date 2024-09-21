5b1f10701bc1b5520fda6f2dd33f8c3acca2f47c
naveenkurra <naveenkurra@velozityglobal.com>
2024-09-21 15:42:27 +0000

    Update dependencies for ecg/train (lines 0-19)

--- AI/ecg/train.py
+++ AI/ecg/train.py
@@ -1,12 +1,19 @@
-
-    reduce_lr = keras.callbacks.ReduceLROnPlateau(
-        factor=0.1,
-        patience=2,
-        min_lr=params["learning_rate"] * 0.001)
-
-    checkpointer = keras.callbacks.ModelCheckpoint(
-        filepath=get_filename_for_saving(save_dir),
-        save_best_only=False)
-
-    batch_size = params.get("batch_size", 32)
-
+    if params.get("generator", False):
+        train_gen = load.data_generator(batch_size, preproc, *train)
+        dev_gen = load.data_generator(batch_size, preproc, *dev)
+        model.fit_generator(
+            train_gen,
+            steps_per_epoch=int(len(train[0]) / batch_size),
+            epochs=MAX_EPOCHS,
+            validation_data=dev_gen,
+            validation_steps=int(len(dev[0]) / batch_size),
+            callbacks=[checkpointer, reduce_lr, stopping])
+    else:
+        train_x, train_y = preproc.process(*train)
+        dev_x, dev_y = preproc.process(*dev)
+        model.fit(
+            train_x, train_y,
+            batch_size=batch_size,
+            epochs=MAX_EPOCHS,
+            validation_data=(dev_x, dev_y),
+            callbacks=[checkpointer, reduce_lr, stopping])
