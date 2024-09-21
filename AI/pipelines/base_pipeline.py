58187e9310feb1a3b64409acdd93338f938063e9
maheshb <maheshb@velozityglobal.com>
2024-09-21 10:24:50 +0000

    Implement few-shot learning capabilities in pipelines/base_pipeline (lines 0-19)

--- AI/pipelines/base_pipeline.py
+++ AI/pipelines/base_pipeline.py
@@ -1,2 +1,19 @@
-                            "showarrow": False,
-                            "bordercolor": "#c7c7c7",
+                            "borderwidth": 1,
+                            "borderpad": 4,
+                            "bgcolor": "#ffffff",
+                            "opacity": 1,
+                        },
+                    )
+
+        fig = go.Figure(
+            data=go.Scatter(
+                x=list(range(len(self.pipeline_loader.dataset.signal))),
+                y=self.pipeline_loader.dataset.signal,
+            ),
+        )
+        fig.update_layout(
+            title="ECG",
+            xaxis_title="Time",
+            yaxis_title="ECG Output Value",
+            title_x=0.5,
+            annotations=annotations,
