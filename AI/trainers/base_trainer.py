6ea28a263ec33e74f5e6cd078d9b154c61682632
devakumar <devakumar@velozityglobal.com>
2024-09-21 14:05:09 +0000

    Implement transfer learning for trainers/base_trainer (lines 0-7)

--- AI/trainers/base_trainer.py
+++ AI/trainers/base_trainer.py
@@ -1,15 +1,7 @@
-
-        print("Validation loss - {:4f}".format(total_loss))
-        print("Validation CLASS accuracy - {:4f}".format(class_accuracy))
-
-        self.writer.add_scalar("Validation loss", total_loss, self.training_epoch)
-        self.writer.add_scalar(
-            "Validation CLASS accuracy", class_accuracy, self.training_epoch,
-        )
-
-    def loop(self):
-        for epoch in range(self.training_epoch, self.epochs):
-            print("Epoch - {}".format(self.training_epoch + 1))
-            self.train_epoch()
-            save_checkpoint(
-                {
+                    "state_dict": self.model.state_dict(),
+                    "optimizer": self.optimizer.state_dict(),
+                    "epoch": epoch,
+                    "total_iter": self.total_iter,
+                },
+                osp.join(self.pth_dir, "{:0>8}.pth".format(epoch)),
+            )
