c94817fc9ee3e39fd254504fca25efb8d21c7d9e
sakshamdrivedi <sakshamdrivedi@velozityglobal.com>
2024-09-23 15:33:21 +0000

    Implement attention mechanism in binarynet (lines 0-7)

--- AI/binarynet.py
+++ AI/binarynet.py
@@ -1,1 +1,7 @@
-        #print(self.B.shape)
+        #print(self.alpha.shape)
+        #print(self.mask.shape)
+        w_bin = torch.sum(self.B.float()*(self.alpha*self.mask.float()),dim=2)
+        #print(w_bin)
+        #print(tensor_shape)
+        if self.structure == 'kernelwise':
+            return w_bin.reshape(self.tensor_shape)
