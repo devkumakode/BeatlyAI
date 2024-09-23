29678a7e507e1b0dcec2c415c9f644636c925ee5
devganesh <devganesh@velozityglobal.com>
2024-09-23 11:42:29 +0000

    Implement transfer learning for ecg/train (lines 0-9)

--- AI/ecg/train.py
+++ AI/ecg/train.py
@@ -1,19 +1,9 @@
-    if params.get("generator", False):
-        train_gen = load.data_generator(batch_size, preproc, *train)
-        dev_gen = load.data_generator(batch_size, preproc, *dev)
-        model.fit_generator(
-            train_gen,
-            steps_per_epoch=int(len(train[0]) / batch_size),
-            epochs=MAX_EPOCHS,
-            validation_data=dev_gen,
-            validation_steps=int(len(dev[0]) / batch_size),
-            callbacks=[checkpointer, reduce_lr, stopping])
-    else:
-        train_x, train_y = preproc.process(*train)
-        dev_x, dev_y = preproc.process(*dev)
-        model.fit(
-            train_x, train_y,
-            batch_size=batch_size,
-            epochs=MAX_EPOCHS,
-            validation_data=(dev_x, dev_y),
-            callbacks=[checkpointer, reduce_lr, stopping])
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("config_file", help="path to config file")
+    parser.add_argument("--experiment", "-e", help="tag with experiment name",
+                        default="default")
+    args = parser.parse_args()
+    params = json.load(open(args.config_file, 'r'))
+    train(args, params)
