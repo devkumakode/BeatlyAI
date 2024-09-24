be6f8f443ea310178f3c619861700d2ce1de7b3b
sakshamdrivedi <sakshamdrivedi@velozityglobal.com>
2024-09-24 17:26:52 +0000

    Add unit tests for train (lines 0-17)

--- AI/train.py
+++ AI/train.py
@@ -1,9 +1,17 @@
-                #print(len(parameters_w))
-                print(structure[i])
-                parameters_w_bin.append(ConvLayer_bin(param.data, len(parameters_w)-1, structure[i], max_bit[i]))    
-                i += 1
-                tmp_param = param.detach()
-                tmp_param.zero_().add_(parameters_w_bin[-1].reconstruct_w())    
-        # Maintain other parameters (e.g. bias, batch normalization) in full precision 
-        else:
-            parameters_b.append(param)
+    net.eval()
+    train_loss = 0.
+    num_batches = 0
+    correct_sum = [0. for i in range(len(TOPK))]
+    total = 0
+    for (inputs, labels) in train_loader:               
+        inputs, labels = inputs.cuda(non_blocking=True), labels.cuda(non_blocking=True)
+        outputs = net(inputs)
+        loss = loss_func(outputs, labels)
+        accuracy(outputs, labels, correct_sum, topk=TOPK)
+        total += labels.size(0)
+        train_loss += loss.data.item()
+        num_batches += 1
+    print('train loss: ', train_loss/num_batches)
+    print('train accuracy: ', [ci/total for ci in correct_sum]) 
+    num_weight_layer = 0.
+    num_bit_layer = 0.
