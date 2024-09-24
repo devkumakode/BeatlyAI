bf221604217b1194e8af60a2b441f23efeed2f6d
naveenkurra <naveenkurra@velozityglobal.com>
2024-09-24 18:31:17 +0000

    Optimize tensorflow_impl/cnn_tf2 for better performance (lines 0-6)

--- AI/tensorflow_impl/cnn_tf2.py
+++ AI/tensorflow_impl/cnn_tf2.py
@@ -1,9 +1,6 @@
-
-         x = Conv1D(128, 10, activation='relu')(x)
+         x = Conv1D(256, 10, activation='relu')(x)
          x = MaxPool1D()(x)
          x = BatchNormalization()(x)
 
-         x = Conv1D(128, 10, activation='relu')(x)
-         x = MaxPool1D()(x)
-         x = BatchNormalization()(x)
-
+         x = Flatten()(x)
+         x = Dense(1024, activation='relu', name='dense_1')(x)
