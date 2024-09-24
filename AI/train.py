633913a2669cccba76e887f1b686d97d27e86570
devganesh <devganesh@velozityglobal.com>
2024-09-24 10:12:46 +0000

    Add support for model compression in train (lines 0-19)

--- AI/train.py
+++ AI/train.py
@@ -1,17 +1,19 @@
+    print('currrent binary filter number per layer: ')
+    for p_w_bin in parameters_w_bin:
+        print(p_w_bin.num_bin_filter)
+    print('currrent average bitwidth per layer: ')
+    for p_w_bin in parameters_w_bin:
+        num_weight_layer += p_w_bin.num_weight
+        num_bit_layer += p_w_bin.avg_bit*p_w_bin.num_weight
+        print(p_w_bin.avg_bit)
+        print(p_w_bin.num_weight)
+    print('currrent average bitwidth: ', num_bit_layer/num_weight_layer)
+    return parameters_w, parameters_b, parameters_w_bin 
+      
+     
+def validate(net, val_loader, loss_func):
+    """Get the validation loss and validation accuracy."""
     net.eval()
-    train_loss = 0.
+    val_loss = 0.
     num_batches = 0
     correct_sum = [0. for i in range(len(TOPK))]
-    total = 0
-    for (inputs, labels) in train_loader:               
-        inputs, labels = inputs.cuda(non_blocking=True), labels.cuda(non_blocking=True)
-        outputs = net(inputs)
-        loss = loss_func(outputs, labels)
-        accuracy(outputs, labels, correct_sum, topk=TOPK)
-        total += labels.size(0)
-        train_loss += loss.data.item()
-        num_batches += 1
-    print('train loss: ', train_loss/num_batches)
-    print('train accuracy: ', [ci/total for ci in correct_sum]) 
-    num_weight_layer = 0.
-    num_bit_layer = 0.
