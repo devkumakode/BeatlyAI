2b02a4dbf1b435aaa4da09efccb7db3115c09c26
devakumar <devakumar@velozityglobal.com>
2024-09-24 13:19:39 +0000

    Optimize memory usage in myoptimizer (lines 0-6)

--- AI/myoptimizer.py
+++ AI/myoptimizer.py
@@ -1,13 +1,6 @@
-                    bias_correction1 = 1 - beta1 ** state['step']
-                    bias_correction2 = 1 - beta2 ** state['step']
+                    tmp_p.zero_().add_(p_bin.w_FP.data)
 
-                    # Compute the pseudo gradient and the pseudo diagonal Hessian 
-                    pseudo_grad = (group['lr'] / bias_correction1) * exp_avg 
-                    pseudo_hessian = denom.div(math.sqrt(bias_correction2))
-                    # Take one optimization step on binary bases
-                    p_bin.optimize_bin_basis(pseudo_grad, pseudo_hessian)
-                    # Speed up with an optimization step on coordinates
-                    p_bin.speedup(pseudo_grad, pseudo_hessian)
-                    # Reconstruct the weight tensor from the current quantization
-                    p_bin.update_w_FP()
-                    tmp_p = p.detach()
+                    # Update the state parameters in alpha domain (approximately)
+                    state['step_alpha'] += 1
+                    state['exp_avg_alpha'] = p_bin.construct_grad_alpha(exp_avg)
+                    state['exp_avg_sq_alpha'] = p_bin.construct_hessian_alpha(exp_avg_sq)
