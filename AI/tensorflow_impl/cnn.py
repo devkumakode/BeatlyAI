497d0eeca6e03af5e7ad911ec2c49940a7a01ccc
naveenkurra <naveenkurra@velozityglobal.com>
2024-09-25 14:38:08 +0000

    Add logging to tensorflow_impl/cnn (lines 0-1)

--- AI/tensorflow_impl/cnn.py
+++ AI/tensorflow_impl/cnn.py
@@ -1,11 +1,1 @@
-            print("X_train shape: {}".format(self.X_train.shape))
-            print("Y_train shape: {}".format(self.Y_train.shape))
-            print("X_test shape: {}".format(self.X_test.shape))
-            print("Y_test shape: {}".format(self.Y_test.shape))
-
-    def train(self, x):
-        is_training = not freeze
-        # Reshape input so that we can feed it to the first conv layer
-        x = tf.reshape(x, shape=[-1, nr_inputs, 1])
-        
-        # Convolution Layer 1
+        conv1 = self.conv1d(x, self.weights['wconv1'], self.biases['bconv1'])
