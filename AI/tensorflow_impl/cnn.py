b2083596ce30be44a2504a9e0a157fa04c5abe73
maheshb <maheshb@velozityglobal.com>
2024-09-25 15:05:59 +0000

    Add support for quantization in tensorflow_impl/cnn (lines 0-12)

--- AI/tensorflow_impl/cnn.py
+++ AI/tensorflow_impl/cnn.py
@@ -1,1 +1,12 @@
-        conv1 = self.conv1d(x, self.weights['wconv1'], self.biases['bconv1'])
+        conv1 = self.maxpool1d(conv1)
+        # Batch Norm Layer 1
+        conv1 = tf.contrib.layers.batch_norm(conv1, is_training=is_training)
+
+        # Convolution Layer 2
+        conv2 = self.conv1d(conv1, self.weights['wconv2'], self.biases['bconv2'])
+        conv2 = self.maxpool1d(conv2)
+        # Batch Norm Layer 2
+        conv2 = tf.contrib.layers.batch_norm(conv2, is_training=is_training)
+
+        # Convolution Layer 3
+        conv3 = self.conv1d(conv2, self.weights['wconv3'], self.biases['bconv3'])
