4fdca6e60e5b3af11fa12c4488238978e53ecd8b
devakumar <devakumar@velozityglobal.com>
2024-09-27 09:49:20 +0000

    Implement adversarial training in tensorflow_impl/cnn_tf2 (lines 0-18)

--- AI/tensorflow_impl/cnn_tf2.py
+++ AI/tensorflow_impl/cnn_tf2.py
@@ -1,5 +1,18 @@
-         x = BatchNormalization()(x)
          x = Dropout(dropout)(x)
 
-         x = Dense(2048, activation='relu', name='dense_2')(x)
-         x = BatchNormalization()(x)
+         outputs = Dense(n_classes, activation='softmax', name='predictions')(x)
+
+         self.cnn_model = tf.keras.Model(inputs=inputs, outputs=outputs)
+         optimizer = tf.keras.optimizers.Adam(lr=learning_rate)
+         accuracy = CategoricalAccuracy()
+         self.cnn_model.compile(optimizer=optimizer, loss='categorical_crossentropy',
+                                metrics=[accuracy])
+
+    def split_data(self, dataset, validation_size):
+        """
+        Suffle then split training, testing and validation sets
+        """
+
+        # In order to use statify in train_test_split we can't use one hot encodings,
+        # so we convert to array of labels
+        label_data = np.argmax(self.label_data, axis=1)
