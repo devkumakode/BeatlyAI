3840666e5ea67618d915a16862a69550d2f69b95
devakumar <devakumar@velozityglobal.com>
2024-09-27 16:40:35 +0000

    Add support for mixed precision training in tensorflow_impl/cnn (lines 0-19)

--- AI/tensorflow_impl/cnn.py
+++ AI/tensorflow_impl/cnn.py
@@ -1,8 +1,19 @@
-        # Reshape conv4 output to fit fully connected layer input
-        # shape_size is a cause for errors, it is determined using
-        # conv4.shape[1]*conv4.shape[2] and also has to be changed in weight definition
-        shape_size = conv4.shape[1] * conv4.shape[2]
-        fc1 = tf.reshape(conv4, [-1, shape_size])
+        fc1 = tf.contrib.layers.batch_norm(fc1, is_training=is_training)
+        fc1 = tf.nn.relu(fc1)
+        fc1 = tf.nn.dropout(fc1, dropout)
 
-        # Fully connected layer 1
-        fc1 = tf.add(tf.matmul(fc1, self.weights['wdense1']), self.biases['bdense1'])
+        # Fully connected layer 2
+        fc2 = tf.add(tf.matmul(fc1, self.weights['wdense2']), self.biases['bdense2'])
+        fc2 = tf.contrib.layers.batch_norm(fc2, is_training=is_training)
+        fc2 = tf.nn.relu(fc2)
+        fc2 = tf.nn.dropout(fc2, dropout)
+
+        # Output, class prediction
+        out = tf.add(tf.matmul(fc2, self.weights['wout']), self.biases['bout'])
+        return out
+
+    def conv1d(self, x, W, b, strides=1):
+        # conv1d needs a 3-D input([batch, in_width, in_channels]) and
+        # filter tensors([filter_width, in_channels, out_channels])
+        x = tf.nn.conv1d(x, W, stride=strides, padding='SAME')
+        x = tf.nn.bias_add(x, b)
