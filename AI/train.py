481b8bdb1a55ccacf0698cf6b08b1f64904c4b4f
devganesh <devganesh@velozityglobal.com>
2024-09-27 12:19:20 +0000

    Add logging to train (lines 0-8)

--- AI/train.py
+++ AI/train.py
@@ -1,19 +1,8 @@
+        
 
-def test(net, test_loader, loss_func):
-    """Get the test loss and test accuracy."""
-    net.eval()
-    test_loss = 0.
-    num_batches = 0
-    correct_sum = [0. for i in range(len(TOPK))]
-    total = 0
-    with torch.no_grad():
-        for (inputs, labels) in test_loader:
-            inputs, labels = inputs.cuda(non_blocking=True), labels.cuda(non_blocking=True)
-            outputs = net(inputs)
-            loss = loss_func(outputs, labels)  
-            accuracy(outputs, labels, correct_sum, topk=TOPK)
-            total += labels.size(0)
-            test_loss += loss.data.item()
-            num_batches += 1
-        print("test loss: ", test_loss/num_batches)
-        print("test accuracy: ", [ci/total for ci in correct_sum])
+def save_model(file_name, net, optimizer_w, optimizer_b, parameters_w_bin):
+    """Save the state dictionary of model and optimizers."""
+    print('saving...')   
+    torch.save({
+        'net_state_dict': net.state_dict(),
+        'optimizer_w_state_dict': optimizer_w.state_dict(),
