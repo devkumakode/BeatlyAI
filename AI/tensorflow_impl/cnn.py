c4ffd6f41afd0deed95a3a7b5318a9dd8f9e2e91
maheshb <maheshb@velozityglobal.com>
2024-09-28 10:56:23 +0000

    Add support for quantization in tensorflow_impl/cnn (lines 0-19)

--- AI/tensorflow_impl/cnn.py
+++ AI/tensorflow_impl/cnn.py
@@ -1,6 +1,19 @@
-    def maxpool1d(self, x, pool_size=2):
-        # [batch, height, width, channels] input type: tf.float32
-        return tf.contrib.keras.layers.MaxPool1D(pool_size=pool_size)(x)
+                                                             labels=y)
+        return tf.reduce_mean(softmax)
 
-    def cost(self, pred):
-        softmax = tf.nn.softmax_cross_entropy_with_logits_v2(logits=pred,
+    def optimizer(self, cost):
+        adam = tf.train.AdamOptimizer(learning_rate=learning_rate)
+        return adam.minimize(cost)
+
+    def evl(self, pred):
+        correct_pred = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
+        return tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+
+    def get_data(self):
+        return (self.X_train, self.X_test,
+                self.Y_train, self.Y_test)
+
+
+# Construct model
+model = CNN()
+pred = model.train(x)
