8a9ee5507e4b4ab7e69f38efe993fe187ef016ff
devganesh <devganesh@velozityglobal.com>
2024-09-28 12:23:37 +0000

    Implement curriculum learning in train (lines 0-20)

--- AI/train.py
+++ AI/train.py
@@ -1,8 +1,20 @@
-        
+        'optimizer_b_state_dict': optimizer_b.state_dict(),
+        'parameters_w_bin': parameters_w_bin,
+        }, file_name)
 
-def save_model(file_name, net, optimizer_w, optimizer_b, parameters_w_bin):
-    """Save the state dictionary of model and optimizers."""
+
+def save_model_ori(file_name, net, optimizer):
+    """Save the state dictionary of model and optimizer for full precision training."""
     print('saving...')   
     torch.save({
         'net_state_dict': net.state_dict(),
-        'optimizer_w_state_dict': optimizer_w.state_dict(),
+        'optimizer_state_dict': optimizer.state_dict(),
+        }, file_name)
+def save_model_simple(file_name, net):
+    """Save the state dictionary of model and optimizer for final training."""
+    print('saving...')   
+    torch.save({
+        'net_state_dict': net.state_dict(),
+        #'optimizer_state_dict': optimizer.state_dict(),
+        }, file_name)
+  
