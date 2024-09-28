664e1418d31acf85029769165306450717883149
sakshamdrivedi <sakshamdrivedi@velozityglobal.com>
2024-09-28 16:36:13 +0000

    Implement adversarial training in ECGnet (lines 0-17)

--- AI/ECGnet.py
+++ AI/ECGnet.py
@@ -1,1 +1,17 @@
-    def __getitem__(self, index):
+        return self.x_train[index], self.y_train[index]  # 返回对应样本即可
+
+    def __len__(self):
+        return self.len
+
+
+class TestDataset(Dataset):
+    def __init__(self):
+        base_path = './'
+        dataset_path = './Dataset'
+        classes = ['NSR', 'APB', 'AFL', 'AFIB', 'SVTA', 'WPW', 'PVC', 'Bigeminy',
+                   'Trigeminy', 'VT', 'IVR', 'VFL', 'Fusion', 'LBBBB', 'RBBBB', 'SDHB', 'PR']
+        ClassesNum = len(classes)
+        X = list()
+        y = list()
+
+        for root, dirs, files in os.walk(dataset_path, topdown=False):
