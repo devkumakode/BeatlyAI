dd6924e6ee867f76a3904d1cb52ff6e4391a97d9
sakshamdrivedi <sakshamdrivedi@velozityglobal.com>
2024-09-28 11:05:49 +0000

    Improve error handling in tensorflow_impl/cnn (lines 0-6)

--- AI/tensorflow_impl/cnn.py
+++ AI/tensorflow_impl/cnn.py
@@ -1,2 +1,6 @@
-        return tf.nn.relu(x)
+    def maxpool1d(self, x, pool_size=2):
+        # [batch, height, width, channels] input type: tf.float32
+        return tf.contrib.keras.layers.MaxPool1D(pool_size=pool_size)(x)
 
+    def cost(self, pred):
+        softmax = tf.nn.softmax_cross_entropy_with_logits_v2(logits=pred,
