e753f040e022ce68ff9b65270f0a7a0ef40c9e43
maheshb <maheshb@velozityglobal.com>
2024-09-28 15:15:18 +0000

    Add binarynet to improve model interpretability (lines 0-20)

--- AI/binarynet.py
+++ AI/binarynet.py
@@ -1,20 +1,20 @@
-        else:
-            self.w_FP.zero_().add_(self.reconstruct_w())
-
-    def construct_grad_alpha(self, grad_w):
-        """Compute and return the gradient (or the first momentum) in alpha domain w.r.t the loss.
-        """
-        if self.structure == 'kernelwise':
-            return torch.matmul(self.B.float(), grad_w.reshape((self.tensor_shape[0],self.tensor_shape[1],-1,1)))*self.mask.float()
-        elif self.structure == 'pixelwise':
-            return torch.matmul(self.B.float(), torch.transpose(grad_w.reshape((self.tensor_shape[0],self.tensor_shape[1],-1,1)), 1,2) )*self.mask.float()
-        elif self.structure == 'channelwise':
-            return torch.matmul(self.B.float(), grad_w.reshape((self.tensor_shape[0],1,-1,1)))*self.mask.float()
-
-    def construct_hessian_alpha(self, diag_hessian_w):
-        """Compute and return the diagonal Hessian (or the second momentum) in alpha domain w.r.t the loss.
-        """
-        if self.structure == 'kernelwise':
-            diag_hessian = torch.matmul(self.B.float()*diag_hessian_w.reshape((self.tensor_shape[0],self.tensor_shape[1],1,-1)), torch.transpose(self.B,2,3).float())
+            diag_hessian = torch.matmul(self.B.float()*torch.transpose(diag_hessian_w.reshape((self.tensor_shape[0],self.tensor_shape[1],1,-1)), 1,3), torch.transpose(self.B,2,3).float())
             return torch.diagonal(diag_hessian,dim1=-2,dim2=-1).unsqueeze(-1)*self.mask.float()
-        elif self.structure == 'pixelwise':
+        elif self.structure == 'channelwise':
+            diag_hessian = torch.matmul(self.B.float()*diag_hessian_w.reshape((self.tensor_shape[0],1,1,-1)), torch.transpose(self.B,2,3).float())
+            return torch.diagonal(diag_hessian,dim1=-2,dim2=-1).unsqueeze(-1)*self.mask.float()
+
+    def sort_importance_bin_filter(self, grad_alpha, diag_hessian_alpha, num_top):
+        """Compute and sort the importance of binary filters (alpha's) in this layer.
+        The importance is defined by the modeled loss increment caused by pruning each individual alpha.
+        Return the selected num_top alpha's with the least importance.
+        """
+        delta_loss_prune = -grad_alpha*self.alpha+0.5*torch.pow(self.alpha,2)*diag_hessian_alpha
+        sorted_ind = torch.argsort(delta_loss_prune[self.mask].view(-1))
+        top_importance_list = torch.tensor([[self.ind_layer, sorted_ind[i], delta_loss_prune.view(-1)[sorted_ind[i]]] for i in range(num_top)])  
+        return top_importance_list
+                
+    def prune_alpha(self, ind_prune): 
+        """Prune the cooresponding alpha's of this layer give the indexes.
+        """
+        num_bin_filter_ = torch.sum(self.mask)
