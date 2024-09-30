632019a475525621ec2237ade3f50dc511c58841
naveenkurra <naveenkurra@velozityglobal.com>
2024-09-30 14:45:07 +0000

    Add explainability features to tensorflow_impl/cnn_tf2 (lines 0-3)

--- AI/tensorflow_impl/cnn_tf2.py
+++ AI/tensorflow_impl/cnn_tf2.py
@@ -1,8 +1,3 @@
-
-        # Splitting the dataset into train and test datasets
-        res = train_test_split(dataset, label_data,
-                               test_size=validation_size, shuffle=True,
-                               stratify=label_data)
-
-        self.X_train, self.X_test, self.Y_train, self.Y_test = res
-
+        # From the training dataset we further split it to obtain the validation dataset
+        res = train_test_split(self.X_train, self.Y_train,
+                               test_size=validation_size, stratify=self.Y_train)
