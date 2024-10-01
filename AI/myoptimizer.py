b374a8417a537e0dc28fe07598f7842d42a3cb2d
devakumar <devakumar@velozityglobal.com>
2024-10-01 15:29:57 +0000

    Add type hints to myoptimizer (lines 0-6)

--- AI/myoptimizer.py
+++ AI/myoptimizer.py
@@ -1,12 +1,6 @@
-            
-            # Check if this is a pruning step        
-            if pruning_rate is not None:
-                # Resort the importance of selected binary filters (alpha's) over all layers 
-                sorted_ind = torch.argsort(importance_list[:,-1])
-                # Compute the number of pruned alpha's in this iteration
-                # Note that unlike the paper, M_p varies over iterations here, but this does not influence the pruning schedule. 
-                M_p = int(sorted_ind.nelement()*pruning_rate[1])
-                # Determine indexes of alpha's to be pruned
-                ind_prune = sorted_ind[:M_p]
-                list_prune = importance_list[ind_prune,:]
-                # Prune alpha's in each layer and reconstruct the weight tensor
+                for i, (p_bin, p) in enumerate(zip(params_bin, group['params'])):
+                    p_bin.prune_alpha((torch.sort(list_prune[list_prune[:,0]==i,1])[0]).to(torch.int64))
+                    p_bin.update_w_FP()
+                    tmp_p = p.detach()
+                    tmp_p.zero_().add_(p_bin.w_FP.data)
+        return loss
