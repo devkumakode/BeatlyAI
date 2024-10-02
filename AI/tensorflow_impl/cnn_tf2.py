bb8d6709675ae0efd315a24c2ac441ef185937be
maheshb <maheshb@velozityglobal.com>
2024-10-02 13:33:54 +0000

    Implement transfer learning for tensorflow_impl/cnn_tf2 (lines 0-20)

--- AI/tensorflow_impl/cnn_tf2.py
+++ AI/tensorflow_impl/cnn_tf2.py
@@ -1,3 +1,20 @@
-        # From the training dataset we further split it to obtain the validation dataset
-        res = train_test_split(self.X_train, self.Y_train,
-                               test_size=validation_size, stratify=self.Y_train)
+
+        self.X_train, self.X_val, self.Y_train, self.Y_val = res
+
+        # Convert the array of labels back into one hot encodings to be able to do training
+        self.Y_train = tf.keras.utils.to_categorical(self.Y_train)
+        self.Y_test = tf.keras.utils.to_categorical(self.Y_test)
+        self.Y_val = tf.keras.utils.to_categorical(self.Y_val)
+
+    def get_data(self):
+        return (self.X_train, self.X_test, self.X_val,
+                self.Y_train, self.Y_test, self.Y_val)
+
+
+def main():
+    # Construct model
+    model = CNN()
+    X_train, X_test, X_val, Y_train, Y_test, Y_val = model.get_data()
+
+    # Set start time
+    total_time = time.time()
