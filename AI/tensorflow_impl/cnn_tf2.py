f0efce131ad64353ce9d47b3919453c33243cd53
naveenkurra <naveenkurra@velozityglobal.com>
2024-10-02 16:02:32 +0000

    Improve error handling in tensorflow_impl/cnn_tf2 (lines 0-6)

--- AI/tensorflow_impl/cnn_tf2.py
+++ AI/tensorflow_impl/cnn_tf2.py
@@ -1,20 +1,6 @@
 
-        self.X_train, self.X_val, self.Y_train, self.Y_val = res
+    print("-"*50)
+    if restore_model:
+        print("Restoring model: {}".format('saved_models/cnn/model.hdf5'))
 
-        # Convert the array of labels back into one hot encodings to be able to do training
-        self.Y_train = tf.keras.utils.to_categorical(self.Y_train)
-        self.Y_test = tf.keras.utils.to_categorical(self.Y_test)
-        self.Y_val = tf.keras.utils.to_categorical(self.Y_val)
-
-    def get_data(self):
-        return (self.X_train, self.X_test, self.X_val,
-                self.Y_train, self.Y_test, self.Y_val)
-
-
-def main():
-    # Construct model
-    model = CNN()
-    X_train, X_test, X_val, Y_train, Y_test, Y_val = model.get_data()
-
-    # Set start time
-    total_time = time.time()
+    # Train
