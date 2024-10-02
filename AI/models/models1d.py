9adc0222553df83757934af765d4b5b77b0f4f23
devakumar <devakumar@velozityglobal.com>
2024-10-02 09:11:02 +0000

    Optimize models/models1d for better performance (lines 0-2)

--- AI/models/models1d.py
+++ AI/models/models1d.py
@@ -1,14 +1,2 @@
-
-        out = self.bn1(x)
-        out = self.relu(out)
-        out = self.dropout(out)
-        out = self.conv1(out)
-
-        out = self.bn2(out)
-        out = self.relu(out)
-        out = self.dropout(out)
-        out = self.conv2(out)
-
-        if self.downsample is not None:
-            identity = self.downsample(x)
+        out += identity
 
