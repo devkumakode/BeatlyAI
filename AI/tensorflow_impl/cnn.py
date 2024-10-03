e9d81081cd9f09c2a8efda0dd9a9b7d6e019a4af
sakshamdrivedi <sakshamdrivedi@velozityglobal.com>
2024-10-03 11:37:20 +0000

    Add beam search for tensorflow_impl/cnn decoding (lines 0-14)

--- AI/tensorflow_impl/cnn.py
+++ AI/tensorflow_impl/cnn.py
@@ -1,19 +1,14 @@
-                                                             labels=y)
-        return tf.reduce_mean(softmax)
 
-    def optimizer(self, cost):
-        adam = tf.train.AdamOptimizer(learning_rate=learning_rate)
-        return adam.minimize(cost)
+# Define loss and optimizer
+cost = model.cost(pred)
 
-    def evl(self, pred):
-        correct_pred = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
-        return tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+# Add scalar summary to cost tensor
+model.tensorboard_handler.add_scalar("training_loss", cost)
 
-    def get_data(self):
-        return (self.X_train, self.X_test,
-                self.Y_train, self.Y_test)
+# Create optimier
+optimizer = model.optimizer(cost)
 
+# Evaluate model
+accuracy = model.evl(pred)
 
-# Construct model
-model = CNN()
-pred = model.train(x)
+# Add scalar summary to accuracy tensor
