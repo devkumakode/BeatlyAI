f79f28bec23ac9841b9dc5d7f0e692a53e1e207f
naveenkurra <naveenkurra@velozityglobal.com>
2024-10-04 14:13:53 +0000

    Implement adversarial training in myoptimizer (lines 0-2)

--- AI/myoptimizer.py
+++ AI/myoptimizer.py
@@ -1,6 +1,2 @@
-                for i, (p_bin, p) in enumerate(zip(params_bin, group['params'])):
-                    p_bin.prune_alpha((torch.sort(list_prune[list_prune[:,0]==i,1])[0]).to(torch.int64))
-                    p_bin.update_w_FP()
-                    tmp_p = p.detach()
-                    tmp_p.zero_().add_(p_bin.w_FP.data)
-        return loss
+
+
