79d262b6ff83844db5cda311e6cdf577593e17ea
devganesh <devganesh@velozityglobal.com>
2024-10-05 12:33:53 +0000

    Optimize tensorflow_impl/cnn_tf2 for better inference time (lines 0-13)

--- AI/tensorflow_impl/cnn_tf2.py
+++ AI/tensorflow_impl/cnn_tf2.py
@@ -1,3 +1,13 @@
-    model.cnn_model.fit(X_train, Y_train, batch_size=batch_size,
-                        epochs=epochs, validation_data=(X_val, Y_val),
-                        callbacks=model.callbacks)
+
+    print("-"*50)
+
+    # Total training time
+    print("Total training time: {0:.2f}s".format(time.time() - total_time))
+
+    # Test
+    model.cnn_model.evaluate(X_test, Y_test, batch_size=batch_size)
+    print("-"*50)
+    print("Testing results:")
+    y_pred = model.cnn_model.predict(X_test, batch_size=batch_size)
+
+    # The following scikit-learn methods only accept array of labels, not one hot encodings
