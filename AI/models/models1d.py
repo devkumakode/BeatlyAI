b2482b89a3c5c3cbfd98c0da1be7dd67a6d4cfa4
devakumar <devakumar@velozityglobal.com>
2024-10-05 18:38:27 +0000

    Add support for model compression in models/models1d (lines 0-164)

--- AI/models/models1d.py
+++ AI/models/models1d.py
@@ -1,14 +1,164 @@
-        return out
+    ):
+
+        super(HeartNet, self).__init__()
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm1d
+        self._norm_layer = norm_layer
+
+        self.inplanes = 32
+        self.dilation = 1
+        if replace_stride_with_dilation is None:
+            # each element in the tuple indicates if we should replace
+            # the 2x2 stride with a dilated convolution instead
+            replace_stride_with_dilation = [False, False, False]
+        if len(replace_stride_with_dilation) != 3:
+            raise ValueError(
+                "replace_stride_with_dilation should be None "
+                "or a 3-element tuple, got {}".format(replace_stride_with_dilation),
+            )
+        self.groups = groups
+        self.base_width = width_per_group
+        self.conv1 = conv_block(1, self.inplanes, stride=1)
+        self.bn1 = norm_layer(self.inplanes)
+        self.relu = nn.ReLU(inplace=True)
+        self.layer0 = self._make_layer(block, 64, layers[0])
+        self.layer1 = self._make_layer(
+            block, 64, layers[1], stride=2, dilate=replace_stride_with_dilation[0],
+        )
+        self.layer2 = self._make_layer(
+            block, 128, layers[2], stride=2, dilate=replace_stride_with_dilation[0],
+        )
+        self.layer2_ = self._make_layer(
+            block, 128, layers[3], stride=2, dilate=replace_stride_with_dilation[0],
+        )
+        self.layer3 = self._make_layer(
+            block, 256, layers[4], stride=2, dilate=replace_stride_with_dilation[1],
+        )
+        self.layer3_ = self._make_layer(
+            block, 256, layers[5], stride=2, dilate=replace_stride_with_dilation[1],
+        )
+        self.layer4 = self._make_layer(
+            block, 512, layers[6], stride=2, dilate=replace_stride_with_dilation[2],
+        )
+        self.layer4_ = self._make_layer(
+            block, 512, layers[7], stride=2, dilate=replace_stride_with_dilation[2],
+        )
+        self.layer5 = self._make_layer(
+            block, 1024, layers[8], stride=2, dilate=replace_stride_with_dilation[2],
+        )
+        self.avgpool = nn.AdaptiveAvgPool1d(1)
+        self.fc = nn.Linear(1024 * block.expansion, num_classes)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv1d):
+                nn.init.kaiming_normal_(m.weight, mode="fan_out", nonlinearity="relu")
+            elif isinstance(m, (nn.BatchNorm1d, nn.GroupNorm)):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+        # Zero-initialize the last BN in each residual branch,
+        # so that the residual branch starts with zeros, and each residual block behaves like an identity.
+        # This improves the model by 0.2~0.3% according to https://arxiv.org/abs/1706.02677
+        if zero_init_residual:
+            for m in self.modules():
+                if isinstance(m, BasicBlockHeartNet):
+                    nn.init.constant_(m.bn2.weight, 0)
+
+    def _make_layer(self, block, planes, blocks, stride=1, dilate=False):
+        norm_layer = self._norm_layer
+        downsample = None
+        previous_dilation = self.dilation
+        self.stride = stride
+        if dilate:
+            self.dilation *= stride
+            stride = 1
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                conv_subsumpling(self.inplanes, planes * block.expansion),
+            )
+
+        layers = []
+        layers.append(
+            block(
+                self.inplanes,
+                planes,
+                stride,
+                downsample,
+                self.groups,
+                self.base_width,
+                previous_dilation,
+                norm_layer,
+            ),
+        )
+        self.inplanes = planes * block.expansion
+        for _ in range(1, blocks):
+            layers.append(
+                block(
+                    self.inplanes,
+                    planes,
+                    groups=self.groups,
+                    base_width=self.base_width,
+                    dilation=self.dilation,
+                    norm_layer=norm_layer,
+                ),
+            )
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+
+        x = self.layer0(x)
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer2_(x)
+        x = self.layer3(x)
+        x = self.layer3_(x)
+        x = self.layer4(x)
+        x = self.layer4_(x)
+        x = self.layer5(x)
+
+        x = self.avgpool(x)
+        x = x.reshape(x.size(0), -1)
+        x = self.fc(x)
+
+        return x
 
 
-class HeartNet(nn.Module):
+class EcgResNet34(nn.Module):
     def __init__(
         self,
-        layers=(1, 2, 2, 2, 2, 2, 2, 2, 1),
+        layers=(1, 5, 5, 5),
         num_classes=1000,
         zero_init_residual=False,
         groups=1,
         width_per_group=64,
         replace_stride_with_dilation=None,
         norm_layer=None,
-        block=BasicBlockHeartNet,
+        block=BasicBlock,
+    ):
+
+        super(EcgResNet34, self).__init__()
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm1d
+        self._norm_layer = norm_layer
+
+        self.inplanes = 32
+        self.dilation = 1
+        if replace_stride_with_dilation is None:
+            # each element in the tuple indicates if we should replace
+            # the 2x2 stride with a dilated convolution instead
+            replace_stride_with_dilation = [False, False, False]
+        if len(replace_stride_with_dilation) != 3:
+            raise ValueError(
+                "replace_stride_with_dilation should be None "
+                "or a 3-element tuple, got {}".format(replace_stride_with_dilation),
+            )
+        self.groups = groups
+        self.base_width = width_per_group
+        self.conv1 = conv_block(1, self.inplanes, stride=1)
+        self.bn1 = norm_layer(self.inplanes)
+        self.relu = nn.ReLU(inplace=True)
+        self.layer1 = self._make_layer(block, 64, layers[0])
