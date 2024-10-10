f5a78bee8005758ce44b82aa26e1694e34172c39
sakshamdrivedi <sakshamdrivedi@velozityglobal.com>
2024-10-10 13:24:35 +0000

    Fix bug in binarynet (lines 0-5)

--- AI/binarynet.py
+++ AI/binarynet.py
@@ -1,10 +1,5 @@
-        self.B *= self.mask.char()
-        return True
-                               
-    def speedup(self, pseudo_grad, pseudo_hessian):
-        """Speed up the optimization on binary bases, i.e. take a following optimization step on coordinates while fixing binary bases. 
-        """
-        revised_grad_w = -pseudo_hessian*self.w_FP+pseudo_grad
-        revised_hessian = torch.matmul(self.B.float()*pseudo_hessian.view((self.tensor_shape[0],self.num_subchannel,1,-1)),torch.transpose(self.B,2,3).float())
-        revised_hessian += torch.diag_embed(1+1e-6-(self.mask.float().squeeze(-1))) 
-        revised_grad = torch.matmul(self.B.float(),revised_grad_w.view((self.tensor_shape[0],self.num_subchannel,-1,1)))
+        self.alpha = -torch.matmul(torch.inverse(revised_hessian),revised_grad)
+        self.alpha *= self.mask.float()
+        ind_neg = self.alpha<0
+        self.alpha[ind_neg] *= -1
+        self.B.contiguous().view(-1,self.B.size(-1))[ind_neg.view(-1),:] *= -1
