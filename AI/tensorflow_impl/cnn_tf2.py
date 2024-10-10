61efd6d2d4e6b6d96f76fc04608a10c09fc44329
devganesh <devganesh@velozityglobal.com>
2024-10-10 11:06:27 +0000

    Implement early stopping in tensorflow_impl/cnn_tf2 (lines 0-3)

--- AI/tensorflow_impl/cnn_tf2.py
+++ AI/tensorflow_impl/cnn_tf2.py
@@ -1,7 +1,3 @@
-    y_pred = np.argmax(y_pred, axis=1)
-    y_true = np.argmax(Y_test, axis=1)
+    disease_indexes = list(range(len(heart_diseases)))
+    print("Indexes {} correspond to labels {}".format(disease_indexes, [x for x in heart_diseases]))
 
-    # Precision and recall could also be done as callbacks in the evaluate or fit function
-    print("Precision: {}".format(precision_score(y_true, y_pred, average='micro')))
-    print("Recall: {}".format(recall_score(y_true, y_pred, average='micro')))
-    print("Confusion matrix: \n{}".format(confusion_matrix(y_true, y_pred, labels=[0,1,2])))
