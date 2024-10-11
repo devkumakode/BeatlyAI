48d68097a430ac8d2151b59849df153fabb271b8
naveenkurra <naveenkurra@velozityglobal.com>
2024-10-11 17:26:58 +0000

    Implement transfer learning for models/models1d (lines 0-20)

--- AI/models/models1d.py
+++ AI/models/models1d.py
@@ -1,41 +1,20 @@
-        return x
-
-
-class HeartNetIEEE(nn.Module):
-    def __init__(self, num_classes=8):
-        super().__init__()
-
-        self.features = nn.Sequential(
-            nn.Conv1d(1, 64, kernel_size=5),
-            nn.ReLU(inplace=True),
-            nn.Conv1d(64, 64, kernel_size=5),
+            nn.Conv1d(1, 8, kernel_size=3, padding=1),
+            nn.BatchNorm1d(8),
             nn.ReLU(inplace=True),
             nn.MaxPool1d(2),
-            nn.Conv1d(64, 128, kernel_size=3),
-            nn.ReLU(inplace=True),
-            nn.Conv1d(128, 128, kernel_size=3),
+            nn.Conv1d(8, 16, kernel_size=3, padding=1),
+            nn.BatchNorm1d(16),
             nn.ReLU(inplace=True),
             nn.MaxPool1d(2),
+            nn.Conv1d(16, 32, kernel_size=3, padding=1),
+            nn.BatchNorm1d(32),
+            nn.ReLU(inplace=True),
+            nn.MaxPool1d(2),
+            nn.Conv1d(32, 32, kernel_size=3, padding=1),
+            nn.BatchNorm1d(32),
+            nn.ReLU(inplace=True),
+            nn.MaxPool1d(2),
+            nn.Conv1d(32, 1, kernel_size=3, padding=1),
+            Flatten(),
         )
 
-        self.classifier = nn.Sequential(
-            nn.Linear(128 * 28, 256), nn.Linear(256, 128), nn.Linear(128, num_classes),
-        )
-
-    def forward(self, x):
-        x = self.features(x)
-        x = x.view(x.size(0), 128 * 28)
-        x = self.classifier(x)
-        return x
-
-
-class Flatten(nn.Module):
-    def forward(self, input):
-        return input.view(input.size(0), -1)
-
-
-class ZolotyhNet(nn.Module):
-    def __init__(self, num_classes=8):
-        super().__init__()
-
-        self.features_up = nn.Sequential(
