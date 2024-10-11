197507dce59e8c21ec4be77dda3ab39ce5fdc74c
devakumar <devakumar@velozityglobal.com>
2024-10-11 11:32:23 +0000

    Add support for quantization in models/models1d (lines 0-41)

--- AI/models/models1d.py
+++ AI/models/models1d.py
@@ -1,82 +1,41 @@
-        self.layer2 = self._make_layer(
-            block, 128, layers[1], stride=2, dilate=replace_stride_with_dilation[0],
+        return x
+
+
+class HeartNetIEEE(nn.Module):
+    def __init__(self, num_classes=8):
+        super().__init__()
+
+        self.features = nn.Sequential(
+            nn.Conv1d(1, 64, kernel_size=5),
+            nn.ReLU(inplace=True),
+            nn.Conv1d(64, 64, kernel_size=5),
+            nn.ReLU(inplace=True),
+            nn.MaxPool1d(2),
+            nn.Conv1d(64, 128, kernel_size=3),
+            nn.ReLU(inplace=True),
+            nn.Conv1d(128, 128, kernel_size=3),
+            nn.ReLU(inplace=True),
+            nn.MaxPool1d(2),
         )
-        self.layer3 = self._make_layer(
-            block, 256, layers[2], stride=2, dilate=replace_stride_with_dilation[1],
+
+        self.classifier = nn.Sequential(
+            nn.Linear(128 * 28, 256), nn.Linear(256, 128), nn.Linear(128, num_classes),
         )
-        self.layer4 = self._make_layer(
-            block, 512, layers[3], stride=2, dilate=replace_stride_with_dilation[2],
-        )
-        self.avgpool = nn.AdaptiveAvgPool1d(1)
-        self.fc = nn.Linear(512 * block.expansion, num_classes)
-
-        for m in self.modules():
-            if isinstance(m, nn.Conv1d):
-                nn.init.kaiming_normal_(m.weight, mode="fan_out", nonlinearity="relu")
-            elif isinstance(m, (nn.BatchNorm1d, nn.GroupNorm)):
-                nn.init.constant_(m.weight, 1)
-                nn.init.constant_(m.bias, 0)
-
-        # Zero-initialize the last BN in each residual branch,
-        # so that the residual branch starts with zeros, and each residual block behaves like an identity.
-        # This improves the model by 0.2~0.3% according to https://arxiv.org/abs/1706.02677
-        if zero_init_residual:
-            for m in self.modules():
-                if isinstance(m, BasicBlock):
-                    nn.init.constant_(m.bn2.weight, 0)
-
-    def _make_layer(self, block, planes, blocks, stride=1, dilate=False):
-        norm_layer = self._norm_layer
-        downsample = None
-        previous_dilation = self.dilation
-        if dilate:
-            self.dilation *= stride
-            stride = 1
-        if stride != 1 or self.inplanes != planes * block.expansion:
-            downsample = nn.Sequential(
-                conv_subsumpling(self.inplanes, planes * block.expansion, stride),
-                norm_layer(planes * block.expansion),
-            )
-
-        layers = []
-        layers.append(
-            block(
-                self.inplanes,
-                planes,
-                stride,
-                downsample,
-                self.groups,
-                self.base_width,
-                previous_dilation,
-                norm_layer,
-            ),
-        )
-        self.inplanes = planes * block.expansion
-        for _ in range(1, blocks):
-            layers.append(
-                block(
-                    self.inplanes,
-                    planes,
-                    groups=self.groups,
-                    base_width=self.base_width,
-                    dilation=self.dilation,
-                    norm_layer=norm_layer,
-                ),
-            )
-
-        return nn.Sequential(*layers)
 
     def forward(self, x):
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu(x)
+        x = self.features(x)
+        x = x.view(x.size(0), 128 * 28)
+        x = self.classifier(x)
+        return x
 
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.layer4(x)
 
-        x = self.avgpool(x)
-        x = x.reshape(x.size(0), -1)
-        x = self.fc(x)
+class Flatten(nn.Module):
+    def forward(self, input):
+        return input.view(input.size(0), -1)
 
+
+class ZolotyhNet(nn.Module):
+    def __init__(self, num_classes=8):
+        super().__init__()
+
+        self.features_up = nn.Sequential(
