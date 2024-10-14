58a9f7cfe4e68adbc61d15a834d373f0f4b95653
maheshb <maheshb@velozityglobal.com>
2024-10-14 11:31:41 +0000

    Refactor tensorflow_impl/cnn for multi-GPU training (lines 0-12)

--- AI/tensorflow_impl/cnn.py
+++ AI/tensorflow_impl/cnn.py
@@ -1,11 +1,12 @@
+
+    # If model not freezed, save the model
+    if not freeze:
+        model.saver.save(sess)
+
+    # Run testing accuracy
+    acc = sess.run(accuracy, feed_dict={x: X_test,
+                                        y: Y_test,
+                                        keep_prob: 1.0})
+    print("Testing Accuracy: {0:.4f}".format(acc))
     print("-"*50)
 
-    # Total training time
-    print("Total training time: {0:.2f}s".format(time.time() - total_time))
-    loss, acc = sess.run([cost, accuracy], feed_dict={x: X_train,
-                                                      y: Y_train,
-                                                      keep_prob: 1.0})
-    
-   
-    print("Training Accuracy: {0:.4f}".format(acc))
-    print("Training Loss: {0:.4f}".format(loss))
