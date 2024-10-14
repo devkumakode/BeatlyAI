dd54e1871ca27943a3e5909d75943a5a863667da
maheshb <maheshb@velozityglobal.com>
2024-10-14 10:02:07 +0000

    Optimize tensorflow_impl/cnn for better performance (lines 0-11)

--- AI/tensorflow_impl/cnn.py
+++ AI/tensorflow_impl/cnn.py
@@ -1,23 +1,11 @@
-        print("#"*50)
-        print("Epoch summary:")
-        print("Epoch: {}".format(epoch))
-        print("Training took: {0:.2f}s".format(time.time() - epoch_time))
-        summary, acc = sess.run([merged, accuracy],
-                                feed_dict={x: X_train,
-                                           y: Y_train,
-                                           keep_prob: 1.0})
-        print("Training accuracy: {0:.4f}".format(acc))
+    print("-"*50)
 
-        # Run testing accuracy
-        acc = sess.run(accuracy, feed_dict={x: X_test,
-                                            y: Y_test,
-                                            keep_prob: 1.0})
-        print("Testing accuracy: {0:.4f}".format(acc))
-        print("#"*50)
-
-        # write to log
-        model.tensorboard_handler.writer.add_summary(summary, epoch)
-
-        # Reset epoch time
-        epoch_time = time.time()
-
+    # Total training time
+    print("Total training time: {0:.2f}s".format(time.time() - total_time))
+    loss, acc = sess.run([cost, accuracy], feed_dict={x: X_train,
+                                                      y: Y_train,
+                                                      keep_prob: 1.0})
+    
+   
+    print("Training Accuracy: {0:.4f}".format(acc))
+    print("Training Loss: {0:.4f}".format(loss))
